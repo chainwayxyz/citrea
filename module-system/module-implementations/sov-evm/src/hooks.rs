7c76d1d91f01837195c17fa3bfd778d287f69224
Esad Yusuf Atik <esadyusufatik+git@gmail.com>
2023-11-28 11:22:02 +0300

    use old conversion of pre_state_root (#8)
    * rename last_block_hashes to latest_block_hashes
    
    * use old conversion of pre_state_root

--- module-system/module-implementations/sov-evm/src/hooks.rs
+++ module-system/module-implementations/sov-evm/src/hooks.rs
@@ -22,15 +22,14 @@ where
             .get(working_set)
             .expect("Head block should always be set");
 
-        let root_hash_bytes: [u8; 32] = pre_state_root.clone().into();
-        parent_block.header.state_root = root_hash_bytes.into();
+        parent_block.header.state_root = H256(pre_state_root.clone().into());
         self.head.set(&parent_block, working_set);
 
         let sealed_parent_block = parent_block.clone().seal();
         let last_block_hash = sealed_parent_block.header.hash;
 
         // since we know the previous state root only here, we can set the last block hash
-        self.last_block_hashes.set(
+        self.latest_block_hashes.set(
             &U256::from(parent_block.header.number),
             &last_block_hash,
             working_set,
@@ -57,7 +56,7 @@ where
         // then on block 258
         // remove block 1, keep blocks 2-257
         if new_pending_env.number > 256 {
-            self.last_block_hashes
+            self.latest_block_hashes
                 .remove(&U256::from(new_pending_env.number - 257), working_set);
         }
     }
