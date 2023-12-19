f35c598b488c1fbe07d5784f23c0b7286aaebcae
Esad Yusuf Atik <esadyusufatik+git@gmail.com>
2023-12-19 16:12:36 +0300

    handle u64 -> u8 overflow (#57)
    * handle u64 -> u8 overflow
    
    * use prettier parsing
    
    * add comments
    
    * fix error

--- full-node/sequencer-client/src/lib.rs
+++ full-node/sequencer-client/src/lib.rs
@@ -3,6 +3,7 @@ use ethers::types::{Bytes, H256};
 use jsonrpsee::core::client::ClientT;
 use jsonrpsee::http_client::{HttpClient, HttpClientBuilder};
 use jsonrpsee::rpc_params;
+use serde::Deserialize;
 use serde_json::Value;
 use tracing::info;
 
@@ -30,22 +31,13 @@ impl SequencerClient {
 
     /// Gets l2 block given l2 height
     pub async fn get_sov_tx(&self, num: u64) -> anyhow::Result<Vec<u8>> {
-        let raw_res: Value = self
+        let res: GetSovTxResponse = self
             .client
             .request("ledger_getTransactionByNumber", rpc_params![num])
             .await
             .context("Failed to make RPC request")?;
 
-        let body = raw_res
-            .get("body")
-            .context("Body field missing in response")?
-            .as_array()
-            .context("Body field is not an array")?
-            .iter()
-            // TODO: handle overflow from u64 to u8 https://github.com/chainwayxyz/secret-sovereign-sdk/issues/48
-            .map(|x| x.as_u64().unwrap() as u8)
-            .collect();
-        Ok(body)
+        Ok(res.body)
     }
 
     /// Sends raw tx to sequencer
@@ -56,4 +48,10 @@ impl SequencerClient {
             .await?;
         Ok(tx_hash)
     }
+}
+
+// the response has more fields, however for now we don't need them
+#[derive(Deserialize)]
+struct GetSovTxResponse {
+    pub body: Vec<u8>,
 }
