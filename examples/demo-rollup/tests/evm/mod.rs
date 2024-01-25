184d6f9105e4bfdde86fd20ccc19c82511673be5
Ömer Talip Akalın <omertalib42@gmail.com>
2024-01-25 09:23:46 +0100

    Add more checks

--- examples/demo-rollup/tests/evm/mod.rs
+++ examples/demo-rollup/tests/evm/mod.rs
@@ -488,16 +488,27 @@ async fn execute(client: &Box<TestClient>) -> Result<(), Box<dyn std::error::Err
             4
         );
 
-        let latest_fee_history_with_rewards = client
+        assert!(client
             .eth_fee_history(
                 // block count hex
                 "0x100".to_string(),
                 reth_primitives::BlockNumberOrTag::Latest,
                 Some(vec![0.01, 0.2]), // totally random
             )
-            .await;
+            .await
+            .reward
+            .is_some());
 
-        assert!(latest_fee_history_with_rewards.reward.is_some())
+        assert!(client
+            .eth_fee_history(
+                // block count hex
+                "0x100".to_string(),
+                reth_primitives::BlockNumberOrTag::Latest,
+                Some(vec![]), // totally random
+            )
+            .await
+            .reward
+            .is_some())
 
         // assert gas price is higher
         // TODO: emulate gas price oracle here to have exact value
