e04cbbfa3a9830cc7bd39a49a0626b9751d9a5ec
Erce Can Bektüre <canerce99@hotmail.com>
2024-01-26 11:51:46 +0300

    Lint

--- examples/demo-rollup/tests/soft_confirmation_rule_enforcer/mod.rs
+++ examples/demo-rollup/tests/soft_confirmation_rule_enforcer/mod.rs
@@ -1,7 +1,3 @@
-use std::error::Error;
-use std::panic::{self, AssertUnwindSafe, PanicInfo};
-use std::sync::Arc;
-
 use demo_stf::genesis_config::GenesisPaths;
 use sov_modules_stf_blueprint::kernels::basic::BasicKernelGenesisPaths;
 use sov_stf_runner::RollupProverConfig;
@@ -18,7 +14,7 @@ async fn too_many_l2_block_per_l1_block_should_panic() {
 
     let (seq_port_tx, seq_port_rx) = tokio::sync::oneshot::channel();
 
-    let seq_task = tokio::spawn(async {
+    let _ = tokio::spawn(async {
         let seq_port = seq_port_rx.await.unwrap();
         let test_client = make_test_client(seq_port).await;
         let limiting_number = test_client.get_limiting_number().await;
@@ -29,7 +25,7 @@ async fn too_many_l2_block_per_l1_block_should_panic() {
         assert_eq!(limiting_number, 10);
 
         // create 2*limiting_number + 1 blocks so it has to give error
-        for i in 0..2 * limiting_number + 1 {
+        for _ in 0..2 * limiting_number + 1 {
             let _response = test_client.spam_publish_batch_request().await;
         }
     });
