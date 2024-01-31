9e3d3fe87b8412a72b6de1e249e17954d7349021
Ömer Talip Akalın <omertalib42@gmail.com>
2024-01-31 21:23:24 +0100

    Test disable download

--- adapters/bitcoin/test_data/start_ci_regtest.sh
+++ adapters/bitcoin/test_data/start_ci_regtest.sh
@@ -1,14 +1,14 @@
-set -e
+# set -e
 
-BITCOIN_VERSION=0.25.0
+# BITCOIN_VERSION=0.25.0
 
-curl -O -L https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
-tar xf bitcoin-25.0-x86_64-linux-gnu.tar.gz
+# curl -O -L https://bitcoincore.org/bin/bitcoin-core-25.0/bitcoin-25.0-x86_64-linux-gnu.tar.gz
+# tar xf bitcoin-25.0-x86_64-linux-gnu.tar.gz
 
-export PATH=$PATH:./bitcoin-25.0/bin
+# export PATH=$PATH:./bitcoin-25.0/bin
 
 bitcoind -regtest=1 -daemon=1 -rpcuser=chainway -rpcpassword=topsecret -rpcport=38332
 sleep 5
-bitcoin-cli -regtest=1 -rpcport=38332 -rpcuser=chainway -rpcpassword=topsecret createwallet "testwallet324"
+bitcoin-cli -regtest=1 -rpcport=38332 -rpcuser=chainway -rpcpassword=topsecret loadwallet "testwallet324"
 sleep 5
 bitcoin-cli -rpcuser=chainway -rpcpassword=topsecret -rpcport=38332 -rpcwallet=testwallet324 -generate 250
