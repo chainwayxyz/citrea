165a535cfba7a9bb15a90abd9ee3a78cf8128779
Erce Can Bektüre <canerce99@hotmail.com>
2024-02-01 12:49:31 +0300

    WIP Implement debug_traceTransaction rpc

--- module-system/module-implementations/sov-evm/src/query.rs
+++ module-system/module-implementations/sov-evm/src/query.rs
@@ -1,17 +1,32 @@
 use std::array::TryFromSliceError;
+use std::convert::Infallible;
 use std::ops::{Range, RangeInclusive};
 
 use alloy_primitives::Uint;
 use alloy_rlp::Encodable;
 use jsonrpsee::core::RpcResult;
 use reth_interfaces::provider::ProviderError;
-use reth_primitives::TransactionKind::{Call, Create};
+use reth_primitives::TransactionKind::{self, Call, Create};
 use reth_primitives::{
-    Block, BlockId, BlockNumberOrTag, SealedHeader, TransactionSignedEcRecovered, U128, U256, U64,
+    Address, Block, BlockId, BlockNumberOrTag, SealedHeader, Transaction, TransactionSigned,
+    TransactionSignedEcRecovered, TxHash, B256, U128, U256, U64,
+};
+use reth_revm::database::{StateProviderDatabase, SubState};
+use reth_rpc::blocking_pool::BlockingTaskPool;
+use reth_rpc_types::trace::geth::{
+    BlockTraceResult, FourByteFrame, GethDebugBuiltInTracerType, GethDebugTracerType,
+    GethDebugTracingCallOptions, GethDebugTracingOptions, GethTrace, NoopFrame, TraceResult,
 };
 use reth_rpc_types_compat::block::from_primitive_with_hash;
+use revm::db::CacheDB;
+use revm::primitives::db::{Database, DatabaseCommit};
 use revm::primitives::{
-    EVMError, ExecutionResult, Halt, InvalidTransaction, TransactTo, KECCAK_EMPTY,
+    AccountInfo, Bytecode, CfgEnv, EVMError, Env, ExecutionResult, Halt, InvalidTransaction,
+    ResultAndState, TransactTo, TxEnv, KECCAK_EMPTY,
+};
+use revm::Inspector;
+use revm_inspectors::tracing::{
+    js::JsInspector, FourByteInspector, TracingInspector, TracingInspectorConfig,
 };
 use sov_modules_api::macros::rpc_gen;
 use sov_modules_api::prelude::*;
@@ -19,6 +34,7 @@ use sov_modules_api::WorkingSet;
 use tracing::info;
 
 use crate::call::get_cfg_env;
+use crate::error::rpc::EthResult;
 use crate::error::rpc::{ensure_success, EthApiError, RevertError, RpcInvalidTransactionError};
 use crate::evm::db::EvmDb;
 use crate::evm::primitive_types::{BlockEnv, Receipt, SealedBlock, TransactionSignedAndRecovered};
@@ -28,6 +44,170 @@ use crate::{
     BloomFilter, Evm, EvmChainConfig, FilterBlockOption, FilterError, MIN_CREATE_GAS,
     MIN_TRANSACTION_GAS,
 };
+
+/// Taken from reth
+/// https://github.com/paradigmxyz/reth/blob/606640285e763b64519213bad34c76fe4d24652f/crates/rpc/rpc/src/eth/revm_utils.rs#L69
+/// Helper type to work with different transaction types when configuring the EVM env.
+///
+/// This makes it easier to handle errors.
+pub(crate) trait FillableTransaction {
+    /// Returns the hash of the transaction.
+    fn hash(&self) -> TxHash;
+
+    /// Fill the transaction environment with the given transaction.
+    fn try_fill_tx_env(&self, tx_env: &mut TxEnv) -> EthResult<()>;
+}
+
+impl FillableTransaction for TransactionSignedEcRecovered {
+    fn hash(&self) -> TxHash {
+        self.hash
+    }
+
+    fn try_fill_tx_env(&self, tx_env: &mut TxEnv) -> EthResult<()> {
+        fill_tx_env_with_recovered(tx_env, self);
+        Ok(())
+    }
+}
+impl FillableTransaction for TransactionSigned {
+    fn hash(&self) -> TxHash {
+        self.hash
+    }
+
+    fn try_fill_tx_env(&self, tx_env: &mut TxEnv) -> EthResult<()> {
+        let signer = self
+            .recover_signer()
+            .ok_or_else(|| EthApiError::InvalidTransactionSignature)?;
+        fill_tx_env(tx_env, self, signer);
+        Ok(())
+    }
+}
+
+/// Fill transaction environment from [TransactionSignedEcRecovered].
+pub fn fill_tx_env_with_recovered(tx_env: &mut TxEnv, transaction: &TransactionSignedEcRecovered) {
+    fill_tx_env(tx_env, transaction.as_ref(), transaction.signer());
+}
+
+/// Returns a new [TxEnv] filled with the transaction's data.
+pub fn tx_env_with_recovered(transaction: &TransactionSignedEcRecovered) -> TxEnv {
+    let mut tx_env = TxEnv::default();
+
+    fill_tx_env(&mut tx_env, transaction.as_ref(), transaction.signer());
+
+    tx_env
+}
+
+/// Fill transaction environment from a [Transaction] and the given sender address.
+pub fn fill_tx_env<T>(tx_env: &mut TxEnv, transaction: T, sender: Address)
+where
+    T: AsRef<Transaction>,
+{
+    tx_env.caller = sender;
+    match transaction.as_ref() {
+        Transaction::Legacy(tx) => {
+            tx_env.gas_limit = tx.gas_limit;
+            tx_env.gas_price = U256::from(tx.gas_price);
+            tx_env.gas_priority_fee = None;
+            tx_env.transact_to = match tx.to {
+                TransactionKind::Call(to) => TransactTo::Call(to),
+                TransactionKind::Create => TransactTo::create(),
+            };
+            tx_env.value = tx.value.into();
+            tx_env.data = tx.input.clone();
+            tx_env.chain_id = tx.chain_id;
+            tx_env.nonce = Some(tx.nonce);
+            tx_env.access_list.clear();
+            tx_env.blob_hashes.clear();
+            tx_env.max_fee_per_blob_gas.take();
+        }
+        Transaction::Eip2930(tx) => {
+            tx_env.gas_limit = tx.gas_limit;
+            tx_env.gas_price = U256::from(tx.gas_price);
+            tx_env.gas_priority_fee = None;
+            tx_env.transact_to = match tx.to {
+                TransactionKind::Call(to) => TransactTo::Call(to),
+                TransactionKind::Create => TransactTo::create(),
+            };
+            tx_env.value = tx.value.into();
+            tx_env.data = tx.input.clone();
+            tx_env.chain_id = Some(tx.chain_id);
+            tx_env.nonce = Some(tx.nonce);
+            tx_env.access_list = tx
+                .access_list
+                .0
+                .iter()
+                .map(|l| {
+                    (
+                        l.address,
+                        l.storage_keys
+                            .iter()
+                            .map(|k| U256::from_be_bytes(k.0))
+                            .collect(),
+                    )
+                })
+                .collect();
+            tx_env.blob_hashes.clear();
+            tx_env.max_fee_per_blob_gas.take();
+        }
+        Transaction::Eip1559(tx) => {
+            tx_env.gas_limit = tx.gas_limit;
+            tx_env.gas_price = U256::from(tx.max_fee_per_gas);
+            tx_env.gas_priority_fee = Some(U256::from(tx.max_priority_fee_per_gas));
+            tx_env.transact_to = match tx.to {
+                TransactionKind::Call(to) => TransactTo::Call(to),
+                TransactionKind::Create => TransactTo::create(),
+            };
+            tx_env.value = tx.value.into();
+            tx_env.data = tx.input.clone();
+            tx_env.chain_id = Some(tx.chain_id);
+            tx_env.nonce = Some(tx.nonce);
+            tx_env.access_list = tx
+                .access_list
+                .0
+                .iter()
+                .map(|l| {
+                    (
+                        l.address,
+                        l.storage_keys
+                            .iter()
+                            .map(|k| U256::from_be_bytes(k.0))
+                            .collect(),
+                    )
+                })
+                .collect();
+            tx_env.blob_hashes.clear();
+            tx_env.max_fee_per_blob_gas.take();
+        }
+        Transaction::Eip4844(tx) => {
+            tx_env.gas_limit = tx.gas_limit;
+            tx_env.gas_price = U256::from(tx.max_fee_per_gas);
+            tx_env.gas_priority_fee = Some(U256::from(tx.max_priority_fee_per_gas));
+            tx_env.transact_to = match tx.to {
+                TransactionKind::Call(to) => TransactTo::Call(to),
+                TransactionKind::Create => TransactTo::create(),
+            };
+            tx_env.value = tx.value.into();
+            tx_env.data = tx.input.clone();
+            tx_env.chain_id = Some(tx.chain_id);
+            tx_env.nonce = Some(tx.nonce);
+            tx_env.access_list = tx
+                .access_list
+                .0
+                .iter()
+                .map(|l| {
+                    (
+                        l.address,
+                        l.storage_keys
+                            .iter()
+                            .map(|k| U256::from_be_bytes(k.0))
+                            .collect(),
+                    )
+                })
+                .collect();
+            tx_env.blob_hashes = tx.blob_versioned_hashes.clone();
+            tx_env.max_fee_per_blob_gas = Some(U256::from(tx.max_fee_per_blob_gas));
+        }
+    }
+}
 
 #[rpc_gen(client, server)]
 impl<C: sov_modules_api::Context> Evm<C> {
@@ -860,6 +1040,217 @@ impl<C: sov_modules_api::Context> Evm<C> {
         });
 
         Ok(transaction)
+    }
+
+    /// Handler for `debug_traceTransaction`
+    #[rpc_method(name = "debug_traceTransaction")]
+    pub fn debug_trace_transaction(
+        &self,
+        tx_hash: B256,
+        opts: Option<GethDebugTracingOptions>,
+        working_set: &mut WorkingSet<C>,
+    ) -> RpcResult<GethTrace> {
+        info!("evm module: debug_traceTransaction({})", tx_hash);
+        // let _permit = self.acquire_trace_permit().await;
+        // DebugApi::new(provider, eth, blocking_task_guard)
+        // Ok(DebugApi::debug_trace_transaction(self, tx_hash, opts.unwrap_or_default()).await?)
+
+        // TODO: check mempool if still not found return error
+        let tx_index = self
+            .transaction_hashes
+            .get(&tx_hash, &mut working_set.accessory_state())
+            .unwrap();
+        // TODO: check mempool if still not found return error
+        let transaction: TransactionSignedAndRecovered = self
+            .transactions
+            .get(tx_index as usize, &mut working_set.accessory_state())
+            .unwrap();
+        let block_number = transaction.block_number;
+
+        let sealed_block = self
+            .get_sealed_block_by_number(Some(BlockNumberOrTag::Number(block_number)), working_set)
+            .expect("Block with known tx must be set");
+        // set the archival version to the block number
+        working_set.set_archival_version(sealed_block.header.number);
+        let block_env = BlockEnv::from(&sealed_block);
+        let cfg = self.cfg.get(working_set).unwrap();
+        let cfg_env = get_cfg_env(&block_env, cfg, Some(get_cfg_env_template()));
+        let parent_hash: BlockId = sealed_block.header.parent_hash.into();
+        let block_txs: Vec<TransactionSigned> = sealed_block
+            .transactions
+            .clone()
+            .map(|id| {
+                self.transactions
+                    .get(id as usize, &mut working_set.accessory_state())
+                    .expect("Transaction must be set")
+                    .signed_transaction
+            })
+            .collect();
+        // TODO: Convert below steps to blocking task
+        let tx: TransactionSignedEcRecovered = transaction.into();
+        // replacement of revm::CacheDB due to nonexistent readonly db
+        let mut evm_db = self.get_db(working_set);
+        let revm_block_env = revm::primitives::BlockEnv::from(&block_env);
+        self.replay_transactions_until(
+            &mut evm_db,
+            cfg_env.clone(),
+            revm_block_env.clone(),
+            block_txs,
+            tx.hash(),
+        )?;
+        let env = Env {
+            cfg: cfg_env,
+            block: revm_block_env,
+            tx: tx_env_with_recovered(&tx),
+        };
+        // TODO: Handle error here
+        Ok(self
+            .trace_transaction(opts.unwrap_or_default(), env, &mut evm_db)
+            .map(|(trace, _)| trace)
+            .unwrap())
+    }
+
+    fn trace_transaction(
+        &self,
+        opts: GethDebugTracingOptions,
+        env: Env,
+        db: &mut EvmDb<'_, C>,
+    ) -> EthResult<(GethTrace, revm::primitives::State)> {
+        let GethDebugTracingOptions {
+            config,
+            tracer,
+            tracer_config,
+            ..
+        } = opts;
+        println!("10");
+        if let Some(tracer) = tracer {
+            return match tracer {
+                GethDebugTracerType::BuiltInTracer(tracer) => match tracer {
+                    GethDebugBuiltInTracerType::FourByteTracer => {
+                        let mut inspector = FourByteInspector::default();
+                        let (res, _) = self.inspect(db, env, &mut inspector)?;
+                        return Ok((FourByteFrame::from(inspector).into(), res.state));
+                    }
+                    GethDebugBuiltInTracerType::CallTracer => {
+                        let call_config = tracer_config
+                            .into_call_config()
+                            .map_err(|_| EthApiError::InvalidTracerConfig)?;
+                        let mut inspector = TracingInspector::new(
+                            TracingInspectorConfig::from_geth_config(&config)
+                                .set_record_logs(call_config.with_log.unwrap_or_default()),
+                        );
+                        let (res, _) = self.inspect(db, env, &mut inspector)?;
+                        let frame = inspector
+                            .into_geth_builder()
+                            .geth_call_traces(call_config, res.result.gas_used());
+                        return Ok((frame.into(), res.state));
+                    }
+                    GethDebugBuiltInTracerType::PreStateTracer => {
+                        // Requires DatabaseRef trait
+                        // meaning we need a readonly state to implement this
+                        todo!("PreStateTracer")
+                    }
+                    GethDebugBuiltInTracerType::NoopTracer => {
+                        Ok((NoopFrame::default().into(), Default::default()))
+                    }
+                },
+                GethDebugTracerType::JsTracer(code) => {
+                    // This also requires DatabaseRef trait
+                    // Implement after readonly state is implemented
+                    todo!("JsTracer")
+                }
+            };
+        }
+
+        // default structlog tracer
+        let inspector_config = TracingInspectorConfig::from_geth_config(&config);
+
+        let mut inspector = TracingInspector::new(inspector_config);
+
+        let (res, _) = self.inspect(db, env, &mut inspector)?;
+        let gas_used = res.result.gas_used();
+        let return_value = res.result.into_output().unwrap_or_default();
+        let frame = inspector
+            .into_geth_builder()
+            .geth_traces(gas_used, return_value, config);
+
+        Ok((frame.into(), res.state))
+    }
+
+    /// Executes the [Env] against the given [Database] without committing state changes.
+    pub(crate) fn inspect<DB, I>(
+        &self,
+        db: DB,
+        env: Env,
+        inspector: I,
+    ) -> EthResult<(ResultAndState, Env)>
+    where
+        DB: Database,
+        <DB as Database>::Error: Into<EthApiError>,
+        I: Inspector<DB>,
+    {
+        let mut evm = revm::EVM::with_env(env);
+        evm.database(db);
+        // TODO: Convert errors
+        let res = evm.inspect(inspector)?;
+        Ok((res, evm.env))
+    }
+
+    /// Same as [inspect] but also returns the database again.
+    ///
+    /// Even though [Database] is also implemented on `&mut`
+    /// this is still useful if there are certain trait bounds on the Inspector's database generic type
+    pub(crate) fn inspect_and_return_db<DB, I>(
+        &self,
+        db: DB,
+        env: Env,
+        inspector: I,
+    ) -> EthResult<(ResultAndState, Env, DB)>
+    where
+        DB: Database,
+        <DB as Database>::Error: Into<EthApiError>,
+        I: Inspector<DB>,
+    {
+        let mut evm = revm::EVM::with_env(env);
+        evm.database(db);
+        // TODO: Convert errors
+        let res = evm.inspect(inspector)?;
+        let db = evm.take_db();
+        Ok((res, evm.env, db))
+    }
+
+    /// Heavily inspired from:
+    /// https://github.com/paradigmxyz/reth/blob/606640285e763b64519213bad34c76fe4d24652f/crates/rpc/rpc/src/eth/revm_utils.rs#L176
+    fn replay_transactions_until<I, Tx>(
+        &self,
+        db: &mut EvmDb<'_, C>,
+        cfg: CfgEnv,
+        block_env: revm::primitives::BlockEnv,
+        transactions: I,
+        target_tx_hash: B256,
+    ) -> EthResult<()>
+    where
+        I: IntoIterator<Item = Tx>,
+        Tx: FillableTransaction,
+    {
+        let env = Env {
+            cfg,
+            block: block_env,
+            tx: TxEnv::default(),
+        };
+        let mut evm = revm::EVM::with_env(env);
+        evm.database(db);
+        for tx in transactions.into_iter() {
+            if tx.hash() == target_tx_hash {
+                // reached the target transaction
+                break;
+            }
+
+            tx.try_fill_tx_env(&mut evm.env.tx)?;
+            let res = evm.transact()?;
+            evm.db.as_mut().expect("is set").commit(res.state)
+        }
+        Ok(())
     }
 
     // https://github.com/paradigmxyz/reth/blob/8892d04a88365ba507f28c3314d99a6b54735d3f/crates/rpc/rpc/src/eth/filter.rs#L349
