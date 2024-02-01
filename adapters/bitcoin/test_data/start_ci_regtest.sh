00a4971068ab8da31f8ea5981a5315a324808f37
Ömer Talip Akalın <omertalib42@gmail.com>
2024-02-01 09:30:21 +0100

    Test remove testwallet

--- adapters/bitcoin/test_data/start_ci_regtest.sh
+++ adapters/bitcoin/test_data/start_ci_regtest.sh
@@ -9,6 +9,8 @@
 
 bitcoind -regtest=1 -daemon=1 -rpcuser=chainway -rpcpassword=topsecret -rpcport=38332
 sleep 10
+rm -rf /home/chainway/.bitcoin/regtest/wallets/testwallet
+sleep 5
 bitcoin-cli -regtest=1 -rpcport=38332 -rpcuser=chainway -rpcpassword=topsecret createwallet "testwallet"
 sleep 5
 bitcoin-cli -rpcuser=chainway -rpcpassword=topsecret -rpcport=38332 -rpcwallet=testwallet324 -generate 250
