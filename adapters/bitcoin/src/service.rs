68c2cefeb8af1a7452b79b58bb107e13a54338c8
Ömer Talip Akalın <omertalib42@gmail.com>
2024-02-26 11:10:52 +0300

    Implement get_block_by_hash in btc da

--- adapters/bitcoin/src/service.rs
+++ adapters/bitcoin/src/service.rs
@@ -440,8 +440,21 @@ impl DaService for BitcoinService {
         Ok(res)
     }
 
-    async fn get_block_by_hash(&self, _hash: [u8; 32]) -> Result<Self::FilteredBlock, Self::Error> {
-        unimplemented!()
+    async fn get_block_by_hash(&self, hash: [u8; 32]) -> Result<Self::FilteredBlock, Self::Error> {
+        let client = self.client.clone();
+        info!("Getting block at height {}", height);
+
+        // Check: Is this hex already?
+        // If not, a middle step of hex conversion is needed
+        let vec_hash = hash.to_vec();
+
+        let hash = match String::from_utf8(vec_hash) {
+            Ok(hash) => hash,
+            Err(_) => return Err(anyhow::anyhow!("Failed to convert block hash to string")),
+        };
+
+        let block = client.get_block(hash).await?;
+        Ok(block)
     }
 }
 
