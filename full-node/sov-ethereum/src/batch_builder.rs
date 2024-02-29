ad6311f0cefdee14ec3f7fd745fa74a9e2255b5b
Roman <r.proskuryakoff@gmail.com>
2024-02-29 16:08:53 +0700

    Refactor: simplify EthBatchBuilder::add_messages (#197)

--- full-node/sov-ethereum/src/batch_builder.rs
+++ full-node/sov-ethereum/src/batch_builder.rs
@@ -27,8 +27,6 @@ impl<C: sov_modules_api::Context> EthBatchBuilder<C> {
 
     /// Adds `messages` to the mempool.
     pub fn add_messages(&mut self, messages: Vec<Vec<u8>>) {
-        for message in messages {
-            self.mempool.push_back(message);
-        }
+        self.mempool.extend(messages);
     }
 }
