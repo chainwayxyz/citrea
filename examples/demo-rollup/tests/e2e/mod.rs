af522e0173af22607d5fb8a8e2399cc9ce7721dc
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-03-05 17:30:02 +0700

    Add link to an isssue in e2e tests with L2 blocks

--- examples/demo-rollup/tests/e2e/mod.rs
+++ examples/demo-rollup/tests/e2e/mod.rs
@@ -892,8 +892,8 @@ async fn test_soft_confirmations_status_two_l1() -> Result<(), anyhow::Error> {
 
     // publish new da block
     da_service.publish_test_block().await.unwrap();
-    seq_test_client.send_publish_batch_request().await; // FIXME should work without L2 block
-    seq_test_client.send_publish_batch_request().await; // FIXME should work without L2 block
+    seq_test_client.send_publish_batch_request().await; // TODO https://github.com/chainwayxyz/secret-sovereign-sdk/issues/214
+    seq_test_client.send_publish_batch_request().await; // TODO https://github.com/chainwayxyz/secret-sovereign-sdk/issues/214
 
     sleep(Duration::from_secs(2)).await;
 
