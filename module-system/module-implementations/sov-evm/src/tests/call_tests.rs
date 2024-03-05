c8a7825839796a8fbd40622b60d1670abcd22ab5
eyusufatik <esadyusufatik@gmail.com>
2024-03-05 09:36:09 +0300

    fmt

--- module-system/module-implementations/sov-evm/src/tests/call_tests.rs
+++ module-system/module-implementations/sov-evm/src/tests/call_tests.rs
@@ -10,8 +10,9 @@ use sov_modules_api::{Context, Module, StateMapAccessor, StateVecAccessor};
 
 use crate::call::CallMessage;
 use crate::evm::primitive_types::Receipt;
-use crate::smart_contracts::{BlockHashContract, LogsContract};
-use crate::smart_contracts::{SelfDestructorContract, SimpleStorageContract, TestContract};
+use crate::smart_contracts::{
+    BlockHashContract, LogsContract, SelfDestructorContract, SimpleStorageContract, TestContract,
+};
 use crate::tests::genesis_tests::get_evm;
 use crate::tests::test_signer::TestSigner;
 use crate::tests::DEFAULT_CHAIN_ID;
