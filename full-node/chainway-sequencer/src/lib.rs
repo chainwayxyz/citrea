38c325078871b56e02f23032833dbdf1c1b6c72a
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-03-06 20:02:10 +0700

    Make db_provider and DbProvider private

--- full-node/chainway-sequencer/src/lib.rs
+++ full-node/chainway-sequencer/src/lib.rs
@@ -1,11 +1,10 @@
 mod commitment_controller;
 mod config;
-pub mod db_provider;
+mod db_provider;
 mod mempool;
 mod rpc;
 mod sequencer;
 mod utils;
 
 pub use config::SequencerConfig;
-pub use db_provider::DbProvider;
 pub use sequencer::ChainwaySequencer;
