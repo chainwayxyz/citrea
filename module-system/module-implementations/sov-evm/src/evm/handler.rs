bd43170c8504777846cb9fec17dae0d450af391a
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-03-07 15:39:13 +0700

    Calculate state diff size and decrement account balance

--- module-system/module-implementations/sov-evm/src/evm/handler.rs
+++ module-system/module-implementations/sov-evm/src/evm/handler.rs
@@ -1,7 +1,9 @@
+use std::mem::size_of_val;
 use std::sync::Arc;
 
 use revm::handler::register::EvmHandler;
-use revm::primitives::{EVMError, ResultAndState};
+use revm::interpreter::InstructionResult;
+use revm::primitives::{EVMError, ExecutionResult, ResultAndState, State, U256};
 use revm::{Context, Database, FrameResult};
 
 pub(crate) fn citrea_handle_register<EXT, DB>(handler: &mut EvmHandler<'_, EXT, DB>)
@@ -21,6 +23,61 @@ impl<EXT, DB: Database> CitreaHandler<EXT, DB> {
         context: &mut Context<EXT, DB>,
         result: FrameResult,
     ) -> Result<ResultAndState, EVMError<<DB as Database>::Error>> {
-        revm::handler::mainnet::output(context, result)
+        let result_and_state = revm::handler::mainnet::output(context, result)?;
+        let ResultAndState { result, state } = result_and_state;
+        if result.is_success() {
+            let diff_size = state_diff_size(&state);
+            let l1_fee_rate = 1; // TODO: get it somehow
+            let l1_fee = U256::from(diff_size * l1_fee_rate);
+            if let Some(_out_of_funds) = decrease_caller_balance(context, l1_fee)? {
+                let result = ExecutionResult::Revert {
+                    gas_used: result.gas_used(),
+                    output: result
+                        .into_output()
+                        .expect("ExecutionResult::Success always has an output"),
+                };
+                return Ok(ResultAndState { result, state });
+            }
+        }
+        Ok(ResultAndState { result, state })
     }
+}
+
+/// Calculates the diff of the modified state.
+fn state_diff_size(state: &State) -> usize {
+    let mut diff_size = 0usize;
+    for account in state.values() {
+        for (k, v) in account.changed_storage_slots() {
+            //
+            let p = &v.previous_or_original_value;
+            let c = &v.present_value;
+            let slot_size = size_of_val(k) + size_of_val(p) + size_of_val(c);
+            diff_size += slot_size;
+        }
+    }
+    diff_size
+}
+
+/// Decreases the balance of the caller by the given amount.
+/// Returns Ok(Some) if the caller's balance is not enough.
+fn decrease_caller_balance<EXT, DB: Database>(
+    context: &mut Context<EXT, DB>,
+    amount: U256,
+) -> Result<Option<InstructionResult>, EVMError<DB::Error>> {
+    let caller = context.evm.env.tx.caller;
+
+    let (caller_account, _) = context
+        .evm
+        .journaled_state
+        .load_account(caller, &mut context.evm.db)?;
+
+    let balance = &mut caller_account.info.balance;
+
+    let Some(new_balance) = balance.checked_sub(amount) else {
+        return Ok(Some(InstructionResult::OutOfFunds));
+    };
+
+    *balance = new_balance;
+
+    Ok(None)
 }
