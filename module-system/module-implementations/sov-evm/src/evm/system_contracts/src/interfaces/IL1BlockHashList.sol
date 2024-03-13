47dd8cd320946a3528740cb0af165cef9b1cfca5
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-03-13 15:57:49 +0300

    Add initialize block number function and natspec

--- module-system/module-implementations/sov-evm/src/evm/system_contracts/src/interfaces/IL1BlockHashList.sol
+++ module-system/module-implementations/sov-evm/src/evm/system_contracts/src/interfaces/IL1BlockHashList.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 interface IL1BlockHashList {
+    function initializeBlockNumber(uint256) external;
     function setBlockInfo(bytes32, bytes32) external;
     function getBlockHash(uint256) external view returns (bytes32);
-    function getMerkleRootFromBlockHash(bytes32) external view returns (bytes32);
-    function getMerkleRootFromBlockNumber(uint256) external view returns (bytes32);
+    function getMerkleRootByHash(bytes32) external view returns (bytes32);
+    function getMerkleRootByNumber(uint256) external view returns (bytes32);
 }
--- module-system/module-implementations/sov-evm/src/evm/system_contracts/test/L1BlockHashList.t.sol
+++ module-system/module-implementations/sov-evm/src/evm/system_contracts/test/L1BlockHashList.t.sol
@@ -8,38 +8,59 @@ contract L1BlockHashListTest is Test {
     L1BlockHashList l1BlockHashList;
     bytes32 randomBlockHash = bytes32(keccak256("CITREA_TEST"));
     bytes32 randomMerkleRoot = bytes32(keccak256("CITREA"));
+    uint256 constant INITIAL_BLOCK_NUMBER = 505050;
 
     function setUp() public {
         l1BlockHashList = new L1BlockHashList();
     }
 
     function testSetBlockInfo() public {
+        l1BlockHashList.initializeBlockNumber(INITIAL_BLOCK_NUMBER);
         l1BlockHashList.setBlockInfo(randomBlockHash, randomMerkleRoot);
-        assertEq(l1BlockHashList.getBlockHash(0), randomBlockHash);
-        assertEq(l1BlockHashList.getMerkleRootFromBlockHash(randomBlockHash), randomMerkleRoot);
-        assertEq(l1BlockHashList.getMerkleRootFromBlockNumber(0), randomMerkleRoot);
+        assertEq(l1BlockHashList.getBlockHash(INITIAL_BLOCK_NUMBER), randomBlockHash);
+        assertEq(l1BlockHashList.getMerkleRootByHash(randomBlockHash), randomMerkleRoot);
+        assertEq(l1BlockHashList.getMerkleRootByNumber(INITIAL_BLOCK_NUMBER), randomMerkleRoot);
+    }
+
+    function testCannotReinitalize() public {
+        l1BlockHashList.initializeBlockNumber(INITIAL_BLOCK_NUMBER);
+        vm.expectRevert("Already initialized");
+        l1BlockHashList.initializeBlockNumber(INITIAL_BLOCK_NUMBER - 10);
     }
 
     function testNonOwnerCannotSetBlockInfo() public {
+        l1BlockHashList.initializeBlockNumber(INITIAL_BLOCK_NUMBER);
         vm.startPrank(address(0x1));
         vm.expectRevert("Caller is not owner");
         l1BlockHashList.setBlockInfo(randomBlockHash, randomMerkleRoot);
     }
 
+    function testNonOwnerCannotInitializeBlockNumber() public {
+        vm.startPrank(address(0x1));
+        vm.expectRevert("Caller is not owner");
+        l1BlockHashList.initializeBlockNumber(INITIAL_BLOCK_NUMBER);
+    }
+
+    function testCannotSetInfoWithoutInitialize() public {
+        vm.expectRevert("Not initialized");
+        l1BlockHashList.setBlockInfo(randomBlockHash, randomMerkleRoot);
+    }
+
     function testBlockInfoAvailableAfterManyWrites() public {
+        l1BlockHashList.initializeBlockNumber(INITIAL_BLOCK_NUMBER);
         for (uint256 i = 0; i < 100; i++) {
             bytes32 blockHash = keccak256(abi.encodePacked(i));
             bytes32 root = keccak256(abi.encodePacked(blockHash));
             l1BlockHashList.setBlockInfo(blockHash, root);
-            assertEq(l1BlockHashList.getBlockHash(i), blockHash);
-            assertEq(l1BlockHashList.getMerkleRootFromBlockHash(blockHash), root);
-            assertEq(l1BlockHashList.getMerkleRootFromBlockNumber(i), root);
+            assertEq(l1BlockHashList.getBlockHash(i + INITIAL_BLOCK_NUMBER), blockHash);
+            assertEq(l1BlockHashList.getMerkleRootByHash(blockHash), root);
+            assertEq(l1BlockHashList.getMerkleRootByNumber(i + INITIAL_BLOCK_NUMBER), root);
         }
 
         bytes32 zeroth_hash = keccak256(abi.encodePacked(uint(0)));
         bytes32 zeroth_root = keccak256(abi.encodePacked(zeroth_hash));
-        assertEq(l1BlockHashList.getBlockHash(0), zeroth_hash);
-        assertEq(l1BlockHashList.getMerkleRootFromBlockHash(zeroth_hash), zeroth_root);
-        assertEq(l1BlockHashList.getMerkleRootFromBlockNumber(0), zeroth_root);
+        assertEq(l1BlockHashList.getBlockHash(INITIAL_BLOCK_NUMBER), zeroth_hash);
+        assertEq(l1BlockHashList.getMerkleRootByHash(zeroth_hash), zeroth_root);
+        assertEq(l1BlockHashList.getMerkleRootByNumber(INITIAL_BLOCK_NUMBER), zeroth_root);
     }
 }
