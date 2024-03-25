69934a3764cdb1f4fd4bd0e8ce61c0ac762f3262
otaliptus <omertalib42@gmail.com>
2024-03-25 14:27:46 +0300

    Increase sync time

--- examples/demo-rollup/tests/e2e/mod.rs
+++ examples/demo-rollup/tests/e2e/mod.rs
@@ -164,7 +164,7 @@ async fn test_soft_batch_save() -> Result<(), anyhow::Error> {
     let full_node_port_2 = full_node_port_rx_2.await.unwrap();
     let full_node_test_client_2 = make_test_client(full_node_port_2).await;
 
-    sleep(Duration::from_secs(10)).await;
+    sleep(Duration::from_secs(20)).await;
 
     let seq_block = seq_test_client
         .eth_get_block_by_number(Some(BlockNumberOrTag::Latest))
