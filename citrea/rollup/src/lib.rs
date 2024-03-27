513b3bab3efe7995a4d989a6f2241d1e9fd82749
otaliptus <omertalib42@gmail.com>
2024-03-27 11:56:28 +0300

    Refactor rollup

--- citrea/rollup/src/lib.rs
+++ citrea/rollup/src/lib.rs
@@ -4,28 +4,16 @@
 use std::env;
 use std::str::FromStr;
 
-use const_rollup_config::{ROLLUP_BATCH_NAMESPACE_RAW, ROLLUP_PROOF_NAMESPACE_RAW};
-use sov_celestia_adapter::types::Namespace;
 mod mock_rollup;
 pub use mock_rollup::*;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{fmt, EnvFilter};
 
-mod celestia_rollup;
-pub use celestia_rollup::*;
-
 mod eth;
 
 mod bitcoin_rollup;
 pub use bitcoin_rollup::*;
-
-/// The rollup stores its data in the namespace b"sov-test" on Celestia
-/// You can change this constant to point your rollup at a different namespace
-pub const ROLLUP_BATCH_NAMESPACE: Namespace = Namespace::const_v0(ROLLUP_BATCH_NAMESPACE_RAW);
-
-/// The rollup stores the zk proofs in the namespace b"sov-test-p" on Celestia.
-pub const ROLLUP_PROOF_NAMESPACE: Namespace = Namespace::const_v0(ROLLUP_PROOF_NAMESPACE_RAW);
 
 /// Default initialization of logging
 pub fn initialize_logging() {
