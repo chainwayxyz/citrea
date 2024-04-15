eac36739dfd828bd41a90c357b4780313200b91b
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-04-15 17:21:05 +0800

    Update uniswap tests from sov repo

--- bin/citrea/tests/evm/uniswap/scripts/02_swap.js
+++ bin/citrea/tests/evm/uniswap/scripts/02_swap.js
@@ -1,82 +1,145 @@
-require('dotenv').config()
-const fs = require('fs');
-const { promisify } = require('util');
+const { Contract, ContractFactory, utils, constants } = require("ethers");
 
-const { providers, Contract, utils, constants } = require('ethers');
-const routerArtifact = require('@uniswap/v2-periphery/build/UniswapV2Router02.json')
-const usdtArtifact = require("../artifacts/contracts/Tether.sol/Tether.json")
-const usdcArtifact = require("../artifacts/contracts/UsdCoin.sol/UsdCoin.json")
+const fs = require("fs");
+const { promisify } = require("util");
 
-USDT_ADDRESS = process.env.USDT_ADDRESS
-USDC_ADDRESS = process.env.USDC_ADDRESS
-WETH_ADDRESS = process.env.WETH_ADDRESS
-FACTORY_ADDRESS = process.env.FACTORY_ADDRESS
-PAIR_ADDRESS = process.env.PAIR_ADDRESS
-ROUTER_ADDRESS = process.env.ROUTER_ADDRESS
+const WETH9 = require("../WETH9.json");
 
+const factoryArtifact = require("@uniswap/v2-core/build/UniswapV2Factory.json");
+const routerArtifact = require("@uniswap/v2-periphery/build/UniswapV2Router02.json");
+const pairArtifact = require("@uniswap/v2-periphery/build/IUniswapV2Pair.json");
 
+async function main() {
+  console.log("Starting Uniswap deployment script...");
+  const [owner, trader] = await ethers.getSigners();
+  // assert that owner.address and trader.address are not undefined
+  if (!owner || !owner.address || !trader || !trader.address) {
+    throw new Error("Could not get owner and trader addresses");
+  }
 
-const provider = new providers.JsonRpcProvider('http://127.0.0.1:8545/')
+  console.log(
+    `Starting with owner=${owner.address} and trader=${trader.address}`,
+  );
 
-const router = new Contract(
-    ROUTER_ADDRESS,
+  console.log("Deploying USDT...");
+  const Usdt = await ethers.getContractFactory("Tether", owner);
+  const usdt = await Usdt.deploy();
+  console.log("USDT deployed at:", usdt.address);
+  console.log("Deploying USDC...");
+  const Usdc = await ethers.getContractFactory("UsdCoin", owner);
+  const usdc = await Usdc.deploy();
+  console.log("USDC deployed at:", usdc.address);
+  console.log("Deploying WETH...");
+  const Weth = new ContractFactory(WETH9.abi, WETH9.bytecode, owner);
+  const weth = await Weth.deploy();
+  console.log("WETH deployed at:", weth.address);
+
+  console.log("Minting some tokens...");
+  const mintAmount = utils.parseEther("100000");
+  await usdt.connect(owner).mint(owner.address, mintAmount);
+  console.log("Minted USDT for owner");
+  await usdc.connect(owner).mint(owner.address, mintAmount);
+  console.log("Minted USDC for owner");
+  await usdt.connect(owner).mint(trader.address, mintAmount);
+  console.log("Minted USDT for trader");
+  await usdc.connect(owner).mint(trader.address, mintAmount);
+  console.log("Minted USDC for trader");
+  console.log("All tokens have been minted");
+
+  console.log("Deploying Uniswap factory...");
+  const Factory = new ContractFactory(
+    factoryArtifact.abi,
+    factoryArtifact.bytecode,
+    owner,
+  );
+  const factory = await Factory.deploy(owner.address);
+  console.log("Factory deployed at:", factory.address);
+
+  console.log("Creating USDT/USDC pair...");
+  const tx = await factory.createPair(usdt.address, usdc.address);
+  await tx.wait();
+  console.log("USDT/USDC pair created");
+
+  console.log("Deploying Uniswap router...");
+  const Router = new ContractFactory(
     routerArtifact.abi,
-    provider
-)
+    routerArtifact.bytecode,
+    owner,
+  );
+  const router = await Router.deploy(factory.address, weth.address);
+  console.log("Router deployed at:", router.address);
 
-const usdt = new Contract(
-    USDT_ADDRESS,
-    usdtArtifact.abi,
-    provider
-)
+  const approvalUsdtOwnerA = await usdt
+    .connect(owner)
+    .approve(router.address, constants.MaxUint256);
+  await approvalUsdtOwnerA.wait();
+  const approvalUsdcOwnerA = await usdc
+    .connect(owner)
+    .approve(router.address, constants.MaxUint256);
+  await approvalUsdcOwnerA.wait();
+  const approvalUsdtTraderA = await usdt
+    .connect(trader)
+    .approve(router.address, constants.MaxUint256);
+  await approvalUsdtTraderA.wait();
+  const approvalUsdcTraderA = await usdc
+    .connect(trader)
+    .approve(router.address, constants.MaxUint256);
+  await approvalUsdcTraderA.wait();
 
-const usdc = new Contract(
-    USDC_ADDRESS,
-    usdcArtifact.abi,
-    provider
-)
+  console.log("Adding liquidity...");
+  const addLiquidityTx = await router
+    .connect(owner)
+    .addLiquidity(
+      usdt.address,
+      usdc.address,
+      utils.parseEther("100"),
+      utils.parseEther("100"),
+      0,
+      0,
+      owner.address,
+      Math.floor(Date.now() / 1000 + 10 * 60),
+      { gasLimit: utils.hexlify(1_000_000) },
+    );
+  addLiquidityTx.wait();
+  console.log("Liquidity added");
 
-const logBalance = async (signerObj) => {
-    let ethBalance
-    let usdtBalance
-    let usdcBalance
-    let balances
-    ethBalance = await signerObj.getBalance()
-    usdtBalance = await usdt.balanceOf(signerObj.address)
-    usdcBalance = await usdc.balanceOf(signerObj.address)
-    balances = {
-        ethBalance: ethBalance,
-        usdtBalance: usdtBalance,
-        usdcBalance: usdcBalance,
-    }
-    console.log('balances', balances)
+  const usdtUsdcPairAddress = await factory.getPair(usdt.address, usdc.address);
+  const usdtUsdcPair = new Contract(
+    usdtUsdcPairAddress,
+    pairArtifact.abi,
+    owner,
+  );
+  let reserves = await usdtUsdcPair.getReserves();
+  console.log("reserves USDT/USDC", reserves);
 
+  let addresses = [
+    `USDT_ADDRESS=${usdt.address}`,
+    `USDC_ADDRESS=${usdc.address}`,
+    `WETH_ADDRESS=${weth.address}`,
+    `FACTORY_ADDRESS=${factory.address}`,
+    `PAIR_ADDRESS=${usdtUsdcPairAddress}`,
+    `ROUTER_ADDRESS=${router.address}`,
+  ];
+
+  console.log("addresses:", addresses);
+
+  const data = addresses.join("\n");
+  const writeFile = promisify(fs.writeFile);
+  const filePath = ".env";
+
+  return writeFile(filePath, data)
+    .then(() => {
+      console.log("Addresses recorded.");
+    })
+    .catch((error) => {
+      console.error("Error logging addresses:", error);
+      throw error;
+    });
 }
-
-const main = async () => {
-    const [owner, trader] = await ethers.getSigners()
-
-    await logBalance(trader)
-
-    const tx = await router.connect(trader).swapExactTokensForTokens(
-        utils.parseUnits('2', 18),
-        utils.parseUnits('1', 18),
-        [USDT_ADDRESS, USDC_ADDRESS],
-        trader.address,
-        Math.floor(Date.now() / 1000) + (60 * 10),
-        {
-            gasLimit: 1000000,
-        }
-    )
-
-    await tx.wait()
-    await logBalance(trader)
-}
-
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
