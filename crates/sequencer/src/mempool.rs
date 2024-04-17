0829748509a6c3e414214b9561697337ad56d4c5
eyusufatik <esadyusufatik@gmail.com>
2024-04-17 13:39:21 +0300

    maximize pool usage

--- crates/sequencer/src/mempool.rs
+++ crates/sequencer/src/mempool.rs
@@ -1,14 +1,14 @@
 use std::sync::Arc;
 
 use citrea_evm::SYSTEM_SIGNER;
-use reth_primitives::{BaseFeeParamsKind, Chain, ChainSpec, TxHash};
-use reth_tasks::TokioTaskExecutor;
+use reth_primitives::{BaseFeeParamsKind, Chain, ChainSpec, ChainSpecBuilder, Genesis, TxHash};
+use reth_tasks::{pool, TokioTaskExecutor};
 use reth_transaction_pool::blobstore::NoopBlobStore;
 use reth_transaction_pool::error::PoolError;
 use reth_transaction_pool::{
     BestTransactions, BestTransactionsAttributes, CoinbaseTipOrdering, EthPooledTransaction,
-    EthTransactionValidator, Pool, PoolResult, TransactionPool, TransactionValidationTaskExecutor,
-    ValidPoolTransaction,
+    EthTransactionValidator, Pool, PoolConfig, PoolResult, SubPoolLimit, TransactionPool,
+    TransactionValidationTaskExecutor, ValidPoolTransaction,
 };
 
 pub use crate::db_provider::DbProvider;
@@ -26,14 +26,41 @@ pub(crate) struct CitreaMempool<C: sov_modules_api::Context>(CitreaMempoolImpl<C
 impl<C: sov_modules_api::Context> CitreaMempool<C> {
     pub(crate) fn new(client: DbProvider<C>) -> Self {
         let blob_store = NoopBlobStore::default();
-        let genesis_hash = client.genesis_block().unwrap().unwrap().header.hash;
+        let genesis_block = client.genesis_block().unwrap().unwrap();
         let evm_config = client.cfg();
-        let chain_spec = ChainSpec {
-            chain: Chain::from_id(evm_config.chain_id),
-            genesis_hash,
-            base_fee_params: BaseFeeParamsKind::Constant(evm_config.base_fee_params),
-            ..Default::default()
-        };
+
+        // very ugly hotfix
+        let chain_spec = ChainSpecBuilder::default()
+            .chain(Chain::from_id(evm_config.chain_id))
+            .shanghai_activated()
+            .genesis(
+                Genesis::default()
+                    .with_nonce(genesis_block.header.nonce.unwrap().into())
+                    .with_timestamp(genesis_block.header.timestamp.saturating_to())
+                    .with_extra_data(genesis_block.header.extra_data)
+                    .with_gas_limit(genesis_block.header.gas_limit.saturating_to())
+                    .with_difficulty(genesis_block.header.difficulty)
+                    .with_mix_hash(genesis_block.header.mix_hash.unwrap())
+                    .with_coinbase(genesis_block.header.miner)
+                    .with_base_fee(Some(
+                        genesis_block
+                            .header
+                            .base_fee_per_gas
+                            .unwrap()
+                            .saturating_to(),
+                    )),
+            )
+            .build();
+
+        let big_pool_limit = SubPoolLimit::new(10_000_000, 2 * 1024 * 1024 * 1024);
+        let mut pool_config = PoolConfig::default();
+
+        pool_config.pending_limit = big_pool_limit.clone();
+        pool_config.queued_limit = big_pool_limit.clone();
+        pool_config.basefee_limit = big_pool_limit;
+        pool_config.blob_limit = SubPoolLimit::new(0, 0);
+        pool_config.max_account_slots = usize::MAX;
+
         Self(Pool::eth_pool(
             TransactionValidationTaskExecutor::eth(
                 client,
@@ -42,7 +69,7 @@ impl<C: sov_modules_api::Context> CitreaMempool<C> {
                 TokioTaskExecutor::default(),
             ),
             blob_store,
-            Default::default(),
+            pool_config,
         ))
     }
 
