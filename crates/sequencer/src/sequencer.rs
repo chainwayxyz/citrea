6ba789883a7f51d18eb1cb15a8d5a5d2457be08d
Erce Can Bektüre <canerce99@hotmail.com>
2024-04-19 16:45:26 +0300

    Lint

--- crates/sequencer/src/sequencer.rs
+++ crates/sequencer/src/sequencer.rs
@@ -14,7 +14,7 @@ use futures::StreamExt;
 use jsonrpsee::RpcModule;
 use reth_primitives::IntoRecoveredTransaction;
 use reth_provider::BlockReaderIdExt;
-use reth_transaction_pool::{BestTransactionsAttributes, PoolTransaction};
+use reth_transaction_pool::BestTransactionsAttributes;
 use sov_accounts::Accounts;
 use sov_accounts::Response::{AccountEmpty, AccountExists};
 use sov_db::ledger_db::{LedgerDB, SlotCommit};
