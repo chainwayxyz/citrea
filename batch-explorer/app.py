ea21c608c43ab4b64591669f7e31682cd494ec6b
Ömer Talip Akalın <56600661+otaliptus@users.noreply.github.com>
2024-05-13 18:52:50 +0300

    Add commitment count + fix cors on local (#559)

--- batch-explorer/app.py
+++ batch-explorer/app.py
@@ -39,7 +39,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
