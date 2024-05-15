eb7c41ac9bc5b27bca9d3ac38f9448d97bb6367a
Erce Can Bektüre <canerce99@hotmail.com>
2024-05-15 16:16:07 +0300

    Update batch explorer with proofs

--- batch-explorer/dtos/commitment.py
+++ batch-explorer/dtos/commitment.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class CommitmentResponse(BaseModel):
     l1_tx_id: str
-    l1_start_hash : str
+    l1_start_hash: str
     l1_end_hash: str
     l1_start_hash_mempool_url: str
     l1_end_hash_mempool_url: str
@@ -25,3 +25,43 @@ class SequencerCommitment(BaseModel):
     l2_end_height: int
     merkle_root: bytes
     status: str
+
+
+"""
+CREATE TABLE IF NOT EXISTS proof (
+    id                          SERIAL PRIMARY KEY,
+    l1_tx_id                    BYTEA NOT NULL,
+    proof_data                  BYTEA NOT NULL,
+    initial_state_root          BYTEA NOT NULL,
+    final_state_root            BYTEA NOT NULL,
+    state_diff                  BYTEA NOT NULL,
+    da_slot_hash                BYTEA NOT NULL,
+    sequencer_public_key        BYTEA NOT NULL,
+    sequencer_da_public_key     BYTEA NOT NULL,
+    validity_condition          BYTEA NOT NULL
+);
+"""
+
+
+class ProofData(BaseModel):
+    l1_tx_id: bytes
+    proof_data: bytes
+    initial_state_root: bytes
+    final_state_root: bytes
+    state_diff: bytes
+    da_slot_hash: bytes
+    sequencer_public_key: bytes
+    sequencer_da_public_key: bytes
+    validity_condition: bytes
+
+
+class ProofDataResponse(BaseModel):
+    l1_tx_id: str
+    proof_data: str
+    initial_state_root: str
+    final_state_root: str
+    state_diff: str
+    da_slot_hash: str
+    sequencer_public_key: str
+    sequencer_da_public_key: str
+    validity_condition: str
--- batch-explorer/utils/deserializers.py
+++ batch-explorer/utils/deserializers.py
@@ -1,5 +1,14 @@
-from dtos.commitment import SequencerCommitment, CommitmentResponse
+from dtos.commitment import (
+    SequencerCommitment,
+    CommitmentResponse,
+    ProofData,
+    ProofDataResponse,
+)
 from config import CONFIG
+
+
+def bytes_to_hex(bytes_):
+    return str(bytes_)[2:-1]
 
 
 def deserialize_commitments(rows):
@@ -24,19 +33,58 @@ def deserialize_commitments(rows):
 def deserialize_to_commitment_response(rows):
     responses = []
     for row in rows:
+        decoded = row[3].decode("utf-8")
         responses.append(
             CommitmentResponse(
-                l1_tx_id=f"{CONFIG.mempool_space_url}/tx/{row[3].decode("utf-8")}",
-                l1_start_hash=row[4].hex(),
-                l1_end_hash=row[5].hex(),
-                l1_start_hash_mempool_url=f"{CONFIG.mempool_space_url}/block/{row[4].hex()}",
-                l1_end_hash_mempool_url=f"{CONFIG.mempool_space_url}/block/{row[5].hex()}",
+                l1_tx_id=f"{CONFIG.mempool_space_url}/tx/{decoded}",
+                l1_start_hash=bytes_to_hex(row[4]),
+                l1_end_hash=bytes_to_hex(row[5]),
+                l1_start_hash_mempool_url=f"{CONFIG.mempool_space_url}/block/{bytes_to_hex(row[4])}",
+                l1_end_hash_mempool_url=f"{CONFIG.mempool_space_url}/block/{bytes_to_hex(row[5])}",
                 l2_start_height=row[6],
                 l2_end_height=row[7],
                 l2_start_height_block_exp_url=f"{CONFIG.blockscout_url}/block/{row[6]}",
                 l2_end_height_block_exp_url=f"{CONFIG.blockscout_url}/block/{row[7]}",
-                merkle_root=row[8].hex(),
+                merkle_root=bytes_to_hex(row[8]),
                 status=row[9],
             )
         )
     return responses
+
+
+def deserialize_proof_data(rows):
+    proofs = []
+    for row in rows:
+        proofs.append(
+            ProofData(
+                l1_tx_id=row[1],
+                proof_data=row[2],
+                initial_state_root=row[3],
+                final_state_root=row[4],
+                state_diff=row[5],
+                da_slot_hash=row[6],
+                sequencer_public_key=row[7],
+                sequencer_da_public_key=row[8],
+                validity_condition=row[9],
+            )
+        )
+    return proofs
+
+
+def deserialize_to_proof_data_response(rows):
+    proofs = []
+    for row in rows:
+        proofs.append(
+            ProofDataResponse(
+                l1_tx_id=bytes_to_hex(row[1]),
+                proof_data=bytes_to_hex(row[2]),
+                initial_state_root=bytes_to_hex(row[3]),
+                final_state_root=bytes_to_hex(row[4]),
+                state_diff=bytes_to_hex(row[5]),
+                da_slot_hash=bytes_to_hex(row[6]),
+                sequencer_public_key=bytes_to_hex(row[7]),
+                sequencer_da_public_key=bytes_to_hex(row[8]),
+                validity_condition=bytes_to_hex(row[9]),
+            )
+        )
+    return proofs
