092dd652c61709dc5b644147f672a4230a887294
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-05-16 09:23:08 +0300

    Fix lints

--- crates/shared-backup-db/src/postgres_connector.rs
+++ crates/shared-backup-db/src/postgres_connector.rs
@@ -47,7 +47,7 @@ impl PostgresConnector {
                 .await;
 
             //connect to new db
-            cfg.dbname(&db_name);
+            cfg.dbname(db_name);
             let mgr = Manager::from_config(cfg, NoTls, mgr_config);
             pool = Pool::builder(mgr)
                 .max_size(pg_config.max_pool_size().unwrap_or(16))
