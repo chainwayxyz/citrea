1db447af5b39cc7a9a6bbbdb4227bd75d922b5d8
eyusufatik <esadyusufatik@gmail.com>
2024-05-18 11:19:38 +0300

    JSON_LOGS environment variable to print logs in json

--- bin/citrea/src/lib.rs
+++ bin/citrea/src/lib.rs
@@ -17,13 +17,25 @@ pub use bitcoin_rollup::*;
 
 /// Default initialization of logging
 pub fn initialize_logging() {
-    tracing_subscriber::registry()
-        .with(fmt::layer())
-        .with(
-            EnvFilter::from_str(&env::var("RUST_LOG").unwrap_or_else(|_| {
-                "debug,hyper=info,risc0_zkvm=info,guest_execution=debug".to_string()
-            }))
-            .unwrap(),
-        )
-        .init();
+    if std::env::var("JSON_LOGS").is_ok() {
+        tracing_subscriber::registry()
+            .with(fmt::layer().json())
+            .with(
+                EnvFilter::from_str(&env::var("RUST_LOG").unwrap_or_else(|_| {
+                    "debug,hyper=info,risc0_zkvm=info,guest_execution=debug".to_string()
+                }))
+                .unwrap(),
+            )
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(fmt::layer())
+            .with(
+                EnvFilter::from_str(&env::var("RUST_LOG").unwrap_or_else(|_| {
+                    "debug,hyper=info,risc0_zkvm=info,guest_execution=debug".to_string()
+                }))
+                .unwrap(),
+            )
+            .init();
+    }
 }
