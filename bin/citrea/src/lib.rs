0b7b52c81856a78845da18c492309d7f7cd76ef9
eyusufatik <esadyusufatik@gmail.com>
2024-05-18 14:36:55 +0300

    review fixes

--- bin/citrea/src/lib.rs
+++ bin/citrea/src/lib.rs
@@ -17,25 +17,20 @@ pub use bitcoin_rollup::*;
 
 /// Default initialization of logging
 pub fn initialize_logging() {
+    let env_filter =
+        EnvFilter::from_str(&env::var("RUST_LOG").unwrap_or_else(|_| {
+            "debug,hyper=info,risc0_zkvm=info,guest_execution=debug".to_string()
+        }))
+        .unwrap();
     if std::env::var("JSON_LOGS").is_ok() {
         tracing_subscriber::registry()
             .with(fmt::layer().json())
-            .with(
-                EnvFilter::from_str(&env::var("RUST_LOG").unwrap_or_else(|_| {
-                    "debug,hyper=info,risc0_zkvm=info,guest_execution=debug".to_string()
-                }))
-                .unwrap(),
-            )
+            .with(env_filter)
             .init();
     } else {
         tracing_subscriber::registry()
             .with(fmt::layer())
-            .with(
-                EnvFilter::from_str(&env::var("RUST_LOG").unwrap_or_else(|_| {
-                    "debug,hyper=info,risc0_zkvm=info,guest_execution=debug".to_string()
-                }))
-                .unwrap(),
-            )
+            .with(env_filter)
             .init();
     }
 }
