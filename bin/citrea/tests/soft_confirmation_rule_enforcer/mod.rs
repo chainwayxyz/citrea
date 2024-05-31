3bcc0fdc7e882eacce0b3a6b65daa043975b25a3
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-05-31 17:06:21 +0300

    WIP

--- bin/citrea/tests/soft_confirmation_rule_enforcer/mod.rs
+++ bin/citrea/tests/soft_confirmation_rule_enforcer/mod.rs
@@ -3,7 +3,7 @@ use sov_mock_da::{MockAddress, MockDaService};
 
 use crate::evm::make_test_client;
 // use citrea::initialize_logging;
-use crate::test_helpers::{start_rollup, tempdir_with_children, NodeMode};
+use crate::test_helpers::{start_rollup, tempdir_with_children, wait_for_l2_block, NodeMode};
 use crate::{DEFAULT_DEPOSIT_MEMPOOL_FETCH_LIMIT, DEFAULT_MIN_SOFT_CONFIRMATIONS_PER_COMMITMENT};
 
 /// Transaction with equal nonce to last tx should not be accepted by mempool.
@@ -52,6 +52,7 @@ async fn too_many_l2_block_per_l1_block() {
         if idx >= limiting_number {
             // There should not be any more blocks published from this point
             // because the limiting number is reached
+            wait_for_l2_block(&test_client, 10, None).await;
             assert_eq!(test_client.eth_block_number().await, 10);
         }
     }
@@ -62,12 +63,14 @@ async fn too_many_l2_block_per_l1_block() {
     for idx in 0..2 * limiting_number + 1 {
         test_client.send_publish_batch_request().await;
         if idx < limiting_number {
+            wait_for_l2_block(&test_client, last_block_number + 1, None).await;
             assert_eq!(test_client.eth_block_number().await, last_block_number + 1);
         }
         last_block_number += 1;
         if idx >= limiting_number {
             // There should not be any more blocks published from this point
             // because the limiting number is reached again
+            wait_for_l2_block(&test_client, 20, None).await;
             assert_eq!(test_client.eth_block_number().await, 20);
         }
     }
