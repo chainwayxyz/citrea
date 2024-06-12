1a587234124151a4e8a99286595517b2a578d69f
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-06-12 21:43:01 +0300

    Get rid of test_data_path() & make_contract_from_abi()

--- crates/evm/src/smart_contracts/mod.rs
+++ crates/evm/src/smart_contracts/mod.rs
@@ -1,5 +1,4 @@
 //! Includes the smart contracts used by the citrea-evm and the rollup itself, extensively for testing.
-use std::path::PathBuf;
 
 mod blockhash_contract;
 mod caller_contract;
@@ -14,8 +13,6 @@ mod simple_storage_contract;
 pub use blockhash_contract::BlockHashContract;
 pub use caller_contract::CallerContract;
 pub use coinbase_contract::CoinbaseContract;
-use ethers_contract::BaseContract;
-use ethers_core::abi::Abi;
 use ethers_core::types::Bytes;
 pub use hive_contract::HiveContract;
 pub use infinite_loop_contract::InfiniteLoopContract;
@@ -23,20 +20,6 @@ pub use logs_contract::LogsContract;
 pub use payable_contract::SimplePayableContract;
 pub use self_destructor_contract::SelfDestructorContract;
 pub use simple_storage_contract::SimpleStorageContract;
-
-fn test_data_path() -> PathBuf {
-    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("src");
-    path.push("evm");
-    path.push("test_data");
-    path
-}
-
-fn make_contract_from_abi(path: PathBuf) -> BaseContract {
-    let abi_json = std::fs::read_to_string(path).unwrap();
-    let abi: Abi = serde_json::from_str(&abi_json).unwrap();
-    BaseContract::from(abi)
-}
 
 /// Trait for testing smart contracts.
 pub trait TestContract: Default {
