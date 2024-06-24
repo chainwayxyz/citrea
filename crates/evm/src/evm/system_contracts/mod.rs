e68a5afd61d0d4ce55b8056b69ccc36e8a6741fe
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-06-24 19:00:10 +0400

    Revert BitcoinLightClient abi calls

--- crates/evm/src/evm/system_contracts/mod.rs
+++ crates/evm/src/evm/system_contracts/mod.rs
@@ -19,20 +19,20 @@ impl BitcoinLightClient {
     }
 
     pub(crate) fn init(block_number: u64) -> Bytes {
-        BitcoinLightClientContract::initializeBlockNumberCall {
-            _blockNumber: U256::from(block_number),
-        }
-        .abi_encode()
-        .into()
+        let block_number = U256::from(block_number);
+
+        let mut func_selector = Vec::with_capacity(4 + 32);
+        func_selector.extend(BitcoinLightClientContract::initializeBlockNumberCall::SELECTOR);
+        func_selector.extend_from_slice(&block_number.to_be_bytes::<32>());
+        func_selector.into()
     }
 
     pub(crate) fn set_block_info(block_hash: [u8; 32], txs_commitments: [u8; 32]) -> Bytes {
-        BitcoinLightClientContract::setBlockInfoCall {
-            _blockHash: block_hash.into(),
-            _witnessRoot: txs_commitments.into(),
-        }
-        .abi_encode()
-        .into()
+        let mut func_selector = Vec::with_capacity(4 + 32 + 32);
+        func_selector.extend(BitcoinLightClientContract::setBlockInfoCall::SELECTOR);
+        func_selector.extend_from_slice(&block_hash);
+        func_selector.extend_from_slice(&txs_commitments);
+        func_selector.into()
     }
 
     /// Return input data to query the block hash by block number mapping
@@ -74,7 +74,7 @@ impl Bridge {
     pub(crate) fn initialize() -> Bytes {
         // Hardcoded until better times.
 
-        // params equal to:
+        // This is equal to:
         //
         // BridgeContract::initializeCall {
         //     _depositScript: hex!("d2205daf577048c5e5a9a75d0a924ed03e226c3304f4a2f01c65ca1dab73522e6b8bad206228eba653cf1819bcfc1bc858630e5ae373eec1a9924322a5fe8445c5e76027ad201521d65f64be3f71b71ca462220f13c77b251027f6ca443a483353a96fbce222ad200fabeed269694ee83d9b3343a571202e68af65d05feda61dbed0c4bdb256a6eaad2000326d6f721c03dc5f1d8817d8f8ee890a95a2eeda0d4d9a01b1cc9b7b1b724dac00630663697472656114").into(),
