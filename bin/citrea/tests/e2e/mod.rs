8a369c89fce8c6e40a0dc7d05dda8918e9dd27a0
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-06-25 17:51:44 +0300

    Change second block base fee

--- bin/citrea/tests/e2e/mod.rs
+++ bin/citrea/tests/e2e/mod.rs
@@ -2095,7 +2095,7 @@ async fn transaction_failing_on_l1_is_removed_from_mempool() -> Result<(), anyho
 
     let random_wallet_address = random_wallet.address();
 
-    let second_block_base_fee = 768881663;
+    let second_block_base_fee = 768595319;
 
     let _pending = seq_test_client
         .send_eth(
