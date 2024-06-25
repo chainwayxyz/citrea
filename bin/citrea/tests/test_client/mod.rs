b4d8001f7e5a21aafda22c591b70b320e3ebef58
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-06-25 16:25:32 +0300

    Remove redundant dead code marker

--- bin/citrea/tests/test_client/mod.rs
+++ bin/citrea/tests/test_client/mod.rs
@@ -664,7 +664,6 @@ impl TestClient {
 #[allow(dead_code)]
 // ethers version of FeeHistory doesn't accept None reward
 pub struct FeeHistory {
-    #[allow(dead_code)]
     pub base_fee_per_gas: Vec<U256>,
     pub gas_used_ratio: Vec<f64>,
     pub oldest_block: U256,
