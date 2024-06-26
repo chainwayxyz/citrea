a0d8263f7d2bee159c0878a54bb71a1cb43fd0aa
Rakan Al-Huneiti <rakan.alhneiti@gmail.com>
2024-06-26 11:59:54 +0300

    Reorganize repo (#779)
    * Move to resources
    
    * Move hive
    
    * Update config paths
    
    * Move genesis files
    
    * Update Hive build
    
    * Rename constants crate to primitives
    
    * Update storage & DA db path
    
    * Update integration tests genesis path
    
    * Fix tests
    
    * Fix clean-node
    
    * Expect instead of unwrap for start_rollup
    
    * Move genesis files
    
    * Fix genesis paths
    
    * Fix path of integration test files
    
    * Fix constant import
    
    * Create an empty resources/dbs directory

--- bin/citrea/provers/risc0/guest-bitcoin/src/bin/bitcoin_da.rs
+++ bin/citrea/provers/risc0/guest-bitcoin/src/bin/bitcoin_da.rs
@@ -2,11 +2,11 @@
 use bitcoin_da::spec::RollupParams;
 use bitcoin_da::verifier::BitcoinVerifier;
 
+use citrea_primitives::{DA_TX_ID_LEADING_ZEROS, ROLLUP_NAME};
 use citrea_stf::runtime::Runtime;
 use citrea_stf::StfVerifier;
 #[cfg(feature = "bench")]
 use risc0_zkvm::guest::env;
-use rollup_constants::{DA_TX_ID_LEADING_ZEROS, ROLLUP_NAME};
 use sov_modules_api::default_context::ZkDefaultContext;
 use sov_modules_stf_blueprint::StfBlueprint;
 use sov_risc0_adapter::guest::Risc0Guest;
