d15f1b763e4dd487ab2c9749e1c62521d3a9bdca
yaziciahmet <yaziciahmet991@gmail.com>
2024-07-02 13:18:30 +0200

    Add common/rpc crate

---
+++ crates/common/src/rpc/mod.rs
@@ -0,0 +1,91 @@
+//! Common RPC crate provides helper methods that are needed in rpc servers
+use std::time::Duration;
+
+use hyper::Method;
+use jsonrpsee::core::RegisterMethodError;
+use jsonrpsee::server::middleware::http::ProxyGetRequestLayer;
+use jsonrpsee::types::error::{INTERNAL_ERROR_CODE, INTERNAL_ERROR_MSG};
+use jsonrpsee::types::ErrorObjectOwned;
+use jsonrpsee::RpcModule;
+use sov_db::ledger_db::LedgerDB;
+use sov_db::schema::types::BatchNumber;
+use tower_http::cors::{Any, CorsLayer};
+
+/// Register the healthcheck rpc
+pub fn register_healthcheck_rpc<T: Send + Sync + 'static>(
+    mut rpc_methods: RpcModule<T>,
+    ledger_db: Option<LedgerDB>,
+) -> Result<RpcModule<T>, RegisterMethodError> {
+    if let Some(ledger_db) = ledger_db {
+        rpc_methods.register_async_method("health_check", move |_, _| {
+            let ledger_db = ledger_db.clone();
+            async move {
+                let head_batch = ledger_db.get_head_soft_batch().map_err(|err| {
+                    ErrorObjectOwned::owned(
+                        INTERNAL_ERROR_CODE,
+                        INTERNAL_ERROR_MSG,
+                        Some(format!("Failed to get head soft batch: {}", err)),
+                    )
+                })?;
+                let head_batch_num: u64 = match head_batch {
+                    Some((i, _)) => i.into(),
+                    None => return Ok::<(), ErrorObjectOwned>(()),
+                };
+                if head_batch_num < 1 {
+                    return Ok::<(), ErrorObjectOwned>(());
+                }
+
+                let soft_batches = ledger_db
+                    .get_soft_batch_range(
+                        &(BatchNumber(head_batch_num - 1)..BatchNumber(head_batch_num + 1)),
+                    )
+                    .map_err(|err| {
+                        ErrorObjectOwned::owned(
+                            INTERNAL_ERROR_CODE,
+                            INTERNAL_ERROR_MSG,
+                            Some(format!("Failed to get soft batch range: {}", err)),
+                        )
+                    })?;
+                let block_time_s = soft_batches[1].timestamp - soft_batches[0].timestamp;
+                tokio::time::sleep(Duration::from_millis(block_time_s * 1500)).await;
+
+                let (new_head_batch_num, _) = ledger_db
+                    .get_head_soft_batch()
+                    .map_err(|err| {
+                        ErrorObjectOwned::owned(
+                            INTERNAL_ERROR_CODE,
+                            INTERNAL_ERROR_MSG,
+                            Some(format!("Failed to get head soft batch: {}", err)),
+                        )
+                    })?
+                    .unwrap();
+                if new_head_batch_num > BatchNumber(head_batch_num) {
+                    Ok::<(), ErrorObjectOwned>(())
+                } else {
+                    Err(ErrorObjectOwned::owned(
+                        INTERNAL_ERROR_CODE,
+                        INTERNAL_ERROR_MSG,
+                        Some("Block number is not increasing"),
+                    ))
+                }
+            }
+        })?;
+    } else {
+        rpc_methods.register_method("health_check", |_, _| Ok::<(), ErrorObjectOwned>(()))?;
+    }
+
+    Ok(rpc_methods)
+}
+
+/// Returns health check proxy layer to be used as http middleware
+pub fn get_healthcheck_proxy_layer() -> ProxyGetRequestLayer {
+    ProxyGetRequestLayer::new("/health", "health_check").unwrap()
+}
+
+/// Returns cors layer to be used as http middleware
+pub fn get_cors_layer() -> CorsLayer {
+    CorsLayer::new()
+        .allow_methods([Method::POST, Method::OPTIONS])
+        .allow_origin(Any)
+        .allow_headers(Any)
+}
