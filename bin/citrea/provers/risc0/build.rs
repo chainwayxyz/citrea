f43cae14ec8008623a8dfdfee4ad30fff96e99a5
Roman <r.proskuryakoff@gmail.com>
2024-07-03 13:08:15 +0400

    REPR_GUEST_BUILD env (#843)
    * Introduce env REPR_GUEST_BUILD instead of GUEST_BUILD_NO_DOCKER
    
    * Fix github actions config

--- bin/citrea/provers/risc0/build.rs
+++ bin/citrea/provers/risc0/build.rs
@@ -4,11 +4,11 @@ use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
 
 fn main() {
     println!("cargo:rerun-if-env-changed=SKIP_GUEST_BUILD");
-    println!("cargo:rerun-if-env-changed=GUEST_BUILD_NO_DOCKER");
+    println!("cargo:rerun-if-env-changed=REPR_GUEST_BUILD");
     println!("cargo:rerun-if-env-changed=OUT_DIR");
 
     if std::env::var("SKIP_GUEST_BUILD").is_ok() {
-        println!("Skipping guest build for CI run");
+        println!("cargo:warning=Skipping guest build");
         let out_dir = std::env::var_os("OUT_DIR").unwrap();
         let out_dir = std::path::Path::new(&out_dir);
         let methods_path = out_dir.join("methods.rs");
@@ -34,15 +34,15 @@ fn get_guest_options() -> HashMap<&'static str, risc0_build::GuestOptions> {
     if cfg!(feature = "bench") {
         features.push("bench".to_string());
     }
-    let use_docker = if std::env::var("GUEST_BUILD_NO_DOCKER").is_ok() {
-        println!("Skipping guest build for CI run");
-        None
-    } else {
+    let use_docker = if std::env::var("REPR_GUEST_BUILD").is_ok() {
         let this_package_dir = std::env!("CARGO_MANIFEST_DIR");
         let root_dir = format!("{this_package_dir}/../../../../");
         Some(DockerOptions {
             root_dir: Some(root_dir.into()),
         })
+    } else {
+        println!("cargo:warning=Guest code is not built in docker");
+        None
     };
 
     guest_pkg_to_options.insert(
