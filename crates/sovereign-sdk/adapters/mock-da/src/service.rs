b60a8e74b94bd58a68bc2b3b5c3b53f836ddea14
eyusufatik <esadyusufatik@gmail.com>
2024-07-04 15:13:43 +0300

    reorganize mock-da to pass check-features

--- crates/sovereign-sdk/adapters/mock-da/src/service.rs
+++ crates/sovereign-sdk/adapters/mock-da/src/service.rs
@@ -13,7 +13,7 @@ use sov_rollup_interface::services::da::{BlobWithNotifier, DaService, SlotData};
 use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
 use tokio::sync::{broadcast, Mutex as AsyncMutex, MutexGuard as AsyncMutexGuard};
 use tokio::time;
-use tracing::{instrument, Instrument};
+use tracing::instrument::Instrument;
 
 use crate::db_connector::DbConnector;
 use crate::types::{MockAddress, MockBlob, MockBlock, MockDaVerifier};
@@ -84,7 +84,7 @@ impl MockDaService {
     }
 
     /// Create a new [`MockDaService`] with given finality.
-    #[instrument(name = "MockDA")]
+    #[tracing::instrument(name = "MockDA")]
     pub fn with_finality(
         sequencer_da_address: MockAddress,
         blocks_to_finality: u32,
