1591805fefd30f95f1afdbd75aea4a986154c873
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-07-09 12:21:42 +0300

    Test fixes

--- bin/citrea/tests/e2e/mod.rs
+++ bin/citrea/tests/e2e/mod.rs
@@ -2099,7 +2099,7 @@ async fn transaction_failing_on_l1_is_removed_from_mempool() -> Result<(), anyho
 
     let random_wallet_address = random_wallet.address();
 
-    let second_block_base_fee = 768592592;
+    let second_block_base_fee = 768641461;
 
     let _pending = seq_test_client
         .send_eth(
