4c78cd7ba2a84b67a8cd27d98d3a45c4b8fbd7f6
yaziciahmet <yaziciahmet991@gmail.com>
2024-07-10 01:05:00 +0200

    Use HexTx struct

--- crates/sovereign-sdk/rollup-interface/src/node/rpc/mod.rs
+++ crates/sovereign-sdk/rollup-interface/src/node/rpc/mod.rs
@@ -410,11 +410,8 @@ pub struct TxResponse<Tx> {
     /// The range of events occurring in this transaction.
     pub event_range: core::ops::Range<u64>,
     /// The transaction body, if stored by the rollup.
-    #[serde(
-        skip_serializing_if = "Option::is_none",
-        with = "utils::rpc_optional_hex"
-    )]
-    pub body: Option<Vec<u8>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub body: Option<HexTx>,
     /// The custom receipt specified by the rollup. This typically contains
     /// information about the outcome of the transaction.
     #[serde(skip_serializing)]
@@ -696,83 +693,6 @@ pub mod utils {
             }
 
             deserializer.deserialize_str(HexStrVisitor(PhantomData))
-        }
-    }
-
-    /// Serialization and deserialization logic for `0x`-prefixed hex strings that are wrapped with Option.
-    pub mod rpc_optional_hex {
-        use core::fmt;
-        use core::marker::PhantomData;
-
-        use hex::{FromHex, ToHex};
-        use serde::de::{self, Visitor};
-        use serde::{Deserialize, Deserializer, Serializer};
-
-        use crate::maybestd::format;
-        use crate::maybestd::string::String;
-
-        /// Serializes `data` as hex string using lowercase characters and prefixing with '0x' if Some, else null.
-        ///
-        /// Lowercase characters are used (e.g. `f9b4ca`). The resulting string's length
-        /// is always even, each byte in data is always encoded using two hex digits.
-        /// Thus, the resulting string contains exactly twice as many bytes as the input
-        /// data.
-        pub fn serialize<T, S>(data: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            T: ToHex,
-            S: Serializer,
-        {
-            match data {
-                Some(data) => {
-                    let hex_string = format!("0x{}", data.encode_hex::<String>());
-                    serializer.serialize_str(&hex_string)
-                }
-                None => serializer.serialize_none(),
-            }
-        }
-
-        /// Deserializes an optional hex string into raw bytes or None.
-        ///
-        /// Both, upper and lower case characters are valid in the input string and can
-        /// even be mixed (e.g. `f9b4ca`, `F9B4CA` and `f9B4Ca` are all valid strings).
-        pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
-        where
-            T: FromHex,
-            <T as FromHex>::Error: fmt::Display,
-            D: Deserializer<'de>,
-        {
-            struct HexStringVisitor<T>(PhantomData<T>);
-
-            impl<'de, T> Visitor<'de> for HexStringVisitor<T>
-            where
-                T: FromHex,
-                <T as FromHex>::Error: fmt::Display,
-            {
-                type Value = Option<T>;
-
-                fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                    formatter.write_str("a hex string or none")
-                }
-
-                fn visit_none<E>(self) -> Result<Self::Value, E>
-                where
-                    E: de::Error,
-                {
-                    Ok(None)
-                }
-
-                fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
-                where
-                    D: Deserializer<'de>,
-                {
-                    let hex_str: String = Deserialize::deserialize(deserializer)?;
-                    FromHex::from_hex(hex_str.trim_start_matches("0x"))
-                        .map(|x| Some(x))
-                        .map_err(de::Error::custom)
-                }
-            }
-
-            deserializer.deserialize_option(HexStringVisitor(PhantomData))
         }
     }
 }
