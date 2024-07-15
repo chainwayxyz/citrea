c64ceaa1a2c0cd3e8766de421d7aeee660a5e060
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-07-15 13:42:11 +0300

    Remove sov-stf-runner

--- crates/sovereign-sdk/full-node/sov-stf-runner/src/lib.rs
+++ crates/sovereign-sdk/full-node/sov-stf-runner/src/lib.rs
@@ -17,16 +17,17 @@ use std::path::Path;
 #[cfg(feature = "native")]
 use anyhow::Context;
 #[cfg(feature = "native")]
-pub use prover_service::*;
-#[cfg(feature = "native")]
-mod runner;
-#[cfg(feature = "native")]
 pub use config::*;
 #[cfg(feature = "native")]
-pub use runner::*;
+pub use prover_service::*;
+use sov_modules_api::{DaSpec, Zkvm};
+use sov_rollup_interface::stf::StateTransitionFunction;
 
 /// Implements the `StateTransitionVerifier` type for checking the validity of a state transition
 pub mod verifier;
+
+type GenesisParams<ST, Vm, Da> = <ST as StateTransitionFunction<Vm, Da>>::GenesisParams;
+type SoftConfirmationHash = [u8; 32];
 
 #[cfg(feature = "native")]
 /// Reads json file.
@@ -41,4 +42,14 @@ pub fn read_json_file<T: serde::de::DeserializeOwned, P: AsRef<Path>>(
         .with_context(|| format!("Failed to parse genesis from {}", path_str))?;
 
     Ok(config)
+}
+
+#[cfg(feature = "native")]
+/// How [`StateTransitionRunner`] is initialized
+pub enum InitVariant<Stf: StateTransitionFunction<Vm, Da>, Vm: Zkvm, Da: DaSpec> {
+    /// From given state root and soft confirmation hash
+    Initialized((Stf::StateRoot, SoftConfirmationHash)),
+    /// From empty state root
+    /// Genesis params for Stf::init
+    Genesis(GenesisParams<Stf, Vm, Da>),
 }
--- crates/sovereign-sdk/full-node/sov-stf-runner/src/prover_service/mod.rs
+++ crates/sovereign-sdk/full-node/sov-stf-runner/src/prover_service/mod.rs
@@ -5,8 +5,11 @@ use serde::{Deserialize, Serialize};
 use sov_modules_api::Zkvm;
 use sov_rollup_interface::da::DaSpec;
 use sov_rollup_interface::services::da::DaService;
-use sov_rollup_interface::zk::{Proof, StateTransitionData};
+use sov_rollup_interface::stf::StateTransitionFunction;
+use sov_rollup_interface::zk::{Proof, StateTransitionData, ZkvmHost};
 use thiserror::Error;
+
+use crate::verifier::StateTransitionVerifier;
 
 /// The possible configurations of the prover.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -113,4 +116,20 @@ pub trait ProverService<Vm: Zkvm> {
         block_header_hash: <<Self::DaService as DaService>::Spec as DaSpec>::SlotHash,
         da_service: &Self::DaService,
     ) -> Result<(<Self::DaService as DaService>::TransactionId, Proof), anyhow::Error>;
+}
+
+/// Represents the possible modes of execution for a zkVM program
+pub enum ProofGenConfig<Stf, Da: DaService, Vm: ZkvmHost>
+where
+    Stf: StateTransitionFunction<Vm::Guest, Da::Spec>,
+{
+    /// Skips proving.
+    Skip,
+    /// The simulator runs the rollup verifier logic without even emulating the zkVM
+    Simulate(StateTransitionVerifier<Stf, Da::Verifier, Vm::Guest>),
+    /// The executor runs the rollup verification logic in the zkVM, but does not actually
+    /// produce a zk proof
+    Execute,
+    /// The prover runs the rollup verification logic in the zkVM and produces a zk proof
+    Prover,
 }
