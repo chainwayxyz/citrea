71982f0ca0a86f4e72c7074a40b8c0f92b54d884
yaziciahmet <yaziciahmet991@gmail.com>
2024-07-15 10:58:35 +0200

    Call resubmit

--- bin/citrea/src/rollup/mod.rs
+++ bin/citrea/src/rollup/mod.rs
@@ -65,7 +65,7 @@ pub trait CitreaRollupBlueprint: RollupBlueprint {
             },
         };
 
-        let seq = CitreaSequencer::new(
+        let mut seq = CitreaSequencer::new(
             da_service,
             prover_storage,
             sequencer_config,
@@ -77,6 +77,7 @@ pub trait CitreaRollupBlueprint: RollupBlueprint {
             rollup_config.rpc,
         )
         .unwrap();
+        seq.resubmit_pending_commitments().await?;
 
         Ok(Sequencer {
             runner: seq,
