34945f56f1d52a433630c11da505633aad64acf7
Roman <r.proskuryakoff@gmail.com>
2024-07-16 11:09:36 +0400

    Simplify code in bincoin DA (#890)

--- crates/bitcoin-da/src/helpers/mod.rs
+++ crates/bitcoin-da/src/helpers/mod.rs
@@ -1,9 +1,9 @@
 // Tags that are used to seperate the different parts of the script
-const ROLLUP_NAME_TAG: &[u8] = &[1];
-const SIGNATURE_TAG: &[u8] = &[2];
-const PUBLICKEY_TAG: &[u8] = &[3];
-const RANDOM_TAG: &[u8] = &[4];
-const BODY_TAG: &[u8] = &[];
+const ROLLUP_NAME_TAG: &[u8; 1] = &[1; 1];
+const SIGNATURE_TAG: &[u8; 1] = &[2; 1];
+const PUBLICKEY_TAG: &[u8; 1] = &[3; 1];
+const RANDOM_TAG: &[u8; 1] = &[4; 1];
+const BODY_TAG: &[u8; 0] = &[];
 
 #[cfg(feature = "native")]
 pub mod builders;
