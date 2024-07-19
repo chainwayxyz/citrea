ae7122a7b0773c22985d92f2df61f069eb37e5e6
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-07-19 12:40:59 +0300

    Owner seperation

--- crates/evm/src/evm/system_contracts/script/GenesisGenerator.s.sol
+++ crates/evm/src/evm/system_contracts/script/GenesisGenerator.s.sol
@@ -51,8 +51,13 @@ contract GenesisGenerator is Script {
     bytes32 MIN_WITHDRAW_SLOT = 0x0000000000000000000000000000000000000000000000000000000000000001;
     uint160 PROXY_IMPL_OFFSET = uint160(0x0100000000000000000000000000000000000000); // uint160(address(proxy)) - uint160(address(impl))
 
-    // Owner of proxy admin
-    address internal owner;
+    // Owner of proxy admin, can update contracts
+    address internal upgradeOwner;
+    // Owner of bridge
+    address internal bridgeOwner;
+    // Owner of fee vaults
+    address internal feeVaultOwner;
+
     address internal feeRecipient;
 
     address[] internal devAddresses = 
@@ -71,7 +76,9 @@ contract GenesisGenerator is Script {
 
 
     function run() public {
-        owner = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        upgradeOwner = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        bridgeOwner = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+        feeVaultOwner = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         feeRecipient = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
         dealBalanceToDevAddrs();
         dealBalanceToBridge();
@@ -82,7 +89,9 @@ contract GenesisGenerator is Script {
     }
 
     function runProd() public {
-        owner = vm.envAddress("OWNER");
+        upgradeOwner = vm.envAddress("UPGRADE_OWNER");
+        bridgeOwner = vm.envAddress("BRIDGE_OWNER");
+        feeVaultOwner = vm.envAddress("FEE_VAULT_OWNER");
         feeRecipient = vm.envAddress("FEE_RECIPIENT");
         string memory defaultPath = "./state/evmProd.json";
         string memory copyPath = vm.envOr("PROD_JSON_PATH", defaultPath);
@@ -108,7 +117,7 @@ contract GenesisGenerator is Script {
     function setProxyAdmin() internal {
         address proxyAdminImpl = address(new ProxyAdmin());
         vm.etch(proxyAdmin, proxyAdminImpl.code);
-        vm.store(proxyAdmin, bytes32(0), bytes32(uint256(uint160(owner))));
+        vm.store(proxyAdmin, bytes32(0), bytes32(uint256(uint160(upgradeOwner))));
         // Remove init proxy impl code from genesis state as it is already copied
         vm.etch(proxyAdminImpl, "");
         vm.resetNonce(proxyAdminImpl);
@@ -137,13 +146,14 @@ contract GenesisGenerator is Script {
         vm.store(namespacedProxy, IMPLEMENTATION_SLOT, bytes32(uint256(uint160(namespacedImpl))));
         vm.store(namespacedProxy, ADMIN_SLOT, bytes32(uint256(uint160(proxyAdmin))));
 
-        // First contract is BitcoinLightClient, and it is the only one that doesn't have an owner, thus everything else's owner slot is set
-        if (index != 1) {
-            vm.store(namespacedProxy, OWNER_SLOT, bytes32(uint256(uint160(owner))));
+        // Set owner for bridge contract
+        if (index == 2) {
+            vm.store(namespacedProxy, OWNER_SLOT, bytes32(uint256(uint160(bridgeOwner))));
         }
 
         // Fee vault contracts have a fee recipient and min withdraw amount
         if ((index >= 3) && (index <= 5)) {
+            vm.store(namespacedProxy, OWNER_SLOT, bytes32(uint256(uint160(feeVaultOwner))));
             vm.store(namespacedProxy, FEE_RECIPIENT_SLOT, bytes32(uint256(uint160(feeRecipient))));
             vm.store(namespacedProxy, MIN_WITHDRAW_SLOT, bytes32(uint256(0.5 ether)));
         } 
