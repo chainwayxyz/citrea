1c3e224cb918dad7bb5c228a51e3c87f5424f6bb
Erce Can Bektüre <canerce99@hotmail.com>
2024-07-19 12:45:47 +0300

    Test ci

--- .github/scripts/check_genesis.sh
+++ .github/scripts/check_genesis.sh
@@ -8,6 +8,11 @@ sleep 1
 # Check if script generates different genesis
 git diff --exit-code ./resources/
 
+if [ $? -ne 0 ]; then
+  echo "Differences found in genesis files."
+  exit 1
+fi
+
 echo "All directories are identical."
 
 exit 0
