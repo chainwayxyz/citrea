62f98dbbe4e903a42a4aa5fe9e897294d33313b2
Erce Can Bektüre <canerce99@hotmail.com>
2024-07-19 10:16:37 +0300

    Test ci

---
+++ .github/scripts/check_genesis.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Paths
+paths=( "./resources/genesis/bitcoin-regtest/evm.json" 
+        "./resources/genesis/mock/evm.json" 
+        "./resources/genesis/mock-dockerized/evm.json" 
+        "./resources/test-data/demo-tests/bitcoin-regtest/evm.json" 
+        "./resources/test-data/demo-tests/mock/evm.json" 
+        "./resources/test-data/integration-tests/evm.json" 
+        "./resources/test-data/integration-tests-low-block-gas-limit/evm.json" 
+        "./resources/test-data/integration-tests-low-max-l2-blocks-per-l1/evm.json")
+
+# Function to clean up copied directories
+cleanup() {
+    for path in "${paths[@]}"; 
+    do
+        rm -rf "${path}_tmp"
+    done
+}
+cleanup
+
+sleep 1
+
+# Copy directories
+for path in "${paths[@]}"; 
+do
+    cp -r "$path" "${path}_tmp"
+done
+
+sleep 5
+
+# Run make command
+make genesis
+
+# Compare directories
+for path in "${paths[@]}"; 
+do
+    diff -r "$path" "${path}_tmp"
+    if [ $? -ne 0 ]; then
+        echo "Error: Differences found between $path and ${path}_tmp"
+        cleanup
+        exit 1
+    fi
+done
+
+echo "All directories are identical."
+sleep 1
+
+# Clean up copied directories
+cleanup
+
+exit 0
