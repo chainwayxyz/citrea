1ae585ac7601713d600020cd6cb11180c8348ba7
yaziciahmet <yaziciahmet991@gmail.com>
2024-07-22 19:55:27 +0200

    Finalize handling

--- crates/ethereum-rpc/src/eth_subscription.rs
+++ crates/ethereum-rpc/src/eth_subscription.rs
@@ -1,14 +1,40 @@
 use std::sync::Arc;
 
-use jsonrpsee::types::ParamsSequence;
-use jsonrpsee::PendingSubscriptionSink;
+use citrea_evm::Evm;
+use jsonrpsee::{PendingSubscriptionSink, SubscriptionMessage};
+use reth_rpc_types::BlockNumberOrTag;
+use sov_modules_api::WorkingSet;
 use sov_rollup_interface::services::da::DaService;
 
 use crate::ethereum::Ethereum;
 
 pub async fn handle_new_heads_subscription<C: sov_modules_api::Context, Da: DaService>(
-    _pending: PendingSubscriptionSink,
-    _ethereum: Arc<Ethereum<C, Da>>,
+    pending: PendingSubscriptionSink,
+    ethereum: Arc<Ethereum<C, Da>>,
 ) {
-    unimplemented!()
+    let mut rx = ethereum.soft_commitment_tx.subscribe();
+    let evm = Evm::<C>::default();
+    let subscription = pending.accept().await.unwrap();
+    tokio::spawn(async move {
+        loop {
+            let block_number = rx.recv().await.unwrap();
+            let mut working_set = WorkingSet::<C>::new(ethereum.storage.clone());
+            let block = evm
+                .get_block_by_number(
+                    Some(BlockNumberOrTag::Number(block_number)),
+                    None,
+                    &mut working_set,
+                )
+                .unwrap()
+                .unwrap();
+
+            let msg = SubscriptionMessage::new(
+                subscription.method_name(),
+                subscription.subscription_id(),
+                &block,
+            )
+            .unwrap();
+            let _ = subscription.send(msg).await;
+        }
+    });
 }
