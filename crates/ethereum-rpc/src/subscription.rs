c1ad4475344b23dcfcd06d52447d9ec1526e1db1
yaziciahmet <yaziciahmet991@gmail.com>
2024-07-23 22:27:20 +0200

    IMplement SubscriptionManager

--- crates/ethereum-rpc/src/subscription.rs
+++ crates/ethereum-rpc/src/subscription.rs
@@ -2,11 +2,62 @@ use std::sync::Arc;
 
 use citrea_evm::Evm;
 use jsonrpsee::{PendingSubscriptionSink, SubscriptionMessage};
-use reth_rpc_types::BlockNumberOrTag;
+use reth_rpc_types::{BlockNumberOrTag, RichBlock};
 use sov_modules_api::WorkingSet;
 use sov_rollup_interface::services::da::DaService;
+use tokio::sync::broadcast;
 
 use crate::ethereum::Ethereum;
+
+pub(crate) struct SubscriptionManager<C: sov_modules_api::Context> {
+    storage: C::Storage,
+    new_heads_tx: broadcast::Sender<RichBlock>,
+}
+
+impl<C: sov_modules_api::Context> SubscriptionManager<C> {
+    pub(crate) fn new(storage: C::Storage, soft_confirmation_rx: broadcast::Receiver<u64>) -> Self {
+        let mut soft_confirmation_rx = soft_confirmation_rx;
+        let storage_c = storage.clone();
+        let new_heads_tx = broadcast::channel(16).0;
+        let new_heads_tx_c = new_heads_tx.clone();
+        // let new_heads_tx_c = new_heads_tx.clone();
+        // Spawn the task that will listen for new soft confirmation heights
+        // and send the corresponding ethereum block to subscribers
+        tokio::spawn(async move {
+            let evm = Evm::<C>::default();
+            loop {
+                let Ok(height) = soft_confirmation_rx.recv().await else {
+                    return;
+                };
+
+                if new_heads_tx_c.receiver_count() == 0 {
+                    continue;
+                }
+
+                let mut working_set = WorkingSet::<C>::new(storage_c.clone());
+                let block = evm
+                    .get_block_by_number(
+                        Some(BlockNumberOrTag::Number(height)),
+                        None,
+                        &mut working_set,
+                    )
+                    .expect("Error querying block from evm")
+                    .expect("Received signal but evm block is not found");
+
+                // Only error is no receiver
+                let _ = new_heads_tx_c.send(block);
+            }
+        });
+        Self {
+            storage,
+            new_heads_tx,
+        }
+    }
+
+    pub(crate) fn subscribe_new_heads(&self) -> broadcast::Receiver<RichBlock> {
+        self.new_heads_tx.subscribe()
+    }
+}
 
 pub async fn handle_new_heads_subscription<C: sov_modules_api::Context, Da: DaService>(
     pending: PendingSubscriptionSink,
