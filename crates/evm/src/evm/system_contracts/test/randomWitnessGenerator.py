19128881c81bd1c572dd1f6054e2b03709ad92ec
erdemkan <salih.kocak.0733@gmail.com>
2024-07-25 15:14:32 +0300

    fixed witness lib, added tests

---
+++ crates/evm/src/evm/system_contracts/test/randomWitnessGenerator.py
@@ -0,0 +1,27 @@
+import random
+
+def random_hex_byte(l, r):
+    # Generate a random integer between l and r
+    random_int = random.randint(l, r)
+    # Convert the integer to a hexadecimal string and remove the '0x' prefix
+    hex_value = hex(random_int)[2:]
+    # Ensure the hex value has two digits
+    if len(hex_value) == 1:
+        hex_value = '0' + hex_value
+    return hex_value
+
+witness = ""
+nStackItems = random_hex_byte(0, 8)
+witness += nStackItems
+for k in range(int(nStackItems, 16)):
+    stackItemLength = random_hex_byte(0, 16)
+    witness += stackItemLength
+    for m in range(int(stackItemLength, 16)):
+        randombyte = random_hex_byte(0, 255)
+        witness += randombyte
+print(witness)
+
+
+
+
+
