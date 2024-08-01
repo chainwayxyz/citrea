44082eb9426bb039a27e5c49f77427e31cd9d6b5
Erce Can Bektüre <canerce99@hotmail.com>
2024-08-01 16:39:16 +0300

    Add empty ledger tests with our rpcs

--- crates/sovereign-sdk/full-node/sov-ledger-rpc/src/client.rs
+++ crates/sovereign-sdk/full-node/sov-ledger-rpc/src/client.rs
@@ -3,8 +3,14 @@
 //!
 //! See [`RpcClient`].
 
+use jsonrpsee::core::RpcResult;
 use jsonrpsee::proc_macros::rpc;
-use sov_rollup_interface::rpc::SoftConfirmationStatus;
+use sov_rollup_interface::rpc::{
+    ProofResponse, SequencerCommitmentResponse, SoftConfirmationResponse, SoftConfirmationStatus,
+    VerifiedProofResponse,
+};
+
+use crate::HexHash;
 
 /// A [`jsonrpsee`] trait for interacting with the ledger JSON-RPC API.
 ///
@@ -14,16 +20,73 @@ use sov_rollup_interface::rpc::SoftConfirmationStatus;
 /// For more information about the specific methods, see the
 /// [`sov_rollup_interface::rpc`] module.
 #[rpc(client, namespace = "ledger")]
-pub trait Rpc<Slot, Batch, Tx>
-where
-    Slot: serde::Serialize,
-    Batch: serde::Serialize,
-    Tx: serde::Serialize,
-{
+pub trait Rpc {
+    /// Gets a single soft confirmation by number.
+    #[method(name = "getSoftConfirmationByNumber")]
+    async fn get_soft_confirmation_by_number(
+        &self,
+        number: u64,
+    ) -> RpcResult<Option<SoftConfirmationResponse>>;
+
+    /// Gets a single soft confirmation by hash.
+    #[method(name = "getSoftConfirmationByHash")]
+    async fn get_soft_confirmation_by_hash(
+        &self,
+        hash: HexHash,
+    ) -> RpcResult<Option<SoftConfirmationResponse>>;
+
+    /// Gets all soft confirmations with numbers `range.start` to `range.end`.
+    #[method(name = "getSoftConfirmationRange")]
+    async fn get_soft_confirmation_range(
+        &self,
+        range: (u64, u64),
+    ) -> RpcResult<Vec<Option<SoftConfirmationResponse>>>;
+
     /// Gets a single event by number.
     #[method(name = "getSoftConfirmationStatus")]
     async fn get_soft_confirmation_status(
         &self,
         soft_confirmation_receipt: u64,
     ) -> RpcResult<SoftConfirmationStatus>;
+
+    /// Gets the commitments in the DA slot with the given height.
+    #[method(name = "getSequencerCommitmentsOnSlotByNumber")]
+    async fn get_sequencer_commitments_on_slot_by_number(
+        &self,
+        height: u64,
+    ) -> RpcResult<Option<Vec<SequencerCommitmentResponse>>>;
+
+    /// Gets the commitments in the DA slot with the given hash.
+    #[method(name = "getSequencerCommitmentsOnSlotByHash")]
+    async fn get_sequencer_commitments_on_slot_by_hash(
+        &self,
+        hash: [u8; 32],
+    ) -> RpcResult<Option<Vec<SequencerCommitmentResponse>>>;
+
+    /// Gets proof by slot height.
+    #[method(name = "getProofBySlotHeight")]
+    async fn get_proof_by_slot_height(&self, height: u64) -> RpcResult<Option<ProofResponse>>;
+
+    /// Gets proof by slot hash.
+    #[method(name = "getProofBySlotHash")]
+    async fn get_proof_by_slot_hash(&self, hash: [u8; 32]) -> RpcResult<Option<ProofResponse>>;
+
+    /// Gets the height pf most recent committed soft confirmation.
+    #[method(name = "getHeadSoftConfirmation")]
+    async fn get_head_soft_confirmation(&self) -> RpcResult<Option<SoftConfirmationResponse>>;
+
+    /// Gets the height pf most recent committed soft confirmation.
+    #[method(name = "getHeadSoftConfirmationHeight")]
+    async fn get_head_soft_confirmation_height(&self) -> RpcResult<u64>;
+
+    /// Gets verified proofs by slot height
+    #[method(name = "getVerifiedProofsBySlotHeight")]
+    async fn get_verified_proofs_by_slot_height(
+        &self,
+        height: u64,
+    ) -> RpcResult<Option<Vec<VerifiedProofResponse>>>;
+
+    /// Gets last verified proog
+    #[method(name = "getLastVerifiedProof")]
+    async fn get_last_verified_proof(&self) -> RpcResult<Option<VerifiedProofResponse>>;
 }
---
+++ crates/sovereign-sdk/full-node/sov-ledger-rpc/tests/empty_ledger.rs
@@ -0,0 +1,71 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use sov_db::ledger_db::LedgerDB;
+use sov_ledger_rpc::client::RpcClient;
+use sov_ledger_rpc::server::rpc_module;
+use sov_ledger_rpc::HexHash;
+use tempfile::tempdir;
+
+async fn rpc_server() -> (jsonrpsee::server::ServerHandle, SocketAddr) {
+    let dir = tempdir().unwrap();
+    let db = LedgerDB::with_path(dir).unwrap();
+    let rpc_module = rpc_module::<LedgerDB, u32, u32>(db).unwrap();
+
+    let server = jsonrpsee::server::ServerBuilder::default()
+        .build("127.0.0.1:0")
+        .await
+        .unwrap();
+    let addr = server.local_addr().unwrap();
+    (server.start(rpc_module), addr)
+}
+
+async fn rpc_client(addr: SocketAddr) -> Arc<impl RpcClient> {
+    Arc::new(
+        jsonrpsee::ws_client::WsClientBuilder::new()
+            .build(format!("ws://{}", addr))
+            .await
+            .unwrap(),
+    )
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn getters_succeed() {
+    let (_server_handle, addr) = rpc_server().await;
+    let rpc_client = rpc_client(addr).await;
+    let hash = HexHash([0; 32]);
+    rpc_client
+        .get_soft_confirmation_by_hash(hash)
+        .await
+        .unwrap();
+
+    rpc_client.get_soft_confirmation_by_number(0).await.unwrap();
+
+    rpc_client
+        .get_sequencer_commitments_on_slot_by_number(0)
+        .await
+        .unwrap();
+
+    rpc_client
+        .get_sequencer_commitments_on_slot_by_hash([0; 32])
+        .await
+        .unwrap();
+
+    rpc_client.get_proof_by_slot_height(0).await.unwrap();
+
+    rpc_client.get_proof_by_slot_hash([0; 32]).await.unwrap();
+
+    rpc_client
+        .get_head_soft_confirmation_height()
+        .await
+        .unwrap();
+
+    rpc_client.get_head_soft_confirmation().await.unwrap();
+
+    rpc_client
+        .get_verified_proofs_by_slot_height(0)
+        .await
+        .unwrap();
+
+    rpc_client.get_last_verified_proof().await.unwrap();
+}
