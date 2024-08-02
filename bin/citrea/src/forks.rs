8ef7abb9d05002439295d2521303ceeb98586808
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-08-02 15:25:05 +0300

    Mark FORKS as pub crate

--- bin/citrea/src/forks.rs
+++ bin/citrea/src/forks.rs
@@ -4,7 +4,7 @@ use sov_rollup_interface::spec::SpecId;
 lazy_static! {
     /// This defines the list of forks which will be activated
     /// at specific heights.
-    static ref FORKS: Vec<(SpecId, u64)> = vec![
+    pub(crate) static ref FORKS: Vec<(SpecId, u64)> = vec![
         (SpecId::Genesis, 0),
         (SpecId::Fork1, 100),
         (SpecId::Fork2, 200)
