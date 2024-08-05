6c13bea6d1102aaae04ab46f66919b3e9c1af80b
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-08-05 14:19:20 +0300

    Fix duration underflow issue

--- crates/sequencer/src/sequencer.rs
+++ crates/sequencer/src/sequencer.rs
@@ -922,7 +922,14 @@ where
         let mut missed_da_blocks_count = 0;
 
         loop {
-            let mut interval = tokio::time::interval(target_block_time - parent_block_exec_time);
+            // We try to produce the next block based on how long the previous block took in terms
+            // of execution time. If last block takes more than target block time (2 secs),
+            // we just reset the interval to 2 secs.
+            let mut interval = if parent_block_exec_time > target_block_time {
+                tokio::time::interval(target_block_time)
+            } else {
+                tokio::time::interval(target_block_time - parent_block_exec_time)
+            };
             // The first ticket completes immediately.
             // See: https://docs.rs/tokio/latest/tokio/time/struct.Interval.html#method.tick
             interval.tick().await;
