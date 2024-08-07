3ccd00f90c349c9ddd47275b7f4a096b94a81f98
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-08-07 14:58:38 +0300

    Fix lints

--- crates/prover/tests/prover_tests.rs
+++ crates/prover/tests/prover_tests.rs
@@ -7,7 +7,6 @@ use sov_mock_da::{
 };
 use sov_mock_zkvm::MockZkvm;
 use sov_rollup_interface::da::Time;
-use sov_rollup_interface::spec::SpecId;
 use sov_rollup_interface::zk::StateTransitionData;
 use sov_stf_runner::mock::MockStf;
 use sov_stf_runner::{
