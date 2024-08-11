02d7856464df6c42c468206259d0c1a6d2393d0b
yaziciahmet <yaziciahmetcleargmail.com>
2024-08-11 23:54:32 +0300

    Fix sov-sdk

--- crates/sovereign-sdk/full-node/sov-ledger-rpc/src/server.rs
+++ crates/sovereign-sdk/full-node/sov-ledger-rpc/src/server.rs
@@ -41,7 +41,7 @@ where
 
     rpc.register_async_method(
         "ledger_getSoftConfirmationByHash",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             let args: HexHash = params.one()?;
             ledger
                 .get_soft_confirmation_by_hash::<Tx>(&args.0)
@@ -50,7 +50,7 @@ where
     )?;
     rpc.register_async_method(
         "ledger_getSoftConfirmationByNumber",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             let args: u64 = params.one()?;
             ledger
                 .get_soft_confirmation_by_number::<Tx>(args)
@@ -59,7 +59,7 @@ where
     )?;
     rpc.register_async_method(
         "ledger_getSoftConfirmationRange",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             let args: (u64, u64) = params.parse()?;
             ledger
                 .get_soft_confirmations_range(args.0, args.1)
@@ -68,14 +68,14 @@ where
     )?;
     rpc.register_async_method(
         "ledger_getSoftConfirmationStatus",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             let args: u64 = params.one()?;
             ledger
                 .get_soft_confirmation_status(args)
                 .map_err(|e| to_jsonrpsee_error_object(LEDGER_RPC_ERROR, e))
         },
     )?;
-    rpc.register_async_method("prover_getLastScannedL1Slot", |_, ledger| async move {
+    rpc.register_async_method("prover_getLastScannedL1Slot", |_, ledger, _| async move {
         ledger
             .get_prover_last_scanned_l1_height()
             .map_err(|e| to_jsonrpsee_error_object(LEDGER_RPC_ERROR, e))
@@ -83,7 +83,7 @@ where
 
     rpc.register_async_method(
         "ledger_getSequencerCommitmentsOnSlotByNumber",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             // Returns commitments on DA slot with given height.
             let height: u64 = params.one()?;
 
@@ -95,7 +95,7 @@ where
 
     rpc.register_async_method(
         "ledger_getSequencerCommitmentsOnSlotByHash",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             // Returns commitments on DA slot with given hash.
             let hash: [u8; 32] = params.one()?;
             let height = ledger
@@ -110,7 +110,7 @@ where
         },
     )?;
 
-    rpc.register_async_method("ledger_getProofBySlotHeight", |params, ledger| async move {
+    rpc.register_async_method("ledger_getProofBySlotHeight", |params, ledger, _| async move {
         // Returns proof on DA slot with given height
         let height: u64 = params.one()?;
         ledger
@@ -118,7 +118,7 @@ where
             .map_err(|e| to_jsonrpsee_error_object(LEDGER_RPC_ERROR, e))
     })?;
 
-    rpc.register_async_method("ledger_getProofBySlotHash", |params, ledger| async move {
+    rpc.register_async_method("ledger_getProofBySlotHash", |params, ledger, _| async move {
         // Returns proof on DA slot with given height
         let hash: [u8; 32] = params.one()?;
         let height = ledger
@@ -134,7 +134,7 @@ where
 
     rpc.register_async_method(
         "ledger_getVerifiedProofsBySlotHeight",
-        |params, ledger| async move {
+        |params, ledger, _| async move {
             // Returns proof on DA slot with given height
             let height: u64 = params.one()?;
             ledger
@@ -143,14 +143,14 @@ where
         },
     )?;
 
-    rpc.register_async_method("ledger_getLastVerifiedProof", |_, ledger| async move {
+    rpc.register_async_method("ledger_getLastVerifiedProof", |_, ledger, _| async move {
         // Returns latest proof data
         ledger
             .get_last_verified_proof()
             .map_err(|e| to_jsonrpsee_error_object(LEDGER_RPC_ERROR, e))
     })?;
 
-    rpc.register_async_method("ledger_getHeadSoftConfirmation", |_, ledger| async move {
+    rpc.register_async_method("ledger_getHeadSoftConfirmation", |_, ledger, _| async move {
         ledger
             .get_head_soft_confirmation()
             .map_err(|e: anyhow::Error| to_jsonrpsee_error_object(LEDGER_RPC_ERROR, e))
@@ -158,7 +158,7 @@ where
 
     rpc.register_async_method(
         "ledger_getHeadSoftConfirmationHeight",
-        |_, ledger| async move {
+        |_, ledger, _| async move {
             ledger
                 .get_head_soft_confirmation_height()
                 .map_err(|e| to_jsonrpsee_error_object(LEDGER_RPC_ERROR, e))
