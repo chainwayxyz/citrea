e2ecd771c78b8a51f8efd71e8ede3085fe893b10
yaziciahmet <yaziciahmet991@gmail.com>
2024-08-20 15:59:50 +0300

    Remove unnecessary allow clippy

--- crates/sovereign-sdk/rollup-interface/src/state_machine/zk/mod.rs
+++ crates/sovereign-sdk/rollup-interface/src/state_machine/zk/mod.rs
@@ -53,7 +53,6 @@ pub trait ZkvmHost: Zkvm + Clone {
     fn run(&mut self, with_proof: bool) -> Result<Proof, anyhow::Error>;
 
     /// Extracts public input and receipt from the proof.
-    #[allow(clippy::type_complexity)]
     fn extract_output<Da: DaSpec, Root: BorshDeserialize>(
         proof: &Proof,
     ) -> Result<StateTransition<Da, Root>, Self::Error>;
