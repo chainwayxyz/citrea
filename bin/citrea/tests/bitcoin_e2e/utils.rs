4275136717586a71debc3d9d32f75a26e4ed7c48
jfldde <168934971+jfldde@users.noreply.github.com>
2024-08-29 15:47:29 +0100

    lint

--- bin/citrea/tests/bitcoin_e2e/utils.rs
+++ bin/citrea/tests/bitcoin_e2e/utils.rs
@@ -1,12 +1,14 @@
-use super::Result;
-use anyhow::bail;
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
 use std::future::Future;
 use std::net::TcpListener;
 use std::path::{Path, PathBuf};
 use std::{fs, io};
+
+use anyhow::bail;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use tokio::time::{sleep, Duration, Instant};
+
+use super::Result;
 
 pub fn get_available_port() -> Result<u16> {
     let listener = TcpListener::bind("127.0.0.1:0")?;
