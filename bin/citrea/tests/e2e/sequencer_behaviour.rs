630ff7c25e09df15ec37402a4dc105ee3e1efff6
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-08-30 17:34:06 +0300

    Further node test fixes

--- bin/citrea/tests/e2e/sequencer_behaviour.rs
+++ bin/citrea/tests/e2e/sequencer_behaviour.rs
@@ -338,7 +338,7 @@ async fn test_gas_limit_too_high() {
 
     let target_gas_limit: u64 = 30_000_000;
     let transfer_gas_limit = 21_000;
-    let system_txs_gas_used = 390434;
+    let system_txs_gas_used = 322536;
     let tx_count = (target_gas_limit - system_txs_gas_used).div_ceil(transfer_gas_limit);
     let addr = Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").unwrap();
 
@@ -516,28 +516,28 @@ async fn test_system_tx_effect_on_block_gas_limit() -> Result<(), anyhow::Error>
 
     let seq_port = seq_port_rx.await.unwrap();
     let seq_test_client = make_test_client(seq_port).await;
-    // sys tx use L1BlockHash(50751 + 80720) + Bridge(261215) = 392686 gas
+    // sys tx use L1BlockHash(50751 + 80720) + Bridge(191065) = 322536 gas
     // the block gas limit is 1_500_000 because the system txs gas limit is 1_500_000 (decided with @eyusufatik and @okkothejawa as bridge init takes 1M gas)
 
-    // 1500000 - 392686 = 1107314 gas left in block
-    // 1107314 / 21000 = 52,72... so 52 ether transfer transactions can be included in the block
+    // 1500000 - 322536 = 1177464 gas left in block
+    // 1107314 / 21000 = 56.07... so 56 ether transfer transactions can be included in the block
 
-    // send 52 ether transfer transactions
+    // send 56 ether transfer transactions
     let addr = Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").unwrap();
 
-    for _ in 0..51 {
+    for _ in 0..55 {
         let _pending = seq_test_client
             .send_eth(addr, None, None, None, 0u128)
             .await
             .unwrap();
     }
 
-    // 52th tx should be the last tx in the soft confirmation
+    // 56th tx should be the last tx in the soft confirmation
     let last_in_tx = seq_test_client
         .send_eth(addr, None, None, None, 0u128)
         .await;
 
-    // 53th tx should not be in soft confirmation
+    // 57th tx should not be in soft confirmation
     let not_in_tx = seq_test_client
         .send_eth(addr, None, None, None, 0u128)
         .await;
