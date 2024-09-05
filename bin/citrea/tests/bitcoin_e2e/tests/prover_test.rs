9f26e91dc7e0df5d76a5a67c4f268937fd5b2bce
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-09-06 00:20:22 +0300

    WIP test for preproven commitments

--- bin/citrea/tests/bitcoin_e2e/tests/prover_test.rs
+++ bin/citrea/tests/bitcoin_e2e/tests/prover_test.rs
@@ -1,11 +1,19 @@
+use std::sync::Arc;
 use std::time::Duration;
 
 use anyhow::bail;
 use async_trait::async_trait;
+use bitcoin::secp256k1::generate_keypair;
+use bitcoin_da::service::{BitcoinService, BitcoinServiceConfig};
+use bitcoin_da::spec::RollupParams;
 use bitcoincore_rpc::RpcApi;
+use citrea_primitives::{REVEAL_BATCH_PROOF_PREFIX, REVEAL_LIGHT_CLIENT_PREFIX};
+use hex::ToHex;
+use sov_rollup_interface::da::{DaData, SequencerCommitment};
 
 use crate::bitcoin_e2e::config::{SequencerConfig, TestCaseConfig};
 use crate::bitcoin_e2e::framework::TestFramework;
+use crate::bitcoin_e2e::node::NodeKind;
 use crate::bitcoin_e2e::test_case::{TestCase, TestCaseRunner};
 use crate::bitcoin_e2e::Result;
 
@@ -98,7 +106,143 @@ impl TestCase for BasicProverTest {
     }
 }
 
+struct SkipPreprovenCommitmentsTest;
+
+#[async_trait]
+impl TestCase for SkipPreprovenCommitmentsTest {
+    fn test_config() -> TestCaseConfig {
+        TestCaseConfig {
+            with_prover: true,
+            with_full_node: true,
+            docker: true,
+            ..Default::default()
+        }
+    }
+
+    fn sequencer_config() -> SequencerConfig {
+        SequencerConfig {
+            min_soft_confirmations_per_commitment: 10,
+            test_mode: true,
+            ..Default::default()
+        }
+    }
+
+    async fn run_test(&self, f: &TestFramework) -> Result<()> {
+        let Some(sequencer) = &f.sequencer else {
+            bail!("Sequencer not running. Set TestCaseConfig with_sequencer to true")
+        };
+
+        let Some(prover) = &f.prover else {
+            bail!("Prover not running. Set TestCaseConfig with_prover to true")
+        };
+
+        let Some(full_node) = &f.full_node else {
+            bail!("FullNode not running. Set TestCaseConfig with_full_node to true")
+        };
+
+        let Some(da) = f.bitcoin_nodes.get(0) else {
+            bail!("bitcoind not running. Test cannot run with bitcoind running as DA")
+        };
+
+        let (secret_key, _public_key) = generate_keypair(&mut rand::thread_rng());
+        let secret_key = secret_key.secret_bytes().encode_hex();
+        // let key_pair = Keypair::from_secret_key(&secp, &secret_key);
+        // let mut buf = [0u8; constants::SECRET_KEY_SIZE * 2];
+        // to_hex(&self.secret_bytes(), &mut buf).expect("fixed-size hex serialization");
+        let da_config = &f.bitcoin_nodes.get(0).unwrap().config;
+        let bitcoin_da_service_config = BitcoinServiceConfig {
+            node_url: format!(
+                "http://127.0.0.1:{}/wallet/{}",
+                da_config.rpc_port,
+                NodeKind::Bitcoin
+            ),
+            node_username: da_config.rpc_user.clone(),
+            node_password: da_config.rpc_password.clone(),
+            network: bitcoin::Network::Regtest,
+            da_private_key: Some(secret_key),
+        };
+        let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+        let bitcoin_da_service = Arc::new(
+            BitcoinService::new_with_wallet_check(
+                bitcoin_da_service_config,
+                RollupParams {
+                    reveal_light_client_prefix: REVEAL_LIGHT_CLIENT_PREFIX.to_vec(),
+                    reveal_batch_prover_prefix: REVEAL_BATCH_PROOF_PREFIX.to_vec(),
+                },
+                tx,
+            )
+            .await
+            .unwrap(),
+        );
+        bitcoin_da_service.clone().spawn_da_queue(rx);
+
+        // Generate confirmed UTXOs
+        da.generate(10, None).await?;
+
+        let seq_height0 = sequencer.client.eth_block_number().await;
+        assert_eq!(seq_height0, 0);
+
+        for _ in 0..10 {
+            sequencer.client.send_publish_batch_request().await;
+        }
+
+        da.generate(5, None).await?;
+
+        // Wait for blob inscribe tx to be in mempool
+        da.wait_mempool_len(1, None).await?;
+
+        da.generate(5, None).await?;
+        let finalized_height = da.get_finalized_height().await?;
+        prover
+            .wait_for_l1_height(finalized_height, Some(Duration::from_secs(300)))
+            .await;
+
+        da.generate(5, None).await?;
+        let _proofs = full_node
+            .wait_for_zkproofs(finalized_height + 5, Some(Duration::from_secs(120)))
+            .await
+            .unwrap();
+
+        // Fetch the commitment created from the previous L1 range
+        let commitments = sequencer
+            .client
+            .ledger_get_sequencer_commitments_on_slot_by_number(11)
+            .await
+            .unwrap()
+            .unwrap();
+        let commitments: Vec<SequencerCommitment> = commitments
+            .into_iter()
+            .map(|response| SequencerCommitment {
+                merkle_root: response.merkle_root,
+                l2_start_block_number: response.l2_start_block_number,
+                l2_end_block_number: response.l2_end_block_number,
+            })
+            .collect();
+
+        let fee_sat_per_vbyte = bitcoin_da_service.get_fee_rate().await.unwrap();
+        bitcoin_da_service
+            .send_transaction_with_fee_rate(
+                None,
+                DaData::SequencerCommitment(commitments.first().unwrap().clone()),
+                fee_sat_per_vbyte,
+            )
+            .await
+            .unwrap();
+
+        da.generate(5, None).await?;
+
+        Ok(())
+    }
+}
+
 #[tokio::test]
 async fn basic_prover_test() -> Result<()> {
     TestCaseRunner::new(BasicProverTest).run().await
+}
+
+#[tokio::test]
+async fn prover_skips_preprovem_commitments_test() -> Result<()> {
+    TestCaseRunner::new(SkipPreprovenCommitmentsTest)
+        .run()
+        .await
 }
