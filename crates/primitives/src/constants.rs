35af77d073c5bec5dfee134c370225c3f3743aa2
jfldde <168934971+jfldde@users.noreply.github.com>
2024-09-08 20:11:24 +0200

    Restore prefixes

--- crates/primitives/src/constants.rs
+++ crates/primitives/src/constants.rs
@@ -1,10 +1,8 @@
 /// Prefix for the reveal transaction ids - batch proof namespace.
-// pub const REVEAL_BATCH_PROOF_PREFIX: &[u8] = [1, 1].as_slice();
-pub const REVEAL_BATCH_PROOF_PREFIX: &[u8] = [1].as_slice();
+pub const REVEAL_BATCH_PROOF_PREFIX: &[u8] = [1, 1].as_slice();
 
 /// Prefix for the reveal transaction ids - light client namespace.
-// pub const REVEAL_LIGHT_CLIENT_PREFIX: &[u8] = [2, 2].as_slice();
-pub const REVEAL_LIGHT_CLIENT_PREFIX: &[u8] = [2].as_slice();
+pub const REVEAL_LIGHT_CLIENT_PREFIX: &[u8] = [2, 2].as_slice();
 
 pub const TEST_PRIVATE_KEY: &str =
     "1212121212121212121212121212121212121212121212121212121212121212";
