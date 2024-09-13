af6584d4161534800562b15c11fcd99f18f5810b
Roman <r.proskuryakoff@gmail.com>
2024-09-13 17:35:39 +0400

    Don't insert duplicate account codes (#1180)
    * Don't insert duplicate account codes
    
    * Don't charge l1 fee if code is already in Db
    
    * Fix lints
    
    * StorageInternalCache::exists -> contains_key
    
    * Revert exists/contains_key
    
    * Don't insert code on duplicates

--- crates/evm/src/evm/db_commit.rs
+++ crates/evm/src/evm/db_commit.rs
@@ -51,9 +51,14 @@ impl<'a, C: sov_modules_api::Context> DatabaseCommit for EvmDb<'a, C> {
 
             if let Some(ref code) = account_info.code {
                 if !code.is_empty() {
-                    // TODO: would be good to have a contains_key method on the StateMap that would be optimized, so we can check the hash before storing the code
-                    self.code
-                        .set(&account_info.code_hash, code, self.working_set);
+                    let exists_in_db = self
+                        .code
+                        .get(&account_info.code_hash, self.working_set)
+                        .is_some();
+                    if !exists_in_db {
+                        self.code
+                            .set(&account_info.code_hash, code, self.working_set);
+                    }
                 }
             }
 
