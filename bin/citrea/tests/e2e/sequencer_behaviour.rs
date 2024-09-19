812c1ac025fdecb5a124ecbfd6c032541f1b23c0
okkothejawa <103260942+okkothejawa@users.noreply.github.com>
2024-09-19 14:24:13 +0300

    Fix block number event bug in light client (#1203)
    * Fix block number event bug in light client
    
    * Test fixes
    
    * Further test fix

--- bin/citrea/tests/e2e/sequencer_behaviour.rs
+++ bin/citrea/tests/e2e/sequencer_behaviour.rs
@@ -247,7 +247,7 @@ async fn transaction_failing_on_l1_is_removed_from_mempool() -> Result<(), anyho
 
     let random_wallet_address = random_wallet.address();
 
-    let second_block_base_fee = 767970154;
+    let second_block_base_fee = 767969424;
 
     let _pending = seq_test_client
         .send_eth(
