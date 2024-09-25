f1fc7cb47eb97225e15079febbd2504008cc56f6
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-09-25 14:35:52 +0300

    Pruning skeleton implementation

--- crates/pruning/src/lib.rs
+++ crates/pruning/src/lib.rs
@@ -1,11 +1,11 @@
-use std::sync::Arc;
-
 use citrea_evm::Evm;
+use futures::future;
 use sov_db::ledger_db::SharedLedgerOps;
-use sov_modules_api::Context;
+use sov_modules_api::default_context::DefaultContext;
 use tokio::select;
 use tokio::sync::broadcast;
 use tokio_util::sync::CancellationToken;
+use tracing::error;
 
 /// Define pruning mode based on configuration and/or CLI arguments
 pub enum PruningMode {
@@ -18,52 +18,85 @@ pub enum PruningMode {
 /// A configuration type to define the behaviour of the pruner.
 pub struct PruningConfig {
     /// Defines the number of blocks from the tip of the chain to remove.
-    distance: u64,
+    pub distance: u64,
 }
 
-pub struct Pruner<C, DB>
+pub struct Pruner<DB>
 where
-    C: Context,
     DB: SharedLedgerOps,
 {
     /// config
     config: PruningConfig,
+    /// The last block number which was pruned.
+    last_pruned_block: u64,
     /// A channel receiver which gets notified of new L2 blocks.
     receiver: broadcast::Receiver<u64>,
-    /// Components to be pruned.
-    evm: Arc<Evm<C>>,
-    ledger_db: Arc<DB>,
+    /// Access to ledger tables.
+    ledger_db: DB,
 }
 
-impl<C, DB> Pruner<C, DB>
+impl<DB> Pruner<DB>
 where
-    C: Context,
-    DB: SharedLedgerOps,
+    DB: SharedLedgerOps + Send + Sync + Clone + 'static,
 {
-    /// Prune everything
-    pub async fn prune(&self, up_to_block: u64) {
-        self.prune_evm(up_to_block).await;
-        self.prune_ledger(up_to_block).await;
+    pub fn new(
+        config: PruningConfig,
+        last_pruned_block: u64,
+        receiver: broadcast::Receiver<u64>,
+        ledger_db: DB,
+    ) -> Self {
+        Self {
+            config,
+            last_pruned_block,
+            receiver,
+            ledger_db,
+        }
     }
 
-    /// Prune evm
-    pub async fn prune_evm(&self, up_to_block: u64) {}
+    /// Prune everything
+    pub async fn prune(&self, up_to_block: u64) {
+        let ledger_db = self.ledger_db.clone();
+        let ledger_pruning_handle =
+            tokio::task::spawn_blocking(move || prune_ledger(ledger_db, up_to_block));
+        let evm_pruning_handle = tokio::task::spawn_blocking(move || prune_evm(up_to_block));
 
-    /// Prune ledger
-    pub async fn prune_ledger(&self, up_to_block: u64) {}
+        future::join_all([ledger_pruning_handle, evm_pruning_handle]).await;
+    }
 
     pub async fn run(mut self, cancellation_token: CancellationToken) {
         loop {
             select! {
                 current_l2_block = self.receiver.recv() => {
                     if let Ok(current_l2_block) = current_l2_block {
-                        self.prune(current_l2_block - self.config.distance).await;
+                        // Calculate the block at which pruning would be triggered.
+                        // This is allowing `self.config.distance` blocks to be produced before we
+                        // decide to prune the previous `self.config.distance` blocks.
+                        let trigger_block = (self.last_pruned_block + self.config.distance) + 1;
+                        if current_l2_block >=trigger_block {
+                            self.prune(current_l2_block - self.config.distance).await;
+                            self.last_pruned_block += self.config.distance;
+                        }
                     }
                 },
                 _ = cancellation_token.cancelled() => {
+                    // Store the last pruned l2 height in ledger DB to be restored in the next initialization.
+                    if let Err(e) = self.ledger_db.set_last_pruned_l2_height(self.last_pruned_block) {
+                        error!("Failed to store last pruned L2 height {}: {:?}", self.last_pruned_block, e);
+                    }
                     return;
                 }
             }
         }
     }
+}
+
+/// Prune evm
+pub fn prune_evm(_up_to_block: u64) {
+    let _evm = Evm::<DefaultContext>::default();
+    unimplemented!()
+}
+
+/// Prune ledger
+pub fn prune_ledger<DB: SharedLedgerOps>(_ledger_db: DB, _up_to_block: u64) {
+    unimplemented!()
 }
