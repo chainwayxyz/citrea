fa89b10a5a8bca584689f19f97b0aba992b77135
eyusufatik <esadyusufatik@gmail.com>
2024-10-06 02:55:08 +0300

    write native part skeleton for light client prover

---
+++ crates/light-client-prover/src/da_block_handler.rs
@@ -0,0 +1,292 @@
+use std::collections::{HashMap, VecDeque};
+use std::sync::Arc;
+
+use borsh::BorshDeserialize;
+use citrea_common::cache::L1BlockCache;
+use citrea_common::da::get_da_block_at_height;
+use sov_db::ledger_db::{LightClientProverLedgerOps, SharedLedgerOps};
+use sov_db::schema::types::SlotNumber;
+use sov_modules_api::{BlobReaderTrait, DaSpec, Zkvm};
+use sov_rollup_interface::da::{BlockHeaderTrait, DaDataLightClient};
+use sov_rollup_interface::services::da::{DaService, SlotData};
+use sov_rollup_interface::spec::SpecId;
+use sov_rollup_interface::zk::ZkvmHost;
+use sov_stf_runner::{LightClientProverConfig, ProverService};
+use tokio::select;
+use tokio::sync::{mpsc, Mutex};
+use tokio::time::{sleep, Duration};
+use tokio_util::sync::CancellationToken;
+use tracing::{error, info};
+
+use crate::input::LightClientCircuitInput;
+use crate::output::LightClientCircuitOutput;
+
+pub(crate) struct L1BlockHandler<Vm, Da, Ps, DB>
+where
+    Da: DaService,
+    Vm: ZkvmHost + Zkvm,
+    DB: LightClientProverLedgerOps + SharedLedgerOps + Clone,
+    Ps: ProverService<Vm>,
+{
+    prover_config: LightClientProverConfig,
+    prover_service: Arc<Ps>,
+    ledger_db: DB,
+    da_service: Arc<Da>,
+    batch_prover_da_pub_key: Vec<u8>,
+    batch_proof_code_commitments_by_spec: HashMap<SpecId, Vm::CodeCommitment>,
+    l1_block_cache: Arc<Mutex<L1BlockCache<Da>>>,
+    pending_l1_blocks: VecDeque<<Da as DaService>::FilteredBlock>,
+}
+
+impl<Vm, Da, Ps, DB> L1BlockHandler<Vm, Da, Ps, DB>
+where
+    Da: DaService,
+    Vm: ZkvmHost + Zkvm,
+    Ps: ProverService<Vm, DaService = Da>,
+    DB: LightClientProverLedgerOps + SharedLedgerOps + Clone,
+{
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        prover_config: LightClientProverConfig,
+        prover_service: Arc<Ps>,
+        ledger_db: DB,
+        da_service: Arc<Da>,
+        batch_prover_da_pub_key: Vec<u8>,
+        batch_proof_code_commitments_by_spec: HashMap<SpecId, Vm::CodeCommitment>,
+    ) -> Self {
+        Self {
+            prover_config,
+            prover_service,
+            ledger_db,
+            da_service,
+            batch_prover_da_pub_key,
+            batch_proof_code_commitments_by_spec,
+            l1_block_cache: Arc::new(Mutex::new(L1BlockCache::new())),
+            pending_l1_blocks: VecDeque::new(),
+        }
+    }
+
+    pub async fn run(mut self, start_l1_height: u64, cancellation_token: CancellationToken) {
+        // if self.prover_config.enable_recovery {
+        //     if let Err(e) = self.check_and_recover_ongoing_proving_sessions().await {
+        //         error!("Failed to recover ongoing proving sessions: {:?}", e);
+        //     }
+        // } else {
+        //     // If recovery is disabled, clear pending proving sessions
+        //     self.ledger_db
+        //         .clear_pending_proving_sessions()
+        //         .expect("Failed to clear pending proving sessions");
+        // }
+
+        let (l1_tx, mut l1_rx) = mpsc::channel(1);
+        let l1_sync_worker = sync_l1(
+            start_l1_height,
+            self.da_service.clone(),
+            l1_tx,
+            self.l1_block_cache.clone(),
+        );
+        tokio::pin!(l1_sync_worker);
+
+        let mut interval = tokio::time::interval(Duration::from_secs(1));
+        interval.tick().await;
+        loop {
+            select! {
+                biased;
+                _ = cancellation_token.cancelled() => {
+                    return;
+                }
+                _ = &mut l1_sync_worker => {},
+                Some(l1_block) = l1_rx.recv() => {
+                    self.pending_l1_blocks.push_back(l1_block);
+                },
+                _ = interval.tick() => {
+                    if let Err(e) = self.process_l1_block().await {
+                        error!("Could not process L1 block and generate proof: {:?}", e);
+                    }
+                },
+            }
+        }
+    }
+
+    async fn process_l1_block(&mut self) -> Result<(), anyhow::Error> {
+        while !self.pending_l1_blocks.is_empty() {
+            let l1_block = self
+                .pending_l1_blocks
+                .front()
+                .expect("Pending l1 blocks cannot be empty");
+            // work on the first unprocessed l1 block
+            let l1_height = l1_block.header().height();
+
+            // Set the l1 height of the l1 hash
+            self.ledger_db
+                .set_l1_height_of_l1_hash(
+                    l1_block.header().hash().into(),
+                    l1_block.header().height(),
+                )
+                .unwrap();
+
+            let mut da_data: Vec<<<Da as DaService>::Spec as DaSpec>::BlobTransaction> = self
+                .da_service
+                .extract_relevant_blobs_light_client(l1_block);
+
+            let batch_proofs = self
+                .extract_batch_proofs(&mut da_data, l1_block.header().hash().into())
+                .await;
+
+            // Do any kind of ordering etc. on batch proofs here
+            // If you do so, don't forget to do the same inside zk
+
+            let circuit_input = self.create_circuit_input(da_data, l1_block).await;
+
+            let circuit_output = self.prove(circuit_input).await?;
+
+            tracing::info!("Proved. Output: {:?}", circuit_output);
+
+            if let Err(e) = self
+                .ledger_db
+                .set_last_scanned_l1_height(SlotNumber(l1_height))
+            {
+                panic!(
+                    "Failed to put prover last scanned l1 height in the ledger db: {}",
+                    e
+                );
+            }
+
+            self.pending_l1_blocks.pop_front();
+        }
+        Ok(())
+    }
+
+    async fn extract_batch_proofs(
+        &self,
+        da_data: &mut [<<Da as DaService>::Spec as DaSpec>::BlobTransaction],
+        da_slot_hash: [u8; 32], // passing this as an argument is not clever
+    ) -> Vec<DaDataLightClient> {
+        let mut batch_proofs = Vec::new();
+
+        da_data.iter_mut().for_each(|blob| {
+            blob.full_data();
+        });
+
+        da_data.iter_mut().for_each(|tx| {
+            // Check for commitment
+            if tx.sender().as_ref() == self.batch_prover_da_pub_key.as_slice() {
+                let data = DaDataLightClient::try_from_slice(tx.full_data());
+
+                if let Ok(proof) = data {
+                    batch_proofs.push(proof);
+                } else {
+                    tracing::warn!(
+                        "Found broken DA data in block 0x{}: {:?}",
+                        hex::encode(da_slot_hash),
+                        data
+                    );
+                }
+            }
+        });
+
+        batch_proofs
+    }
+
+    async fn create_circuit_input(
+        &self,
+        da_data: Vec<<<Da as DaService>::Spec as DaSpec>::BlobTransaction>,
+        l1_block: &Da::FilteredBlock,
+    ) -> LightClientCircuitInput<Da::Spec> {
+        let (inclusion_proof, completeness_proof) = self
+            .da_service
+            .get_extraction_proof_light_client(l1_block)
+            .await;
+
+        LightClientCircuitInput {
+            da_data,
+            inclusion_proof,
+            completeness_proof,
+            da_block_header: l1_block.header().clone(),
+            batch_pover_da_pub_key: self.batch_prover_da_pub_key.clone(),
+        }
+    }
+
+    async fn prove(
+        &self,
+        circuit_input: LightClientCircuitInput<<Da as DaService>::Spec>,
+    ) -> Result<LightClientCircuitOutput, anyhow::Error> {
+        let da_slot_hash = circuit_input.da_block_header.hash();
+        let prover_service = self.prover_service.as_ref();
+
+        prover_service
+            .submit_witness(borsh::to_vec(&circuit_input)?, da_slot_hash.clone())
+            .await;
+
+        prover_service.prove(da_slot_hash.clone()).await?;
+
+        let output: LightClientCircuitOutput = prover_service
+            .wait_for_proving_and_extract_output(da_slot_hash)
+            .await?;
+
+        Ok(output)
+    }
+
+    // async fn check_and_recover_ongoing_proving_sessions(&self) -> Result<(), anyhow::Error> {
+    //     let prover_service = self.prover_service.as_ref();
+    //     let results = prover_service
+    //         .recover_proving_sessions_and_send_to_da(&self.da_service)
+    //         .await?;
+
+    //     for (tx_id, proof) in results {
+    //         self.extract_and_store_proof(tx_id, proof).await?;
+    //     }
+    //     Ok(())
+    // }
+}
+
+async fn sync_l1<Da>(
+    start_l1_height: u64,
+    da_service: Arc<Da>,
+    sender: mpsc::Sender<Da::FilteredBlock>,
+    l1_block_cache: Arc<Mutex<L1BlockCache<Da>>>,
+) where
+    Da: DaService,
+{
+    let mut l1_height = start_l1_height;
+    info!("Starting to sync from L1 height {}", l1_height);
+
+    'block_sync: loop {
+        // TODO: for a node, the da block at slot_height might not have been finalized yet
+        // should wait for it to be finalized
+        let last_finalized_l1_block_header =
+            match da_service.get_last_finalized_block_header().await {
+                Ok(header) => header,
+                Err(e) => {
+                    error!("Could not fetch last finalized L1 block header: {}", e);
+                    sleep(Duration::from_secs(2)).await;
+                    continue;
+                }
+            };
+
+        let new_l1_height = last_finalized_l1_block_header.height();
+
+        for block_number in l1_height + 1..=new_l1_height {
+            let l1_block =
+                match get_da_block_at_height(&da_service, block_number, l1_block_cache.clone())
+                    .await
+                {
+                    Ok(block) => block,
+                    Err(e) => {
+                        error!("Could not fetch last finalized L1 block: {}", e);
+                        sleep(Duration::from_secs(2)).await;
+                        continue 'block_sync;
+                    }
+                };
+            if block_number > l1_height {
+                l1_height = block_number;
+                if let Err(e) = sender.send(l1_block).await {
+                    error!("Could not notify about L1 block: {}", e);
+                    continue 'block_sync;
+                }
+            }
+        }
+
+        sleep(Duration::from_secs(2)).await;
+    }
+}
