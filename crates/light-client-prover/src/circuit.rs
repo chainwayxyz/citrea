106920063739d388d1d3d6d423244d019e5ee5ce
eyusufatik <esadyusufatik@gmail.com>
2024-10-06 02:55:08 +0300

    write native part skeleton for light client prover

--- crates/light-client-prover/src/circuit.rs
+++ crates/light-client-prover/src/circuit.rs
@@ -1,1 +1,14 @@
+use sov_modules_api::DaSpec;
+use sov_rollup_interface::da::DaVerifier;
 
+use crate::input::LightClientCircuitInput;
+use crate::output::LightClientCircuitOutput;
+
+pub enum LightClientVerificationError {}
+
+pub fn run_circuit<DaV: DaVerifier, DaS: DaSpec>(
+    input: LightClientCircuitInput<DaS>,
+    da_verifier: DaV,
+) -> Result<LightClientCircuitOutput, LightClientVerificationError> {
+    todo!()
+}
--- crates/light-client-prover/src/input.rs
+++ crates/light-client-prover/src/input.rs
@@ -1,1 +1,12 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use sov_rollup_interface::da::DaSpec;
 
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct LightClientCircuitInput<Da: DaSpec> {
+    pub da_data: Vec<Da::BlobTransaction>,
+    pub inclusion_proof: Da::InclusionMultiProof,
+    pub completeness_proof: Da::CompletenessProof,
+    pub da_block_header: Da::BlockHeader,
+
+    pub batch_pover_da_pub_key: Vec<u8>,
+}
