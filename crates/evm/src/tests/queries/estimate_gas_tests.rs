1f5782fd946ee68029afc9599f7f9ffbcec01fd2
Rakan Al-Huneiti <rakan.alhneiti@gmail.com>
2024-10-09 17:59:07 +0300

    Implement state and block overrides (#1270)
    * Implement state and block overrides
    
    * Update comment
    
    * apply_block_overrides func
    
    * apply_state_overrides func
    
    * Use a single func for replacing account storage
    
    * Clippy
    
    * Address feedback
    
    Use Reth's BlockOverrides with saturating_to to convert back to u64
    
    * Test for block overrides
    
    * Test for state overrides
    
    * Comment tests
    
    * Create a fresh working set
    
    * Remove box
    
    * remove alloy-serde
    
    ---------
    
    Co-authored-by: eyusufatik <esadyusufatik@gmail.com>

--- crates/evm/src/tests/queries/estimate_gas_tests.rs
+++ crates/evm/src/tests/queries/estimate_gas_tests.rs
@@ -338,8 +338,8 @@ fn test_access_list() {
 }
 
 #[test]
-fn test_estimate_gas_with_varied_inputs() {
-    let (evm, mut working_set, signer, _) = init_evm();
+fn estimate_gas_with_varied_inputs_test() {
+    let (evm, mut working_set, _, signer, _) = init_evm();
 
     let simple_call_data = 0;
     let simple_result =
