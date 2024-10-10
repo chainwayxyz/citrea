088adcdd23a423bbcf16dfd82d572ab3d41cdb17
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2024-10-11 02:01:26 +0300

    implement FromEnv for other configs

--- crates/common/src/config.rs
+++ crates/common/src/config.rs
@@ -10,6 +10,35 @@ use sov_stf_runner::ProverGuestRunConfig;
 pub trait FromEnv: Sized {
     fn from_env() -> anyhow::Result<Self>;
 }
+impl FromEnv for PruningConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(PruningConfig {
+            distance: std::env::var("PRUNING_DISTANCE")?.parse()?,
+        })
+    }
+}
+impl FromEnv for sov_mock_da::MockDaConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            sender_address: std::env::var("SENDER_ADDRESS")?.parse()?,
+            db_path: std::env::var("DB_PATH")?.into(),
+        })
+    }
+}
+
+impl FromEnv for bitcoin_da::service::BitcoinServiceConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            node_url: std::env::var("NODE_URL")?,
+            node_username: std::env::var("NODE_USERNAME")?,
+            node_password: std::env::var("NODE_PASSWORD")?,
+            network: serde_json::from_str(&format!("\"{}\"", std::env::var("NETWORK")?))?,
+            da_private_key: std::env::var("DA_PRIVATE_KEY").ok(),
+            tx_backup_dir: std::env::var("TX_BACKUP_DIR")?,
+        })
+    }
+}
+
 /// Runner configuration.
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct RunnerConfig {
@@ -32,13 +61,12 @@ impl FromEnv for RunnerConfig {
             include_tx_body: std::env::var("INCLUDE_TX_BODY")?.parse()?,
             accept_public_input_as_proven: std::env::var("ACCEPT_PUBLIC_INPUT_AS_PROVEN")
                 .ok()
-                .map(|val| val.parse().ok())
-                .flatten(),
-            sync_blocks_count: toml::from_str(&std::env::var("SYNC_BLOCKS_COUNT").unwrap_or("".to_string()))?,
-            pruning_config: std::env::var("PRUNING_CONFIG")
+                .and_then(|val| val.parse().ok()),
+            sync_blocks_count: std::env::var("SYNC_BLOCKS_COUNT")
                 .ok()
-                .map(|val| toml::from_str(&val).ok())
-                .flatten(),
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_sync_blocks_count()),
+            pruning_config: PruningConfig::from_env().ok(),
         })
     }
 }
@@ -69,30 +97,37 @@ pub struct RpcConfig {
     #[serde(default = "default_max_subscriptions_per_connection")]
     pub max_subscriptions_per_connection: u32,
 }
-impl FromEnv for RpcConfig{
+impl FromEnv for RpcConfig {
     fn from_env() -> anyhow::Result<Self> {
-        let mut toml_string = "".to_string();
-        toml_string.push_str(&format!("bind_host = {}\n", std::env::var("RPC_BIND_HOST")?));
-        toml_string.push_str(&format!("bind_port = {}\n", std::env::var("RPC_BIND_PORT")?));
-        if let Ok(val) = std::env::var("RPC_MAX_CONNECTIONS") {
-            toml_string.push_str(&format!("max_connections = {}\n", val));
-        }
-        if let Ok(val) = std::env::var("RPC_MAX_REQUEST_BODY_SIZE") {
-            toml_string.push_str(&format!("max_request_body_size = {}\n", val));
-        }
-        if let Ok(val) = std::env::var("RPC_MAX_RESPONSE_BODY_SIZE") {
-            toml_string.push_str(&format!("max_response_body_size = {}\n", val));
-        }
-        if let Ok(val) = std::env::var("RPC_BATCH_REQUESTS_LIMIT") {
-            toml_string.push_str(&format!("batch_requests_limit = {}\n", val));
-        }
-        if let Ok(val) = std::env::var("RPC_ENABLE_SUBSCRIPTIONS") {
-            toml_string.push_str(&format!("enable_subscriptions = {}\n", val));
-        }
-        if let Ok(val) = std::env::var("RPC_MAX_SUBSCRIPTIONS_PER_CONNECTION") {
-            toml_string.push_str(&format!("max_subscriptions_per_connection = {}\n", val));
-        }
-        Ok(toml::from_str(&toml_string)?)
+        Ok(Self {
+            bind_host: std::env::var("RPC_BIND_HOST")?,
+            bind_port: std::env::var("RPC_BIND_PORT")?.parse()?,
+            // for the rest of the fields, in case of a parsing error, the default value will be used
+            max_connections: std::env::var("RPC_MAX_CONNECTIONS")
+                .ok()
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_max_connections()),
+            max_request_body_size: std::env::var("RPC_MAX_REQUEST_BODY_SIZE")
+                .ok()
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_max_request_body_size()),
+            max_response_body_size: std::env::var("RPC_MAX_RESPONSE_BODY_SIZE")
+                .ok()
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_max_response_body_size()),
+            batch_requests_limit: std::env::var("RPC_BATCH_REQUESTS_LIMIT")
+                .ok()
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_batch_requests_limit()),
+            enable_subscriptions: std::env::var("RPC_ENABLE_SUBSCRIPTIONS")
+                .ok()
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_enable_subscriptions()),
+            max_subscriptions_per_connection: std::env::var("RPC_MAX_SUBSCRIPTIONS_PER_CONNECTION")
+                .ok()
+                .and_then(|val| val.parse().ok())
+                .unwrap_or(default_max_subscriptions_per_connection()),
+        })
     }
 }
 #[inline]
@@ -138,6 +173,16 @@ pub struct StorageConfig {
     /// File descriptor limit for RocksDB
     pub db_max_open_files: Option<i32>,
 }
+impl FromEnv for StorageConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            path: std::env::var("STORAGE_PATH")?.into(),
+            db_max_open_files: std::env::var("DB_MAX_OPEN_FILES")
+                .ok()
+                .and_then(|val| val.parse().ok()),
+        })
+    }
+}
 
 /// Important public keys for the rollup
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
@@ -154,7 +199,15 @@ pub struct RollupPublicKeys {
     #[serde(with = "hex::serde")]
     pub prover_da_pub_key: Vec<u8>,
 }
-
+impl FromEnv for RollupPublicKeys {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            sequencer_public_key: hex::decode(std::env::var("SEQUENCER_PUBLIC_KEY")?)?,
+            sequencer_da_pub_key: hex::decode(std::env::var("SEQUENCER_DA_PUB_KEY")?)?,
+            prover_da_pub_key: hex::decode(std::env::var("PROVER_DA_PUB_KEY")?)?,
+        })
+    }
+}
 /// Rollup Configuration
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct FullNodeConfig<BitcoinServiceConfig> {
@@ -169,7 +222,20 @@ pub struct FullNodeConfig<BitcoinServiceConfig> {
     /// Important pubkeys
     pub public_keys: RollupPublicKeys,
 }
-
+impl<BitcoinServiceConfig> FromEnv for FullNodeConfig<BitcoinServiceConfig>
+where
+    BitcoinServiceConfig: FromEnv + DeserializeOwned,
+{
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            rpc: RpcConfig::from_env()?,
+            storage: StorageConfig::from_env()?,
+            runner: RunnerConfig::from_env().ok(),
+            da: BitcoinServiceConfig::from_env()?,
+            public_keys: RollupPublicKeys::from_env()?,
+        })
+    }
+}
 /// Prover configuration
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub struct ProverConfig {
@@ -191,6 +257,15 @@ impl Default for ProverConfig {
     }
 }
 
+impl FromEnv for ProverConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(ProverConfig {
+            proving_mode: serde_json::from_str(&format!("\"{}\"", std::env::var("PROVING_MODE")?))?,
+            proof_sampling_number: std::env::var("PROOF_SAMPLING_NUMBER")?.parse()?,
+            enable_recovery: std::env::var("ENABLE_RECOVERY")?.parse()?,
+        })
+    }
+}
 /// Reads toml file as a specific type.
 pub fn from_toml_path<P: AsRef<Path>, R: DeserializeOwned>(path: P) -> anyhow::Result<R> {
     let mut contents = String::new();
@@ -239,7 +314,22 @@ impl Default for SequencerConfig {
         }
     }
 }
-
+impl FromEnv for SequencerConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            private_key: std::env::var("PRIVATE_KEY")?,
+            min_soft_confirmations_per_commitment: std::env::var(
+                "MIN_SOFT_CONFIRMATIONS_PER_COMMITMENT",
+            )?
+            .parse()?,
+            test_mode: std::env::var("TEST_MODE")?.parse()?,
+            deposit_mempool_fetch_limit: std::env::var("DEPOSIT_MEMPOOL_FETCH_LIMIT")?.parse()?,
+            mempool_conf: SequencerMempoolConfig::from_env()?,
+            da_update_interval_ms: std::env::var("DA_UPDATE_INTERVAL_MS")?.parse()?,
+            block_production_interval_ms: std::env::var("BLOCK_PRODUCTION_INTERVAL_MS")?.parse()?,
+        })
+    }
+}
 /// Mempool Config for the sequencer
 /// Read: https://github.com/ledgerwatch/erigon/wiki/Transaction-Pool-Design
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
@@ -273,7 +363,19 @@ impl Default for SequencerMempoolConfig {
         }
     }
 }
-
+impl FromEnv for SequencerMempoolConfig {
+    fn from_env() -> anyhow::Result<Self> {
+        Ok(Self {
+            pending_tx_limit: std::env::var("PENDING_TX_LIMIT")?.parse()?,
+            pending_tx_size: std::env::var("PENDING_TX_SIZE")?.parse()?,
+            queue_tx_limit: std::env::var("QUEUE_TX_LIMIT")?.parse()?,
+            queue_tx_size: std::env::var("QUEUE_TX_SIZE")?.parse()?,
+            base_fee_tx_limit: std::env::var("BASE_FEE_TX_LIMIT")?.parse()?,
+            base_fee_tx_size: std::env::var("BASE_FEE_TX_SIZE")?.parse()?,
+            max_account_slots: std::env::var("MAX_ACCOUNT_SLOTS")?.parse()?,
+        })
+    }
+}
 #[cfg(test)]
 mod tests {
     use std::io::Write;
@@ -376,7 +478,7 @@ mod tests {
         assert_eq!(config, expected);
     }
     #[test]
-    fn test_correct_config_sequencer() {
+    fn test_correct_sequencer_config() {
         let config = r#"
             private_key = "1212121212121212121212121212121212121212121212121212121212121212"
             min_soft_confirmations_per_commitment = 123
@@ -418,41 +520,130 @@ mod tests {
         };
         assert_eq!(config, expected);
     }
-    #[test]
-    fn test_read_rpc_config_from_env(){
-        std::env::set_var("RPC_BIND_HOST", "\"127.0.0.1\"");
-        std::env::set_var("RPC_BIND_PORT", "8080");
-        std::env::set_var("RPC_MAX_CONNECTIONS", "100");
-        let rpc_config = RpcConfig::from_env().unwrap();
 
-        let expected = RpcConfig {
-            bind_host: "127.0.0.1".to_string(),
-            bind_port: 8080,
-            max_connections: 50,
-            max_request_body_size: default_max_request_body_size(),
-            max_response_body_size: default_max_response_body_size(),
-            batch_requests_limit: default_batch_requests_limit(),
-            enable_subscriptions: default_enable_subscriptions(),
-            max_subscriptions_per_connection: default_max_subscriptions_per_connection(),
+    #[test]
+    fn test_correct_prover_config_from_env() {
+        std::env::set_var("PROVING_MODE", "skip");
+        std::env::set_var("PROOF_SAMPLING_NUMBER", "500");
+        std::env::set_var("ENABLE_RECOVERY", "true");
+
+        let prover_config = ProverConfig::from_env().unwrap();
+
+        let expected = ProverConfig {
+            proving_mode: ProverGuestRunConfig::Skip,
+            proof_sampling_number: 500,
+            enable_recovery: true,
         };
-        assert_eq!(rpc_config, expected);
+        assert_eq!(prover_config, expected);
     }
     #[test]
-    fn test_read_runner_config_from_env() {
-        std::env::set_var("SEQUENCER_CLIENT_URL", "\"http://localhost:8545\"");
-        std::env::set_var("INCLUDE_TX_BODY", "true");
-        std::env::set_var("ACCEPT_PUBLIC_INPUT_AS_PROVEN", "true");
-        std::env::set_var("SYNC_BLOCKS_COUNT", "100");
-        let runner_config = RunnerConfig::from_env().unwrap();
+    fn test_correct_sequencer_config_from_env() {
+        std::env::set_var(
+            "PRIVATE_KEY",
+            "1212121212121212121212121212121212121212121212121212121212121212",
+        );
+        std::env::set_var("MIN_SOFT_CONFIRMATIONS_PER_COMMITMENT", "123");
+        std::env::set_var("TEST_MODE", "false");
+        std::env::set_var("DEPOSIT_MEMPOOL_FETCH_LIMIT", "10");
+        std::env::set_var("DA_UPDATE_INTERVAL_MS", "1000");
+        std::env::set_var("BLOCK_PRODUCTION_INTERVAL_MS", "1000");
+        std::env::set_var("PENDING_TX_LIMIT", "100000");
+        std::env::set_var("PENDING_TX_SIZE", "200");
+        std::env::set_var("QUEUE_TX_LIMIT", "100000");
+        std::env::set_var("QUEUE_TX_SIZE", "200");
+        std::env::set_var("BASE_FEE_TX_LIMIT", "100000");
+        std::env::set_var("BASE_FEE_TX_SIZE", "200");
+        std::env::set_var("MAX_ACCOUNT_SLOTS", "16");
 
-        let expected = RunnerConfig {
-            sequencer_client_url: "http://localhost:8545".to_string(),
-            include_tx_body: true,
-            accept_public_input_as_proven: Some(true),
-            sync_blocks_count: 100,
-            pruning_config: None,
+        let sequencer_config = SequencerConfig::from_env().unwrap();
+
+        let expected = SequencerConfig {
+            private_key: "1212121212121212121212121212121212121212121212121212121212121212"
+                .to_string(),
+            min_soft_confirmations_per_commitment: 123,
+            test_mode: false,
+            deposit_mempool_fetch_limit: 10,
+            mempool_conf: SequencerMempoolConfig {
+                pending_tx_limit: 100000,
+                pending_tx_size: 200,
+                queue_tx_limit: 100000,
+                queue_tx_size: 200,
+                base_fee_tx_limit: 100000,
+                base_fee_tx_size: 200,
+                max_account_slots: 16,
+            },
+            da_update_interval_ms: 1000,
+            block_production_interval_ms: 1000,
         };
+        assert_eq!(sequencer_config, expected);
+    }
 
-        assert_eq!(runner_config, expected);
+    #[test]
+    fn test_correct_full_node_config_from_env() {
+        std::env::set_var(
+            "SEQUENCER_PUBLIC_KEY",
+            "0000000000000000000000000000000000000000000000000000000000000000",
+        );
+        std::env::set_var(
+            "SEQUENCER_DA_PUB_KEY",
+            "7777777777777777777777777777777777777777777777777777777777777777",
+        );
+        std::env::set_var("PROVER_DA_PUB_KEY", "");
+
+        std::env::set_var("RPC_BIND_HOST", "127.0.0.1");
+        std::env::set_var("RPC_BIND_PORT", "12345");
+        std::env::set_var("RPC_MAX_CONNECTIONS", "500");
+        std::env::set_var("RPC_ENABLE_SUBSCRIPTIONS", "true");
+        std::env::set_var("RPC_MAX_SUBSCRIPTIONS_PER_CONNECTION", "200");
+
+        std::env::set_var(
+            "SENDER_ADDRESS",
+            "0000000000000000000000000000000000000000000000000000000000000000",
+        );
+        std::env::set_var("DB_PATH", "/tmp/da");
+
+        std::env::set_var("STORAGE_PATH", "/tmp/rollup");
+        std::env::set_var("DB_MAX_OPEN_FILES", "123");
+
+        std::env::set_var("INCLUDE_TX_BODY", "true");
+        std::env::set_var("SEQUENCER_CLIENT_URL", "http://0.0.0.0:12346");
+        std::env::set_var("PRUNING_DISTANCE", "1000");
+
+        let full_node_config: FullNodeConfig<sov_mock_da::MockDaConfig> =
+            FullNodeConfig::from_env().unwrap();
+
+        let expected = FullNodeConfig {
+            rpc: RpcConfig {
+                bind_host: "127.0.0.1".to_string(),
+                bind_port: 12345,
+                max_connections: 500,
+                max_request_body_size: default_max_request_body_size(),
+                max_response_body_size: default_max_response_body_size(),
+                batch_requests_limit: default_batch_requests_limit(),
+                enable_subscriptions: true,
+                max_subscriptions_per_connection: 200,
+            },
+            storage: StorageConfig {
+                path: "/tmp/rollup".into(),
+                db_max_open_files: Some(123),
+            },
+            runner: Some(RunnerConfig {
+                sequencer_client_url: "http://0.0.0.0:12346".to_string(),
+                include_tx_body: true,
+                accept_public_input_as_proven: None,
+                sync_blocks_count: default_sync_blocks_count(),
+                pruning_config: Some(PruningConfig { distance: 1000 }),
+            }),
+            da: sov_mock_da::MockDaConfig {
+                sender_address: [0; 32].into(),
+                db_path: "/tmp/da".into(),
+            },
+            public_keys: RollupPublicKeys {
+                sequencer_public_key: vec![0; 32],
+                sequencer_da_pub_key: vec![119; 32],
+                prover_da_pub_key: vec![],
+            },
+        };
+        assert_eq!(full_node_config, expected);
     }
 }
