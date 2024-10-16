f7439284405657829331f2ecb498885eaff70ddb
yaziciahmet <yaziciahmet991@gmail.com>
2024-10-16 16:53:25 +0200

    Implement debuggable verifying key wrapper

--- crates/sp1/src/lib.rs
+++ crates/sp1/src/lib.rs
@@ -1,3 +1,16 @@
+use serde::{Deserialize, Serialize};
+use sp1_sdk::{HashableKey, SP1VerifyingKey};
+
 pub mod guest;
 #[cfg(feature = "native")]
 pub mod host;
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct VerifyingKey(SP1VerifyingKey);
+
+impl std::fmt::Debug for VerifyingKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let key = self.0.bytes32();
+        write!(f, "VerifyingKey {{ SP1VerifyingKey {{ vk: {} }} }}", key)
+    }
+}
