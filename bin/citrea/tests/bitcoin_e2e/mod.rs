e95b9772a90f066deff31fbc17b3845e17cd80f0
jfldde <168934971+jfldde@users.noreply.github.com>
2024-10-16 13:45:11 +0100

    Bump to latest rev

--- bin/citrea/tests/bitcoin_e2e/mod.rs
+++ bin/citrea/tests/bitcoin_e2e/mod.rs
@@ -1,5 +1,23 @@
+use std::path::PathBuf;
+
 pub mod bitcoin_test;
 // pub mod mempool_accept;
 pub mod prover_test;
 pub mod sequencer_commitments;
 pub mod sequencer_test;
+
+pub(super) fn get_citrea_path() -> PathBuf {
+    std::env::var("CITREA_E2E_TEST_BINARY").map_or_else(
+        |_| {
+            let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            manifest_dir
+                .ancestors()
+                .nth(2)
+                .expect("Failed to find workspace root")
+                .join("target")
+                .join("debug")
+                .join("citrea")
+        },
+        PathBuf::from,
+    )
+}
