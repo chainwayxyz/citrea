67ad7c947bd4782a5701ed96420df79be8cc26b3
yaziciahmet <yaziciahmet991@gmail.com>
2024-10-17 14:47:49 +0200

    Prepare to test SP1 guest code

--- crates/sp1/src/lib.rs
+++ crates/sp1/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use sp1_sdk::{HashableKey, SP1VerifyingKey};
+use sp1_sdk::{CpuProver, HashableKey, Prover, SP1VerifyingKey};
 
 pub mod guest;
 #[cfg(feature = "native")]
@@ -12,5 +12,13 @@ impl std::fmt::Debug for VerifyingKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let key = self.0.bytes32();
         write!(f, "VerifyingKey {{ SP1VerifyingKey {{ vk: {} }} }}", key)
+    }
+}
+
+#[cfg(feature = "native")]
+impl VerifyingKey {
+    pub fn from_elf(elf: &[u8]) -> Self {
+        let (_, vk) = CpuProver::new().setup(elf);
+        Self(vk)
     }
 }
