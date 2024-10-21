77ece94a3d22791fac573c5c7882bf89999ec84e
jfldde <168934971+jfldde@users.noreply.github.com>
2024-10-21 17:21:49 +0100

    Test prover tx chaining

--- bin/citrea/tests/bitcoin_e2e/tx_chain.rs
+++ bin/citrea/tests/bitcoin_e2e/tx_chain.rs
@@ -2,6 +2,8 @@ use async_trait::async_trait;
 use bitcoin::{Amount, Transaction};
 use bitcoin_da::REVEAL_OUTPUT_AMOUNT;
 use bitcoincore_rpc::RpcApi;
+use citrea_e2e::bitcoin::FINALITY_DEPTH;
+use citrea_e2e::config::TestCaseConfig;
 use citrea_e2e::framework::TestFramework;
 use citrea_e2e::test_case::{TestCase, TestCaseRunner};
 use citrea_e2e::traits::Restart;
@@ -146,8 +148,166 @@ impl TestCase for TestSequencerTransactionChaining {
 }
 
 #[tokio::test]
-async fn test_transaction_chaining() -> Result<()> {
+async fn test_sequencer_transaction_chaining() -> Result<()> {
     TestCaseRunner::new(TestSequencerTransactionChaining)
+        .set_citrea_path(get_citrea_path())
+        .run()
+        .await
+}
+
+struct TestProverTransactionChaining;
+
+impl TestProverTransactionChaining {
+    fn get_reveal_tx_input_value(&self, reveal_tx: &Transaction) -> Amount {
+        Amount::from_sat(reveal_tx.vsize() as u64 + REVEAL_OUTPUT_AMOUNT)
+    }
+}
+
+#[async_trait]
+impl TestCase for TestProverTransactionChaining {
+    fn test_config() -> TestCaseConfig {
+        TestCaseConfig {
+            with_batch_prover: true,
+            ..Default::default()
+        }
+    }
+
+    async fn run_test(&mut self, f: &mut TestFramework) -> Result<()> {
+        let sequencer = f.sequencer.as_mut().unwrap();
+        let batch_prover = f.batch_prover.as_mut().unwrap();
+        let da = f.bitcoin_nodes.get(0).expect("DA not running.");
+
+        let min_soft_confirmations_per_commitment =
+            sequencer.min_soft_confirmations_per_commitment();
+
+        for _ in 0..min_soft_confirmations_per_commitment {
+            sequencer.client.send_publish_batch_request().await?;
+        }
+
+        // Wait for blob tx to hit the mempool
+        da.wait_mempool_len(2, None).await?;
+
+        da.generate(FINALITY_DEPTH, None).await?;
+        let finalized_height = da.get_finalized_height().await?;
+
+        batch_prover
+            .wait_for_l1_height(finalized_height, None)
+            .await?;
+
+        da.generate(1, None).await?;
+        let block_height = da.get_block_count().await?;
+
+        // Get block holding prover txs
+        let block = da
+            .get_block(&da.get_block_hash(block_height).await?)
+            .await?;
+        let txs = &block.txdata;
+
+        assert_eq!(txs.len(), 3, "Block should contain exactly 3 transactions");
+
+        let _coinbase = &txs[0];
+        let tx2 = &txs[1];
+        let tx3 = &txs[2];
+
+        assert_eq!(
+            tx3.input[0].previous_output.txid,
+            tx2.compute_txid(),
+            "TX3 should reference TX2's output"
+        );
+
+        // Verify output values
+        assert_eq!(tx2.output[0].value, self.get_reveal_tx_input_value(tx3));
+        assert_eq!(tx3.output[0].value, Amount::from_sat(REVEAL_OUTPUT_AMOUNT));
+
+        // // Do another round and make sure second batch is chained from first batch
+        for _ in 0..min_soft_confirmations_per_commitment {
+            sequencer.client.send_publish_batch_request().await?;
+        }
+
+        // Wait for blob tx to hit the mempool
+        da.wait_mempool_len(2, None).await?;
+
+        da.generate(FINALITY_DEPTH, None).await?;
+        let finalized_height = da.get_finalized_height().await?;
+
+        batch_prover
+            .wait_for_l1_height(finalized_height, None)
+            .await?;
+
+        da.generate(1, None).await?;
+        let block_height = da.get_block_count().await?;
+
+        // Get block holding prover txs
+        let block = da
+            .get_block(&da.get_block_hash(block_height).await?)
+            .await?;
+        let txs = &block.txdata;
+
+        assert_eq!(txs.len(), 3, "Block should contain exactly 3 transactions");
+
+        let _coinbase = &txs[0];
+        let tx4 = &txs[1];
+        let tx5 = &txs[2];
+
+        assert_eq!(
+            tx5.input[0].previous_output.txid,
+            tx4.compute_txid(),
+            "TX3 should reference TX2's output"
+        );
+
+        // Verify output values
+        assert_eq!(tx4.output[0].value, self.get_reveal_tx_input_value(tx5));
+        assert_eq!(tx5.output[0].value, Amount::from_sat(REVEAL_OUTPUT_AMOUNT));
+
+        batch_prover.restart(None).await?;
+
+        // // Do another round post restart and make sure third batch is chained from second batch
+        for _ in 0..min_soft_confirmations_per_commitment {
+            sequencer.client.send_publish_batch_request().await?;
+        }
+
+        // Wait for blob tx to hit the mempool
+        da.wait_mempool_len(2, None).await?;
+
+        da.generate(FINALITY_DEPTH, None).await?;
+        let finalized_height = da.get_finalized_height().await?;
+
+        batch_prover
+            .wait_for_l1_height(finalized_height, None)
+            .await?;
+
+        da.generate(1, None).await?;
+        let block_height = da.get_block_count().await?;
+
+        // Get block holding prover txs
+        let block = da
+            .get_block(&da.get_block_hash(block_height).await?)
+            .await?;
+        let txs = &block.txdata;
+
+        assert_eq!(txs.len(), 3, "Block should contain exactly 3 transactions");
+
+        let _coinbase = &txs[0];
+        let tx6 = &txs[1];
+        let tx7 = &txs[2];
+
+        assert_eq!(
+            tx7.input[0].previous_output.txid,
+            tx6.compute_txid(),
+            "TX3 should reference TX2's output"
+        );
+
+        // Verify output values
+        assert_eq!(tx6.output[0].value, self.get_reveal_tx_input_value(tx7));
+        assert_eq!(tx7.output[0].value, Amount::from_sat(REVEAL_OUTPUT_AMOUNT));
+
+        Ok(())
+    }
+}
+
+#[tokio::test]
+async fn test_prover_transaction_chaining() -> Result<()> {
+    TestCaseRunner::new(TestProverTransactionChaining)
         .set_citrea_path(get_citrea_path())
         .run()
         .await
