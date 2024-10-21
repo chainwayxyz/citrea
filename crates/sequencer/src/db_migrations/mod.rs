ec68f17d95c448b52ce0a2de18cd0b13e69424cb
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-10-21 14:21:37 +0300

    Remove sequencer migration

--- crates/sequencer/src/db_migrations/mod.rs
+++ crates/sequencer/src/db_migrations/mod.rs
@@ -2,10 +2,8 @@ use std::sync::OnceLock;
 
 use sov_db::ledger_db::migrations::LedgerMigration;
 
-mod pruned_height;
-
 pub fn migrations() -> &'static Vec<Box<dyn LedgerMigration + Send + Sync + 'static>> {
     static MIGRATIONS: OnceLock<Vec<Box<dyn LedgerMigration + Send + Sync + 'static>>> =
         OnceLock::new();
-    MIGRATIONS.get_or_init(|| vec![Box::new(pruned_height::MigratePrunedL2Height {})])
+    MIGRATIONS.get_or_init(|| vec![])
 }
