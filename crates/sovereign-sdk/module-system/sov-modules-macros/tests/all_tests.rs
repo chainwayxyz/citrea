44f1d284c902f3887ae39c7c3a1c5551ef0a4efd
yaziciahmet <yaziciahmet991@gmail.com>
2024-10-29 12:27:47 +0100

    Fix removed sov-modules-macros test

--- crates/sovereign-sdk/module-system/sov-modules-macros/tests/all_tests.rs
+++ crates/sovereign-sdk/module-system/sov-modules-macros/tests/all_tests.rs
@@ -33,7 +33,6 @@ fn module_dispatch_tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/dispatch/derive_genesis.rs");
     t.pass("tests/dispatch/derive_dispatch.rs");
-    t.pass("tests/dispatch/derive_event.rs");
     t.compile_fail("tests/dispatch/missing_serialization.rs");
 }
 
