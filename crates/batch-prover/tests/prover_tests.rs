5a823e93735028a4323a4a57588e2bde84b3b25d
yaziciahmet <yaziciahmet991@gmail.com>
2024-11-05 00:13:44 +0700

    Fix prover tests

--- crates/batch-prover/tests/prover_tests.rs
+++ crates/batch-prover/tests/prover_tests.rs
@@ -1,254 +1,158 @@
-// use std::collections::VecDeque;
-// use std::sync::Arc;
+use std::collections::VecDeque;
+use std::sync::Arc;
 
-// use prover_services::ParallelProverService;
-// use sov_db::ledger_db::LedgerDB;
-// use sov_db::rocks_db_config::RocksdbConfig;
-// use sov_mock_da::{
-//     MockAddress, MockBlockHeader, MockDaService, MockDaSpec, MockDaVerifier, MockHash,
-//     MockValidityCond,
-// };
-// use sov_mock_zkvm::MockZkvm;
-// use sov_rollup_interface::da::Time;
-// use sov_rollup_interface::zk::StateTransitionData;
-// use sov_stf_runner::mock::MockStf;
-// use sov_stf_runner::{
-//     ProofProcessingStatus, ProverGuestRunConfig, ProverService, ProverServiceError,
-//     WitnessSubmissionStatus,
-// };
+use prover_services::{ParallelProverService, ProofGenMode};
+use sov_db::ledger_db::LedgerDB;
+use sov_db::rocks_db_config::RocksdbConfig;
+use sov_mock_da::{
+    MockAddress, MockBlockHeader, MockDaService, MockDaSpec, MockHash, MockValidityCond,
+};
+use sov_mock_zkvm::MockZkvm;
+use sov_modules_api::StateTransition;
+use sov_rollup_interface::da::Time;
+use sov_rollup_interface::zk::StateTransitionData;
+use sov_stf_runner::mock::MockStf;
+use sov_stf_runner::ProverService;
 
-// #[tokio::test(flavor = "multi_thread")]
-// async fn test_successful_prover_execution() -> Result<(), ProverServiceError> {
-//     let temp = tempfile::tempdir().unwrap();
+#[tokio::test(flavor = "multi_thread")]
+async fn test_successful_prover_execution() {
+    let TestProver {
+        prover_service, vm, ..
+    } = make_new_prover(1);
 
-//     let da_service = Arc::new(MockDaService::new(MockAddress::from([0; 32]), temp.path()));
+    let header_hash = MockHash::from([0; 32]);
+    prover_service
+        .add_proof_data((
+            borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
+            vec![],
+        ))
+        .await;
+    let proofs = prover_service.prove().await.unwrap();
+    vm.make_proof();
 
-//     let TestProver {
-//         prover_service, vm, ..
-//     } = make_new_prover();
+    prover_service.submit_proofs(proofs).await.unwrap();
+}
 
-//     let header_hash = MockHash::from([0; 32]);
-//     prover_service
-//         .submit_input(
-//             borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//             header_hash,
-//         )
-//         .await;
-//     prover_service.prove(header_hash).await?;
-//     vm.make_proof();
-//     prover_service
-//         .wait_for_proving_and_send_to_da(header_hash, &da_service)
-//         .await?;
+#[tokio::test(flavor = "multi_thread")]
+async fn test_parallel_proving_and_extract() {
+    let num_cpus = num_cpus::get();
+    let TestProver {
+        prover_service, vm, ..
+    } = make_new_prover(num_cpus);
 
-//     // The proof has already been sent, and the prover_service no longer has a reference to it.
-//     let err = prover_service
-//         .wait_for_proving_and_send_to_da(header_hash, &da_service)
-//         .await
-//         .unwrap_err();
+    let header_hash_1 = MockHash::from([0; 32]);
+    prover_service
+        .add_proof_data((
+            borsh::to_vec(&make_transition_data(header_hash_1)).unwrap(),
+            vec![],
+        ))
+        .await;
 
-//     assert_eq!(
-//         err.to_string(),
-//         "Missing witness for: 0x0000000000000000000000000000000000000000000000000000000000000000"
-//     );
+    let header_hash_2 = MockHash::from([1; 32]);
+    prover_service
+        .add_proof_data((
+            borsh::to_vec(&make_transition_data(header_hash_2)).unwrap(),
+            vec![],
+        ))
+        .await;
 
-//     Ok(())
-// }
+    let proofs = prover_service.prove().await.unwrap();
+    vm.make_proof();
 
-// #[tokio::test(flavor = "multi_thread")]
-// async fn test_prover_status_busy() -> Result<(), anyhow::Error> {
-//     let temp = tempfile::tempdir().unwrap();
-//     let da_service = Arc::new(MockDaService::new(MockAddress::from([0; 32]), temp.path()));
-//     let TestProver {
-//         prover_service,
-//         vm,
-//         num_worker_threads,
-//         ..
-//     } = make_new_prover();
+    let state_transitions = prover_service
+        .extract_output::<StateTransition<MockDaSpec, [u8; 0]>>(proofs)
+        .await
+        .unwrap();
+    assert_eq!(state_transitions.len(), 2);
+    assert_eq!(state_transitions[0].da_slot_hash, header_hash_1);
+    assert_eq!(state_transitions[1].da_slot_hash, header_hash_2);
+}
 
-//     let header_hashes = (1..num_worker_threads + 1).map(|hash| MockHash::from([hash as u8; 32]));
+#[tokio::test(flavor = "multi_thread")]
+async fn test_parallel_proving_and_submit() {
+    let num_cpus = num_cpus::get();
+    let TestProver {
+        prover_service, vm, ..
+    } = make_new_prover(num_cpus);
 
-//     // Saturate the prover.
-//     for header_hash in header_hashes.clone() {
-//         prover_service
-//             .submit_input(
-//                 borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//                 header_hash,
-//             )
-//             .await;
+    let header_hash_1 = MockHash::from([0; 32]);
+    prover_service
+        .add_proof_data((
+            borsh::to_vec(&make_transition_data(header_hash_1)).unwrap(),
+            vec![],
+        ))
+        .await;
 
-//         let poof_processing_status = prover_service.prove(header_hash).await?;
-//         assert_eq!(
-//             ProofProcessingStatus::ProvingInProgress,
-//             poof_processing_status
-//         );
-//     }
+    let header_hash_2 = MockHash::from([1; 32]);
+    prover_service
+        .add_proof_data((
+            borsh::to_vec(&make_transition_data(header_hash_2)).unwrap(),
+            vec![],
+        ))
+        .await;
 
-//     // Attempting to create another proof while the prover is busy.
-//     {
-//         let header_hash = MockHash::from([0; 32]);
-//         prover_service
-//             .submit_input(
-//                 borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//                 header_hash,
-//             )
-//             .await;
+    let proofs = prover_service.prove().await.unwrap();
+    vm.make_proof();
 
-//         let status = prover_service.prove(header_hash).await?;
-//         // The prover is busy and won't accept any new jobs.
-//         assert_eq!(ProofProcessingStatus::Busy, status);
+    let txs_and_proofs = prover_service.submit_proofs(proofs).await.unwrap();
+    assert_eq!(txs_and_proofs.len(), 2);
+}
 
-//         let proof_submission_status = prover_service
-//             .wait_for_proving_and_send_to_da(header_hash, &da_service)
-//             .await
-//             .unwrap_err();
+struct TestProver {
+    prover_service:
+        ParallelProverService<MockDaService, MockZkvm<MockValidityCond>, MockStf<MockValidityCond>>,
+    vm: MockZkvm<MockValidityCond>,
+}
 
-//         // The new job wasn't accepted.
-//         assert_eq!(
-//         proof_submission_status.to_string(),
-//         "Missing witness for: 0x0000000000000000000000000000000000000000000000000000000000000000");
-//     }
+fn make_new_prover(thread_pool_size: usize) -> TestProver {
+    let tmpdir = tempfile::tempdir().unwrap();
+    let da_service = Arc::new(MockDaService::new(
+        MockAddress::from([0; 32]),
+        tmpdir.path(),
+    ));
 
-//     vm.make_proof();
-//     for header_hash in header_hashes.clone() {
-//         prover_service
-//             .wait_for_proving_and_send_to_da(header_hash, &da_service)
-//             .await?;
-//     }
+    let vm = MockZkvm::new(MockValidityCond::default());
+    let proof_mode = ProofGenMode::Execute;
 
-//     // Retry once the prover is available to process new proofs.
-//     {
-//         let header_hash = MockHash::from([(num_worker_threads + 1) as u8; 32]);
-//         prover_service
-//             .submit_input(
-//                 borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//                 header_hash,
-//             )
-//             .await;
+    let tmpdir = tempfile::tempdir().unwrap();
+    let ledger_db = LedgerDB::with_config(&RocksdbConfig::new(tmpdir.path(), None)).unwrap();
+    TestProver {
+        prover_service: ParallelProverService::new(
+            da_service,
+            vm.clone(),
+            proof_mode,
+            (),
+            thread_pool_size,
+            ledger_db,
+        )
+        .expect("Should be able to instantiate Prover service"),
+        vm,
+    }
+}
 
-//         let status = prover_service.prove(header_hash).await?;
-//         assert_eq!(ProofProcessingStatus::ProvingInProgress, status);
-//     }
-
-//     Ok(())
-// }
-
-// #[tokio::test]
-// async fn test_missing_witness() -> Result<(), anyhow::Error> {
-//     let TestProver { prover_service, .. } = make_new_prover();
-//     let header_hash = MockHash::from([0; 32]);
-//     let err = prover_service.prove(header_hash).await.unwrap_err();
-
-//     assert_eq!(
-//         err.to_string(),
-//         "Missing witness for block: 0x0000000000000000000000000000000000000000000000000000000000000000"
-//     );
-//     Ok(())
-// }
-
-// #[tokio::test]
-// async fn test_multiple_witness_submissions() -> Result<(), anyhow::Error> {
-//     let TestProver { prover_service, .. } = make_new_prover();
-
-//     let header_hash = MockHash::from([0; 32]);
-//     let submission_status = prover_service
-//         .submit_input(
-//             borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//             header_hash,
-//         )
-//         .await;
-
-//     assert_eq!(
-//         WitnessSubmissionStatus::SubmittedForProving,
-//         submission_status
-//     );
-
-//     let submission_status = prover_service
-//         .submit_input(
-//             borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//             header_hash,
-//         )
-//         .await;
-
-//     assert_eq!(WitnessSubmissionStatus::WitnessExist, submission_status);
-
-//     Ok(())
-// }
-
-// #[tokio::test]
-// async fn test_generate_multiple_proofs_for_the_same_witness() -> Result<(), anyhow::Error> {
-//     let TestProver { prover_service, .. } = make_new_prover();
-
-//     let header_hash = MockHash::from([0; 32]);
-//     prover_service
-//         .submit_input(
-//             borsh::to_vec(&make_transition_data(header_hash)).unwrap(),
-//             header_hash,
-//         )
-//         .await;
-
-//     let status = prover_service.prove(header_hash).await?;
-//     assert_eq!(ProofProcessingStatus::ProvingInProgress, status);
-
-//     let err = prover_service.prove(header_hash).await.unwrap_err();
-//     assert_eq!(err.to_string(), "Proof generation for 0x0000000000000000000000000000000000000000000000000000000000000000 still in progress");
-//     Ok(())
-// }
-
-// struct TestProver {
-//     prover_service:
-//         ParallelProverService<MockDaService, MockZkvm<MockValidityCond>, MockStf<MockValidityCond>>,
-//     vm: MockZkvm<MockValidityCond>,
-//     num_worker_threads: usize,
-// }
-
-// fn make_new_prover() -> TestProver {
-//     let num_threads = num_cpus::get();
-//     let vm = MockZkvm::new(MockValidityCond::default());
-
-//     let prover_config = ProverGuestRunConfig::Execute;
-//     let zk_stf = MockStf::<MockValidityCond>::default();
-//     let da_verifier = MockDaVerifier::default();
-//     let tmpdir = tempfile::tempdir().unwrap();
-//     let ledger_db = LedgerDB::with_config(&RocksdbConfig::new(tmpdir.path(), None)).unwrap();
-//     TestProver {
-//         prover_service: ParallelProverService::new(
-//             vm.clone(),
-//             zk_stf,
-//             da_verifier,
-//             prover_config,
-//             (),
-//             num_threads,
-//             ledger_db,
-//         )
-//         .expect("Should be able to instantiate Prover service"),
-//         vm,
-//         num_worker_threads: num_threads,
-//     }
-// }
-
-// fn make_transition_data(
-//     header_hash: MockHash,
-// ) -> StateTransitionData<[u8; 0], Vec<u8>, MockDaSpec> {
-//     StateTransitionData {
-//         initial_state_root: [],
-//         final_state_root: [],
-//         initial_batch_hash: [0; 32],
-//         inclusion_proof: [0; 32],
-//         completeness_proof: (),
-//         da_data: vec![],
-//         sequencer_commitments_range: (0, 0),
-//         da_block_header_of_commitments: MockBlockHeader {
-//             prev_hash: [0; 32].into(),
-//             hash: header_hash,
-//             txs_commitment: header_hash,
-//             height: 0,
-//             time: Time::now(),
-//         },
-//         soft_confirmations: VecDeque::new(),
-//         state_transition_witnesses: VecDeque::new(),
-//         da_block_headers_of_soft_confirmations: VecDeque::new(),
-//         sequencer_public_key: vec![],
-//         sequencer_da_public_key: vec![],
-//         preproven_commitments: vec![],
-//     }
-// }
+fn make_transition_data(
+    header_hash: MockHash,
+) -> StateTransitionData<[u8; 0], Vec<u8>, MockDaSpec> {
+    StateTransitionData {
+        initial_state_root: [],
+        final_state_root: [],
+        initial_batch_hash: [0; 32],
+        inclusion_proof: [0; 32],
+        completeness_proof: (),
+        da_data: vec![],
+        sequencer_commitments_range: (0, 0),
+        da_block_header_of_commitments: MockBlockHeader {
+            prev_hash: [0; 32].into(),
+            hash: header_hash,
+            txs_commitment: header_hash,
+            height: 0,
+            time: Time::now(),
+        },
+        soft_confirmations: VecDeque::new(),
+        state_transition_witnesses: VecDeque::new(),
+        da_block_headers_of_soft_confirmations: VecDeque::new(),
+        sequencer_public_key: vec![],
+        sequencer_da_public_key: vec![],
+        preproven_commitments: vec![],
+    }
+}
