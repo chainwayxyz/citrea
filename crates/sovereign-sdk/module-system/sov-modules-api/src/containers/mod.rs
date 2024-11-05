7b1e69670cfebdf3d22a6c4dcb86a58d8d2433b1
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-11-06 02:46:26 +0700

    Remove offchain value and vec

--- crates/sovereign-sdk/module-system/sov-modules-api/src/containers/mod.rs
+++ crates/sovereign-sdk/module-system/sov-modules-api/src/containers/mod.rs
@@ -3,8 +3,6 @@ mod accessory_value;
 mod accessory_vec;
 
 mod offchain_map;
-mod offchain_value;
-mod offchain_vec;
 
 mod kernel_value;
 mod versioned_value;
@@ -20,8 +18,6 @@ pub use accessory_vec::AccessoryStateVec;
 pub use kernel_value::KernelStateValue;
 pub use map::StateMap;
 pub use offchain_map::OffchainStateMap;
-pub use offchain_value::OffchainStateValue;
-pub use offchain_vec::OffchainStateVec;
 pub use traits::{
     StateMapAccessor, StateMapError, StateValueAccessor, StateValueError, StateVecAccessor,
     StateVecError,
