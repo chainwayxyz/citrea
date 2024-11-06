77678803f0b22b3eee067da147fdde09b81e93a6
yaziciahmet <yaziciahmet991@gmail.com>
2024-11-06 17:48:37 +0700

    Use match statement

--- crates/light-client-prover/src/circuit.rs
+++ crates/light-client-prover/src/circuit.rs
@@ -5,6 +5,17 @@ use sov_modules_api::{BatchProofCircuitOutput, BlobReaderTrait, DaSpec};
 use sov_rollup_interface::da::{DaDataLightClient, DaNamespace, DaVerifier};
 use sov_rollup_interface::zk::{LightClientCircuitInput, LightClientCircuitOutput, ZkvmGuest};
 
+/// The maximum target value, which corresponds to the minimum difficulty
+const MAX_TARGET: U256 =
+    U256::from_be_hex("00000000FFFF0000000000000000000000000000000000000000000000000000");
+
+/// An epoch should be two weeks (represented as number of seconds)
+/// seconds/minute * minutes/hour * hours/day * 14 days
+const EXPECTED_EPOCH_TIMESPAN: u32 = 60 * 60 * 24 * 14;
+
+/// Number of blocks per epoch
+const BLOCKS_PER_EPOCH: u64 = 2016;
+
 #[derive(Debug)]
 pub enum LightClientVerificationError {
     DaTxsCouldntBeVerified,
@@ -16,7 +27,25 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
 ) -> Result<LightClientCircuitOutput<DaV::Spec>, LightClientVerificationError> {
     let input: LightClientCircuitInput<DaV::Spec> = guest.read_from_host();
 
-    let previous_light_client_proof_output = verify_header_chain::<_, G>(&input);
+    // Extract previous light client proof output
+    let previous_light_client_proof_output =
+        input.previous_light_client_proof_journal.map(|journal| {
+            let prev_output = G::verify_and_extract_output::<LightClientCircuitOutput<DaV::Spec>>(
+                &journal,
+                &input.light_client_proof_method_id.into(),
+            )
+            .expect("Got invalid previous light client proof");
+            // Method ids match
+            assert_eq!(
+                input.light_client_proof_method_id,
+                prev_output.light_client_proof_method_id,
+            );
+
+            prev_output
+        });
+
+    let block_updates =
+        verify_da_block(&previous_light_client_proof_output, &input.da_block_header);
 
     // Verify data from da
     let _validity_condition = da_verifier
@@ -33,8 +62,8 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
     // https://github.com/chainwayxyz/citrea/issues/1401
     let batch_proof_method_id = input.batch_proof_method_id;
     // Parse the batch proof da data
-    // TODO: We are currently assuming batch proofs are ordered. Erce pr will handle that so I am currently ignoring that case.
-    for (idx, blob) in input.da_data.iter().enumerate() {
+    // TODO: We are currently assuming batch proofs are ordered. Erce's pr will handle that so I am currently ignoring that case.
+    for blob in input.da_data {
         if blob.sender().as_ref() == input.batch_prover_da_pub_key {
             let data = DaDataLightClient::try_from_slice(blob.verified_data());
 
@@ -43,22 +72,11 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
                     DaDataLightClient::Complete(proof) => {
                         let journal =
                             G::extract_raw_output(&proof).expect("DaData proofs must be valid");
-                        let batch_proof_output: BatchProofCircuitOutput<DaV::Spec, [u8; 32]> =
+                        let _batch_proof_output: BatchProofCircuitOutput<DaV::Spec, [u8; 32]> =
                             G::verify_and_extract_output(&journal, &batch_proof_method_id.into())
                                 .expect("Batch proof could not be verified");
 
-                        if idx == 0 {
-                            if let Some(ref previous_light_client_proof_output) =
-                                previous_light_client_proof_output
-                            {
-                                // If this is the first batch proof we need to verify that
-                                // previous light client proof output state root matches starting batch proof state root
-                                assert_eq!(
-                                    previous_light_client_proof_output.state_root,
-                                    batch_proof_output.initial_state_root
-                                );
-                            }
-                        }
+                        // TODO: do necessary validations
                     }
                     DaDataLightClient::Aggregate(_) => todo!(),
                     DaDataLightClient::Chunk(_) => todo!(),
@@ -67,52 +85,56 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
         }
     }
 
-    // do what you want with proofs
-    // complete proof has raw bytes inside
-    // to extract *and* verify the proof you need to use the zk guest
-    // can be passed from the guest code to this function
-
     Ok(LightClientCircuitOutput {
         state_root: [1; 32],
         light_client_proof_method_id: input.light_client_proof_method_id,
-        da_block_hash: input.da_block_header.hash(),
-        da_block_height: 0,
-        da_total_work: [0; 32],
-        da_current_target_bits: 0,
-        da_epoch_start_time: 0,
-        da_prev_11_timestamps: [0; 11],
+        da_block_hash: block_updates.hash,
+        da_block_height: block_updates.height,
+        da_total_work: block_updates.total_work,
+        da_current_target_bits: block_updates.current_target_bits,
+        da_epoch_start_time: block_updates.epoch_start_time,
+        da_prev_11_timestamps: block_updates.prev_11_timestamps,
     })
 
     // First
 }
 
-// Verify header chain. Returns `None` if this is the first light client proof.
-fn verify_header_chain<Spec: DaSpec, G: ZkvmGuest>(
-    input: &LightClientCircuitInput<Spec>,
-) -> Option<LightClientCircuitOutput<Spec>> {
-    let da_block_header = &input.da_block_header;
+struct BlockUpdates<Spec: DaSpec> {
+    hash: Spec::SlotHash,
+    height: u64,
+    total_work: [u8; 32],
+    epoch_start_time: u32,
+    prev_11_timestamps: [u32; 11],
+    current_target_bits: u32,
+}
 
-    // Immediately verify current block header hash
+fn verify_da_block<Spec: DaSpec>(
+    previous_light_client_proof_output: &Option<LightClientCircuitOutput<Spec>>,
+    da_block_header: &Spec::BlockHeader,
+) -> BlockUpdates<Spec> {
+    // Check 1: Verify block hash
     assert!(da_block_header.verify_hash());
 
-    // TODO: hardcode the first DA block and verify it
-    let Some(previous_light_client_proof_journal) = &input.previous_light_client_proof_journal
-    else {
-        return None;
+    let target = bits_to_target(da_block_header.bits());
+    let work_add = target_to_work(&target);
+
+    // TODO: this is first light client proof, hardcode the first da block and verify accordingly
+    let Some(previous_light_client_proof_output) = previous_light_client_proof_output else {
+        // First light client proof must start from the first epoch block
+        // TODO: think about it?
+        assert!(da_block_header.height() % BLOCKS_PER_EPOCH == 0);
+        return BlockUpdates {
+            hash: da_block_header.hash(),
+            height: da_block_header.height(),
+            // TODO: total work should be the hardcoded initial block's total_work + work_add
+            total_work: work_add.to_be_bytes(),
+            epoch_start_time: da_block_header.time().secs() as u32,
+            // TODO: what are we gonna do idk yet
+            prev_11_timestamps: [0; 11],
+            current_target_bits: da_block_header.bits(),
+        };
     };
 
-    let previous_light_client_proof_output =
-        G::verify_and_extract_output::<LightClientCircuitOutput<Spec>>(
-            previous_light_client_proof_journal,
-            &input.light_client_proof_method_id.into(),
-        )
-        .expect("Got invalid previous light client proof");
-
-    // Check 1: method ids match
-    assert_eq!(
-        input.light_client_proof_method_id,
-        previous_light_client_proof_output.light_client_proof_method_id
-    );
     // Check 2: block heights are consecutive
     assert_eq!(
         da_block_header.height() - 1,
@@ -128,17 +150,49 @@ fn verify_header_chain<Spec: DaSpec, G: ZkvmGuest>(
         da_block_header.bits(),
         previous_light_client_proof_output.da_current_target_bits
     );
-    // Check 5: valid timestamp
+    // Check 5: proof of work
+    assert!(verify_target_hash(da_block_header.hash().into(), target));
+    // Check 6: valid timestamp
     assert!(verify_timestamp(
         da_block_header.time().secs() as u32,
         previous_light_client_proof_output.da_prev_11_timestamps
     ));
 
-    let target = bits_to_target(previous_light_client_proof_output.da_current_target_bits);
-    // Check 6: proof of work
-    assert!(verify_target_hash(da_block_header.hash().into(), target));
+    let epoch_block = da_block_header.height() % BLOCKS_PER_EPOCH;
+    // Check if this is epoch block, and update time accordingly
+    let mut epoch_start_time = previous_light_client_proof_output.da_epoch_start_time;
+    if epoch_block == 0 {
+        epoch_start_time = da_block_header.time().secs() as u32;
+    }
 
-    Some(previous_light_client_proof_output)
+    // Update previous timestamps
+    let mut prev_11_timestamps = previous_light_client_proof_output.da_prev_11_timestamps;
+    prev_11_timestamps[da_block_header.height() as usize % 11] =
+        da_block_header.time().secs() as u32;
+
+    // If the next block is epoch start block, calculate the next epoch's difficulty target
+    let mut current_target_bits = da_block_header.bits();
+    if epoch_block == BLOCKS_PER_EPOCH - 1 {
+        let next_target = calculate_new_difficulty(
+            epoch_start_time,
+            da_block_header.time().secs() as u32,
+            da_block_header.bits(),
+        );
+        current_target_bits = target_to_bits(&next_target);
+    }
+
+    let total_work = U256::from_be_bytes(previous_light_client_proof_output.da_total_work)
+        .saturating_add(&work_add)
+        .to_be_bytes();
+
+    BlockUpdates {
+        hash: da_block_header.hash(),
+        height: da_block_header.height(),
+        total_work,
+        epoch_start_time,
+        prev_11_timestamps,
+        current_target_bits,
+    }
 }
 
 /// Verifies the block time against the median of the previous 11 blocks' timestamps
@@ -154,16 +208,12 @@ fn verify_timestamp(block_time: u32, mut prev_11_timestamps: [u32; 11]) -> bool 
 /// `hash` is the hash in little-endian byte order.
 fn verify_target_hash(hash: [u8; 32], target_bytes: [u8; 32]) -> bool {
     for i in 0..32 {
-        if hash[31 - i] < target_bytes[i] {
-            // The hash is valid because a byte in hash is less than the corresponding byte in target
-            return true;
-        } else if hash[31 - i] > target_bytes[i] {
-            // The hash is invalid because a byte in hash is greater than the corresponding byte in target
-            return false;
+        match hash[31 - i].cmp(&target_bytes[i]) {
+            std::cmp::Ordering::Less => return true,     // Hash is valid
+            std::cmp::Ordering::Greater => return false, // Hash is invalid
+            std::cmp::Ordering::Equal => continue,       // Continue to the next byte if equal
         }
-        // If the bytes are equal, continue to the next byte
     }
-
     true
 }
 
@@ -196,6 +246,19 @@ fn bits_to_target(bits: u32) -> [u8; 32] {
     target.to_be_bytes()
 }
 
+/// Converts the big-endian target value to the little-endian `bits` field of a block header.
+fn target_to_bits(target: &[u8; 32]) -> u32 {
+    let target_u256 = U256::from_be_slice(target);
+    let target_bits = target_u256.bits();
+    let size = (263 - target_bits) / 8;
+    let mut compact_target = [0u8; 4];
+    compact_target[0] = 33 - size as u8;
+    compact_target[1] = target[size - 1_usize];
+    compact_target[2] = target[size];
+    compact_target[3] = target[size + 1_usize];
+    u32::from_be_bytes(compact_target)
+}
+
 /// Calculates the work done for a block hash that satisfies a given.
 /// Should use the `bits` field of the block header to calculate the target.
 fn target_to_work(target: &[u8; 32]) -> U256 {
@@ -203,4 +266,30 @@ fn target_to_work(target: &[u8; 32]) -> U256 {
     let target_plus_one = target.saturating_add(&U256::ONE);
 
     U256::MAX.wrapping_div(&target_plus_one)
+}
+
+/// Calculates the new difficulty target for the next epoch.
+fn calculate_new_difficulty(
+    epoch_start_time: u32,
+    last_timestamp: u32,
+    current_target: u32,
+) -> [u8; 32] {
+    // Step 1: Calculate the actual timespan of the epoch
+    let mut actual_timespan = last_timestamp - epoch_start_time;
+    if actual_timespan < EXPECTED_EPOCH_TIMESPAN / 4 {
+        actual_timespan = EXPECTED_EPOCH_TIMESPAN / 4;
+    } else if actual_timespan > EXPECTED_EPOCH_TIMESPAN * 4 {
+        actual_timespan = EXPECTED_EPOCH_TIMESPAN * 4;
+    }
+    // Step 2: Calculate the new target
+    let new_target_bytes = bits_to_target(current_target);
+    let mut new_target = U256::from_be_bytes(new_target_bytes)
+        .wrapping_mul(&U256::from(actual_timespan))
+        .wrapping_div(&U256::from(EXPECTED_EPOCH_TIMESPAN));
+    // Step 3: Clamp the new target to the maximum target
+    if new_target > MAX_TARGET {
+        new_target = MAX_TARGET;
+    }
+
+    new_target.to_be_bytes()
 }
