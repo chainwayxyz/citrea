fe8fa508765a5b15a9f17f74fcfde00c28ad0eb5
Erce Can Bektüre <canerce99@hotmail.com>
2024-11-06 15:09:12 +0700

    Recursively chain outputs

--- crates/light-client-prover/src/circuit.rs
+++ crates/light-client-prover/src/circuit.rs
@@ -79,11 +79,6 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
         })
         .collect();
 
-    // If we have a previous light client proof, check they can be chained
-    // If not, skip for now
-    // TODO: Once we have a manually planted light client proof use that and assume prev light client proof always exists
-    // So there will be no need for all these checks
-
     // Mapping from initial state root to final state root and last L2 height
     let mut initial_to_final = std::collections::BTreeMap::<[u8; 32], ([u8; 32], u64)>::new();
 
@@ -92,75 +87,67 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
     let mut last_state_root = [0u8; 32];
     let mut last_l2_height = 0;
 
+    // If we have a previous light client proof, check they can be chained
+    // If not, skip for now
+    // TODO: Once we have a manually planted light client proof use that and assume prev light client proof always exists
+    // So there will be no need for all these if lets
     if let Some(previous_output) = &deserialized_previous_light_client_proof_journal {
+        last_l2_height = previous_output.last_l2_height;
+        last_state_root = previous_output.state_root;
         for unverified_info in previous_output.unverified_batch_proofs_info.iter() {
             if unverified_info.last_l2_height <= previous_output.last_l2_height {
                 continue;
             }
-            if let Some((final_root, last_l2)) =
-                initial_to_final.remove(&unverified_info.final_state_root)
-            {
-                initial_to_final
-                    .entry(unverified_info.initial_state_root)
-                    .or_insert((final_root, last_l2));
-            }
+            // Add them directly as they are the ones that could not be matched
+            initial_to_final.insert(
+                unverified_info.initial_state_root,
+                (
+                    unverified_info.final_state_root,
+                    unverified_info.last_l2_height,
+                ),
+            );
         }
 
         for output in deserialized_outputs.iter() {
             if output.last_l2_height <= previous_output.last_l2_height {
                 continue;
             }
-
-            if let Some((final_root, last_l2)) = initial_to_final.remove(&output.final_state_root) {
-                initial_to_final
-                    .entry(output.initial_state_root)
-                    .or_insert((final_root, last_l2));
-            }
+            recursive_match_state_roots(
+                &mut initial_to_final,
+                &BatchProofInfo::new(
+                    previous_output.state_root,
+                    output.final_state_root,
+                    output.last_l2_height,
+                ),
+            );
         }
 
-        // Collect the entries into a vector to avoid multiple borrows during iteration
-        let entries: Vec<(_, (_, _))> = initial_to_final
-            .iter()
-            .map(|(k, v)| (*k, (v.0, v.1)))
-            .collect();
-
-        for (initial_root, (final_root, last_l2)) in entries {
-            // Directly remove the entry to ensure there is no simultaneous mutable and immutable borrow
-            if let Some((final_root_value, last_l2_value)) = initial_to_final.remove(&final_root) {
-                // Insert the new entry after removing to avoid mutable borrow issues
-                initial_to_final.insert(initial_root, (final_root_value, last_l2_value));
-            } else {
-                // If there is no entry for the final root, create a new `BatchProofInfo` and push it
-                let unverified_info = BatchProofInfo {
-                    initial_state_root: initial_root,
-                    final_state_root: final_root,
-                    last_l2_height: last_l2,
-                };
-                unverified_outputs.push(unverified_info);
-            }
+        // Do recursive matching for previous state root
+        recursive_match_state_roots(
+            &mut initial_to_final,
+            &BatchProofInfo::new(
+                previous_output.state_root,
+                previous_output.state_root,
+                previous_output.last_l2_height,
+            ),
+        );
+        // Now only thing left is the state update if exists and others are unverified
+        if let Some((final_root, last_l2)) = initial_to_final.remove(&previous_output.state_root) {
+            last_l2_height = last_l2;
+            last_state_root = final_root;
         }
 
-        // Check if the final state root of the prev light client proof matches the initial state root of the first batch proof
-        if let Some((final_root, last_l2)) = initial_to_final.get(&previous_output.state_root) {
-            // Update the last L2 height and state root
-            last_state_root = *final_root;
-            last_l2_height = *last_l2;
-        } else {
-            // Batch proof(s) missing, chain is broken
-            // Push the latest roots to unverified outputs
-            last_state_root = previous_output.state_root;
-            last_l2_height = previous_output.last_l2_height;
-
-            // Throw the remaining roots into unverified outputs
-            for (initial_root, (final_root, last_l2)) in initial_to_final.iter() {
-                let unverified_info = BatchProofInfo {
-                    initial_state_root: *initial_root,
-                    final_state_root: *final_root,
-                    last_l2_height: *last_l2,
-                };
-                unverified_outputs.push(unverified_info);
-            }
-        }
+        // Collect unverified outputs
+        unverified_outputs = initial_to_final
+            .into_iter()
+            .map(
+                |(initial_state_root, (final_state_root, last_l2_height))| BatchProofInfo {
+                    initial_state_root,
+                    final_state_root,
+                    last_l2_height,
+                },
+            )
+            .collect::<Vec<_>>();
     }
 
     Ok(LightClientCircuitOutput {
@@ -169,4 +156,96 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
         unverified_batch_proofs_info: unverified_outputs,
         last_l2_height,
     })
+}
+
+fn recursive_match_state_roots(
+    initial_to_final: &mut std::collections::BTreeMap<[u8; 32], ([u8; 32], u64)>,
+    bp_info: &BatchProofInfo,
+) {
+    if let Some((final_root, last_l2)) = initial_to_final.remove(&bp_info.final_state_root) {
+        recursive_match_state_roots(
+            initial_to_final,
+            &BatchProofInfo::new(bp_info.initial_state_root, final_root, last_l2),
+        );
+    } else {
+        initial_to_final.insert(
+            bp_info.initial_state_root,
+            (bp_info.final_state_root, bp_info.last_l2_height),
+        );
+    }
+}
+
+#[test]
+fn test_recursive_match_state_roots() {
+    let mut initial_to_final = std::collections::BTreeMap::<[u8; 32], ([u8; 32], u64)>::new();
+
+    let bp_info = BatchProofInfo::new([2u8; 32], [3u8; 32], 1);
+    initial_to_final.insert([1u8; 32], ([2u8; 32], 1));
+    initial_to_final.insert([3u8; 32], ([4u8; 32], 3));
+    initial_to_final.insert([4u8; 32], ([5u8; 32], 4));
+    // First of all this should chain 2 to 5 from 2-3 -> 3-4 -> 4-5
+    recursive_match_state_roots(&mut initial_to_final, &bp_info);
+
+    assert_eq!(initial_to_final.len(), 2);
+
+    let first = initial_to_final.get(&[1u8; 32]).unwrap();
+    assert_eq!(first.0, [2u8; 32]);
+
+    let second = initial_to_final.get(&[2u8; 32]).unwrap();
+    assert_eq!(second.0, [5u8; 32]);
+
+    let bp_info_prev_state = BatchProofInfo::new([1u8; 32], [1u8; 32], 0);
+
+    recursive_match_state_roots(&mut initial_to_final, &bp_info_prev_state);
+
+    let only = initial_to_final.get(&[1u8; 32]).unwrap();
+    assert_eq!(only.0, [5u8; 32]);
+
+    assert_eq!(initial_to_final.len(), 1);
+}
+
+#[test]
+fn test_recursive_match_state_roots_empty_btreemap() {
+    let mut initial_to_final = std::collections::BTreeMap::<[u8; 32], ([u8; 32], u64)>::new();
+
+    let bp_info = BatchProofInfo::new([2u8; 32], [3u8; 32], 1);
+    // First of all this should chain 2 to 5 from 2-3 -> 3-4 -> 4-5
+    recursive_match_state_roots(&mut initial_to_final, &bp_info);
+
+    assert_eq!(initial_to_final.len(), 1);
+
+    let first = initial_to_final.get(&[2u8; 32]).unwrap();
+    assert_eq!(first.0, [3u8; 32]);
+}
+
+#[test]
+fn test_recursive_match_state_roots_with_unchainable_elements() {
+    let mut initial_to_final = std::collections::BTreeMap::<[u8; 32], ([u8; 32], u64)>::new();
+
+    let bp_info = BatchProofInfo::new([45u8; 32], [46u8; 32], 45);
+    initial_to_final.insert([1u8; 32], ([2u8; 32], 1));
+    initial_to_final.insert([3u8; 32], ([5u8; 32], 3));
+    initial_to_final.insert([6u8; 32], ([7u8; 32], 6));
+
+    recursive_match_state_roots(&mut initial_to_final, &bp_info);
+
+    assert_eq!(initial_to_final.len(), 4);
+
+    let first = initial_to_final.get(&[1u8; 32]).unwrap();
+    assert_eq!(first.0, [2u8; 32]);
+
+    let none = initial_to_final.get(&[2u8; 32]);
+    assert!(none.is_none());
+
+    let second = initial_to_final.get(&[3u8; 32]).unwrap();
+    assert_eq!(second.0, [5u8; 32]);
+    assert_eq!(second.1, 3);
+
+    let third = initial_to_final.get(&[6u8; 32]).unwrap();
+    assert_eq!(third.0, [7u8; 32]);
+    assert_eq!(third.1, 6);
+
+    let fourth = initial_to_final.get(&[45u8; 32]).unwrap();
+    assert_eq!(fourth.0, [46u8; 32]);
+    assert_eq!(fourth.1, 45);
 }
