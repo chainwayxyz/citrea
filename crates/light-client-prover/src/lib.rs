8fc42667b9dee2e2e796be678a8a3308f4ac07c1
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-11-08 18:22:00 +0700

    Add migrations to light client

--- crates/light-client-prover/src/lib.rs
+++ crates/light-client-prover/src/lib.rs
@@ -2,5 +2,7 @@ pub mod circuit;
 #[cfg(feature = "native")]
 pub mod da_block_handler;
 #[cfg(feature = "native")]
+pub mod db_migrations;
+#[cfg(feature = "native")]
 pub mod runner;
 pub(crate) mod utils;
