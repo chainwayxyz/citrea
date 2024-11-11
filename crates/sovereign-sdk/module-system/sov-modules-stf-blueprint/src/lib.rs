5d29a053eada28d7e6cadd9d2a415b07d0e14725
yaziciahmet <yaziciahmet991@gmail.com>
2024-11-11 13:36:48 +0700

    Please fix comments already

--- crates/sovereign-sdk/module-system/sov-modules-stf-blueprint/src/lib.rs
+++ crates/sovereign-sdk/module-system/sov-modules-stf-blueprint/src/lib.rs
@@ -595,8 +595,8 @@ where
         sequencer_commitments.sort_unstable();
 
         // The preproven indices are sorted by the prover when originally passed.
-         // Therefore, we can iterate of sequencer commitments and filter out
-         // matching preproven indices.
+        // Therefore, we can iterate of sequencer commitments and filter out
+        // matching preproven indices.
         let mut preproven_commitments_iter = preproven_commitment_indices.into_iter().peekable();
         let sequencer_commitments_iter = sequencer_commitments
             .into_iter()
