0bcd68bd00e3f60cbfbb19916ec6bb2bbffdb0f3
yaziciahmet <yaziciahmet991@gmail.com>
2024-11-19 10:11:16 +0100

    Lint

--- crates/bitcoin-da/src/verifier.rs
+++ crates/bitcoin-da/src/verifier.rs
@@ -90,7 +90,7 @@ impl DaVerifier for BitcoinVerifier {
             .wtxids
             .iter()
             .filter(|wtxid| wtxid.starts_with(prefix));
-for (wtxid, tx) in relevant_wtxid_iter.zip_eq(&completeness_proof) {
+        for (wtxid, tx) in relevant_wtxid_iter.zip_eq(&completeness_proof) {
             // ensure completeness proof tx matches the inclusion tx
             if tx.compute_wtxid().as_byte_array() != wtxid {
                 return Err(ValidationError::RelevantTxNotInProof);
