3234d7b830f2f37b714be57bc728f697412db183
Erce Can Bektüre <canerce99@hotmail.com>
2024-11-19 20:41:14 +0300

    Lint

--- crates/evm/src/evm/db_commit.rs
+++ crates/evm/src/evm/db_commit.rs
@@ -80,10 +80,8 @@ impl<'a, C: sov_modules_api::Context> DatabaseCommit for EvmDb<'a, C> {
             for (key, value) in storage_slots.into_iter() {
                 let value = value.present_value();
                 // If cancun is enabled there is no need to add the keys because they will not be deleted
-                if !self.current_spec.is_enabled_in(SpecId::CANCUN) {
-                    if db_account.storage.get(&key, self.working_set).is_none() {
-                        db_account.keys.push(&key, self.working_set);
-                    }
+                if !self.current_spec.is_enabled_in(SpecId::CANCUN) && db_account.storage.get(&key, self.working_set).is_none() {
+                    db_account.keys.push(&key, self.working_set);
                 }
                 db_account.storage.set(&key, &value, self.working_set);
             }
