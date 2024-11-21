21007c7882d13e83a0be5de2fa40f0a17be18b0b
eyusufatik <esadyusufatik@gmail.com>
2024-11-21 13:54:09 +0300

    extract necessary info for test_mempool_accept regarding prevouts & fix no decompression in chunked tx proof extraction

--- crates/bitcoin-da/src/service.rs
+++ crates/bitcoin-da/src/service.rs
@@ -4,6 +4,7 @@
 use core::result::Result::Ok;
 use core::str::FromStr;
 use core::time::Duration;
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -14,9 +15,10 @@ use backoff::ExponentialBackoff;
 use bitcoin::block::Header;
 use bitcoin::consensus::{encode, Decodable};
 use bitcoin::hashes::Hash;
+use bitcoin::psbt::Input;
 use bitcoin::secp256k1::SecretKey;
 use bitcoin::{Amount, BlockHash, CompactTarget, Transaction, Txid, Wtxid};
-use bitcoincore_rpc::json::TestMempoolAcceptResult;
+use bitcoincore_rpc::json::{SignRawTransactionInput, TestMempoolAcceptResult};
 use bitcoincore_rpc::{Auth, Client, Error, RpcApi, RpcError};
 use borsh::BorshDeserialize;
 use citrea_primitives::compression::{compress_blob, decompress_blob};
@@ -424,27 +426,87 @@ impl BitcoinService {
     ) -> Result<Vec<Txid>> {
         debug!("Sending chunked transaction");
         let mut raw_txs = Vec::with_capacity(commit_chunks.len() * 2 + 2);
+        assert_eq!(
+            commit_chunks.len(),
+            reveal_chunks.len(),
+            "Chunks commit and reveal mismatch"
+        );
 
-        for (commit, reveal) in commit_chunks.into_iter().zip(reveal_chunks) {
+        let mut all_tx_map = HashMap::new();
+
+        commit_chunks.iter().for_each(|c| {
+            all_tx_map.insert(c.compute_txid(), c.clone());
+            ()
+        });
+
+        reveal_chunks.iter().for_each(|r| {
+            all_tx_map.insert(r.compute_txid(), r.clone());
+            ()
+        });
+
+        all_tx_map.insert(commit.compute_txid(), commit.clone());
+        all_tx_map.insert(reveal.tx.compute_txid(), reveal.tx.clone());
+
+        for i in 0..commit_chunks.len() {
+            let commit = &commit_chunks[i];
+
+            let mut inputs = vec![];
+
+            for input in commit.input.iter() {
+                if let Some(entry) = all_tx_map.get(&input.previous_output.txid) {
+                    inputs.push(SignRawTransactionInput {
+                        txid: input.previous_output.txid,
+                        vout: input.previous_output.vout,
+                        script_pub_key: entry.output[input.previous_output.vout as usize]
+                            .script_pubkey
+                            .clone(),
+                        redeem_script: None,
+                        amount: Some(entry.output[input.previous_output.vout as usize].value),
+                    });
+                }
+            }
+
             let signed_raw_commit_tx = self
                 .client
-                .sign_raw_transaction_with_wallet(&commit, None, None)
+                .sign_raw_transaction_with_wallet(commit, Some(inputs.as_slice()), None)
                 .await?;
-            raw_txs.push(signed_raw_commit_tx.hex);
-            let serialized_reveal_tx = encode::serialize(&reveal);
+            raw_txs.push(signed_raw_commit_tx.hex.clone());
+
+            let signed_chunk_commit: Transaction =
+                encode::deserialize(&signed_raw_commit_tx.hex).unwrap();
+
+            let reveal = &reveal_chunks[i];
+            let serialized_reveal_tx = encode::serialize(reveal);
             raw_txs.push(serialized_reveal_tx);
         }
 
+        let mut inputs = vec![];
+
+        for input in commit.input.iter() {
+            if let Some(entry) = all_tx_map.get(&input.previous_output.txid) {
+                inputs.push(SignRawTransactionInput {
+                    txid: input.previous_output.txid,
+                    vout: input.previous_output.vout,
+                    script_pub_key: entry.output[input.previous_output.vout as usize]
+                        .script_pubkey
+                        .clone(),
+                    redeem_script: None,
+                    amount: Some(entry.output[input.previous_output.vout as usize].value),
+                });
+            }
+        }
         let signed_raw_commit_tx = self
             .client
-            .sign_raw_transaction_with_wallet(&commit, None, None)
+            .sign_raw_transaction_with_wallet(&commit, Some(inputs.as_slice()), None)
             .await?;
+
         raw_txs.push(signed_raw_commit_tx.hex);
 
         let serialized_reveal_tx = encode::serialize(&reveal.tx);
         raw_txs.push(serialized_reveal_tx);
 
         self.test_mempool_accept(&raw_txs).await?;
+
         let txids = self.send_raw_transactions(&raw_txs).await?;
 
         for txid in txids[1..txids.len() - 1].iter().step_by(2) {
@@ -781,7 +843,7 @@ impl DaService for BitcoinService {
                     }
                 }
             }
-            let zk_proof: Proof = borsh::from_slice(&body)
+            let zk_proof: Proof = borsh::from_slice(decompress_blob(&body).as_slice())
                 .map_err(|e| anyhow!("{}: Failed to parse Proof from Aggregate: {e}", tx_id))?;
             aggregates.push((i, zk_proof));
         }
