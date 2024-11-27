8f31afb7e4c992feeebd16c327359333f17d0fa0
eyusufatik <esadyusufatik@gmail.com>
2024-11-27 17:19:08 +0300

    fix ctrl c and error handling in fullnode

--- crates/fullnode/src/runner.rs
+++ crates/fullnode/src/runner.rs
@@ -391,6 +391,7 @@ where
                                 // This block failed to process, add remaining L2 blocks to queue including this one.
                                 let remaining_l2s: Vec<(u64, GetSoftConfirmationResponse)> = l2_blocks[index..].to_vec();
                                 pending_l2_blocks.extend(remaining_l2s);
+                                break;
                             }
                         }
                         continue;
