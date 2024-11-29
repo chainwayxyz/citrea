ed4fb1e5028b0a80f810e26d240d4c9a3904853e
jfldde <168934971+jfldde@users.noreply.github.com>
2024-11-29 09:11:51 +0000

    Restore code

--- bin/citrea/tests/bitcoin_e2e/sequencer_test.rs
+++ bin/citrea/tests/bitcoin_e2e/sequencer_test.rs
@@ -30,7 +30,6 @@ impl TestCase for BasicSequencerTest {
             .get_head_soft_confirmation()
             .await?
             .unwrap();
-        println!("head_batch0 : {:?}", head_batch0);
         assert_eq!(head_batch0.l2_height, 1);
 
         sequencer.client.send_publish_batch_request().await?;
--- crates/sovereign-sdk/full-node/sov-ledger-rpc/src/lib.rs
+++ crates/sovereign-sdk/full-node/sov-ledger-rpc/src/lib.rs
@@ -1,8 +1,5 @@
 #![forbid(unsafe_code)]
 
-use serde::{Deserialize, Deserializer, Serialize};
-use serde_json::Value;
-
 #[cfg(feature = "server")]
 pub mod server;
 
@@ -11,5 +8,5 @@ pub mod client;
 
 /// A 32-byte hash [`serde`]-encoded as a hex string optionally prefixed with
 /// `0x`. See [`sov_rollup_interface::rpc::utils::rpc_hex`].
-#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, serde::Serialize, serde::Deserialize)]
 pub struct HexHash(#[serde(with = "sov_rollup_interface::rpc::utils::rpc_hex")] pub [u8; 32]);
