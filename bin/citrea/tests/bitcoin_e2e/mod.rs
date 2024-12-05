8421e480903e89d5263eacbba7f12412354a3223
jfldde <168934971+jfldde@users.noreply.github.com>
2024-12-05 09:48:57 +0000

    Remove HashStf

--- bin/citrea/tests/bitcoin_e2e/mod.rs
+++ bin/citrea/tests/bitcoin_e2e/mod.rs
@@ -4,6 +4,7 @@ pub mod batch_prover_test;
 pub mod light_client_test;
 // pub mod mempool_accept;
 pub mod bitcoin_test;
+pub mod full_node;
 pub mod sequencer_commitments;
 pub mod sequencer_test;
 pub mod tx_chain;
