e15c5446acf279d6ee224b61496df6a859a7d99d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-12-09 16:24:01 +0300

    Update directory

--- bin/citrea/src/guests.rs
+++ bin/citrea/src/guests.rs
@@ -57,7 +57,7 @@ lazy_static! {
     pub(crate) static ref BATCH_PROOF_DEVNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
 
-        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/testnet/batch-0.elf"));
+        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/devnet/batch-0.elf"));
         m
     };
     pub(crate) static ref LIGHT_CLIENT_MAINNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
@@ -75,7 +75,7 @@ lazy_static! {
     pub(crate) static ref LIGHT_CLIENT_DEVNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
 
-        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/testnet/light-0.elf"));
+        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/devnet/light-0.elf"));
         m
     };
 }
