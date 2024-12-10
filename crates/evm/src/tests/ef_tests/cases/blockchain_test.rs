c691716fabdf8dc26395639f6ba9b2244cc9d264
yaziciahmet <yaziciahmet991@gmail.com>
2024-12-10 23:31:20 +0100

    Try bumping to Cancun without adding any tests

--- crates/evm/src/tests/ef_tests/cases/blockchain_test.rs
+++ crates/evm/src/tests/ef_tests/cases/blockchain_test.rs
@@ -125,7 +125,7 @@ impl Case for BlockchainTestCase {
         // Iterate through test cases, filtering by the network type to exclude specific forks.
         self.tests
             .values()
-            .filter(|case| case.network <= ForkSpec::Shanghai)
+            .filter(|case| case.network <= ForkSpec::Cancun)
             .par_bridge()
             .try_for_each(|case| {
                 let mut evm_config = EvmConfig::default();
