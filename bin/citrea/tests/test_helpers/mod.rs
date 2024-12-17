d9d9d0a23c544b24bcceed6b8d7330ed1da22ee1
yaziciahmet <yaziciahmet991@gmail.com>
2024-12-17 23:10:44 +0100

    Update test_helpers according to nightly

--- bin/citrea/tests/test_helpers/mod.rs
+++ bin/citrea/tests/test_helpers/mod.rs
@@ -191,8 +191,8 @@ pub fn create_default_rollup_config(
         telemetry: Default::default(),
         forks: Forks::from_slice(&[
             Fork::new(SpecId::Genesis, 0),
-            Fork::new(SpecId::Fork1, 10000),
-            Fork::new(SpecId::Fork2, 20000),
+            Fork::new(SpecId::Fork1, 1000),
+            Fork::new(SpecId::Fork2, 2000),
         ])
         .unwrap(),
     }
