e70101866273a2e2e0cc24e232703e403ea81f08
yaziciahmet <yaziciahmet991@gmail.com>
2024-12-19 21:14:18 +0100

    Fix CANCUN related test

--- crates/evm/src/tests/queries/estimate_gas_tests.rs
+++ crates/evm/src/tests/queries/estimate_gas_tests.rs
@@ -94,7 +94,7 @@ fn test_tx_request_fields_gas() {
     );
     assert_eq!(
         contract_diff_size.unwrap(),
-        serde_json::from_value::<EstimatedDiffSize>(json![{"gas":"0x6601","l1DiffSize":"0x60"}])
+        serde_json::from_value::<EstimatedDiffSize>(json![{"gas":"0x6601","l1DiffSize":"0x1f"}])
             .unwrap()
     );
 
@@ -110,7 +110,7 @@ fn test_tx_request_fields_gas() {
     );
     assert_eq!(
         contract_diff_size.unwrap(),
-        serde_json::from_value::<EstimatedDiffSize>(json![{"gas":"0x6601","l1DiffSize":"0x60"}])
+        serde_json::from_value::<EstimatedDiffSize>(json![{"gas":"0x6601","l1DiffSize":"0x1f"}])
             .unwrap()
     );
 
