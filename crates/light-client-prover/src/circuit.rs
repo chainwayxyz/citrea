d49f2c851d711da4ca8c91f2fc5f79d8597d8847
eyusufatik <esadyusufatik@gmail.com>
2025-01-07 10:42:20 +0300

    don't panic on da blob cannot deserialize to zk proof receipt

--- crates/light-client-prover/src/circuit.rs
+++ crates/light-client-prover/src/circuit.rs
@@ -111,9 +111,10 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
             if let Ok(data) = data {
                 match data {
                     DaDataLightClient::Complete(proof) => {
-                        // TODO: don't panic here, ignore if cant extract
-                        let journal =
-                            G::extract_raw_output(&proof).expect("DaData proofs must be valid");
+                        let Ok(journal) = G::extract_raw_output(&proof) else {
+                            // cannot parse the output, skip
+                            continue;
+                        };
 
                         let (
                             batch_proof_output_initial_state_root,
