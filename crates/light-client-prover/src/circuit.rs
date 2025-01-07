6867cb906cf0a4f32aba72de0561e5b92dfc76ea
Erce Can Bektüre <canerce99@hotmail.com>
2025-01-07 18:40:42 +0300

    Get rid of unnecessary if statement

--- crates/light-client-prover/src/circuit.rs
+++ crates/light-client-prover/src/circuit.rs
@@ -149,13 +149,7 @@ pub fn run_circuit<DaV: DaVerifier, G: ZkvmGuest>(
 
                         let batch_proof_method_id = if batch_proof_method_ids.len() == 1 {
                             // Check if last l2 height is greater than or equal to the only batch proof method id activation height
-                            if batch_proof_output_last_l2_height >= batch_proof_method_ids[0].0 {
-                                batch_proof_method_ids[0].1
-                            } else {
-                                // If not continue to the next blob
-                                current_proof_index += 1;
-                                continue;
-                            }
+                            batch_proof_method_ids[0].1
                         } else {
                             let idx = match batch_proof_method_ids
                                 // Returns err and the index to be inserted, which is the index of the first element greater than the key
