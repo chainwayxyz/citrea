e15797c2cb0095c7e55ff2127d6390ed4a22aa67
yaziciahmet <yaziciahmet991@gmail.com>
2025-01-07 16:52:49 +0100

    Impl serialize_v1 for blob

--- crates/bitcoin-da/src/spec/blob.rs
+++ crates/bitcoin-da/src/spec/blob.rs
@@ -81,4 +81,21 @@ impl BlobReaderTrait for BlobWithSender {
         self.blob.advance(num_bytes);
         self.verified_data()
     }
+
+    fn serialize_v1(&self) -> anyhow::Result<Vec<u8>> {
+        let v1 = BlobWithSenderV1 {
+            hash: self.hash,
+            sender: self.sender.clone(),
+            blob: &self.blob,
+        };
+        Ok(borsh::to_vec(&v1)?)
+    }
+}
+
+#[derive(BorshSerialize)]
+/// Internal type to ease serialization process
+struct BlobWithSenderV1<'a> {
+    hash: [u8; 32],
+    sender: AddressWrapper,
+    blob: &'a CountedBufReader<BlobBuf>,
 }
--- crates/sovereign-sdk/adapters/mock-da/src/verifier.rs
+++ crates/sovereign-sdk/adapters/mock-da/src/verifier.rs
@@ -34,6 +34,10 @@ impl BlobReaderTrait for MockBlob {
         self.data.advance(num_bytes);
         self.verified_data()
     }
+
+    fn serialize_v1(&self) -> anyhow::Result<Vec<u8>> {
+        Ok(borsh::to_vec(self)?)
+    }
 }
 
 /// A [`sov_rollup_interface::da::DaSpec`] suitable for testing.
