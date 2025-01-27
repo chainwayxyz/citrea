1491ead3581a4ad8d58404f81e6d61ed72c0a68b
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-01-27 11:28:36 +0300

    fix hex state root cmp

--- bin/citrea/tests/bitcoin_e2e/full_node.rs
+++ bin/citrea/tests/bitcoin_e2e/full_node.rs
@@ -41,7 +41,7 @@ impl TestCase for FullNodeRestartTest {
             .unwrap();
 
         // Verify genesis is not reprocessed
-        assert_eq!(genesis_state_root, genesis_state_root_after);
+        assert_eq!(genesis_state_root.0, genesis_state_root_after.0);
 
         sequencer.client.send_publish_batch_request().await?;
         full_node.wait_for_l2_height(1, None).await?;
