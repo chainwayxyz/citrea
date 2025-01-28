df98966ac1a71b09b1e74c955ff61e5c5b9ab96c
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-01-28 16:48:03 +0300

    Revert "faster hex for proof serialization"
    This reverts commit b3b03d3e93c2159c841c5f9233829368c2876000.

--- crates/sovereign-sdk/rollup-interface/src/node/rpc/mod.rs
+++ crates/sovereign-sdk/rollup-interface/src/node/rpc/mod.rs
@@ -274,7 +274,7 @@ pub struct BatchProofResponse {
 #[serde(rename_all = "camelCase")]
 pub struct VerifiedBatchProofResponse {
     /// Proof
-    #[serde(with = "faster_hex")]
+    #[serde(with = "utils::rpc_hex")]
     pub proof: ProofRpcResponse,
     /// State transition
     pub proof_output: BatchProofOutputRpcResponse,
