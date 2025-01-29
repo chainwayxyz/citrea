64aff1a8f2c5a59d02814ca537ee171904aba62f
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-01-29 15:49:45 +0300

    fix da_slot_height

--- crates/batch-prover/src/runner.rs
+++ crates/batch-prover/src/runner.rs
@@ -187,7 +187,7 @@ where
 
         let current_l1_block = get_da_block_at_height(
             &self.da_service,
-            soft_confirmation.da_slot_height.to(),
+            soft_confirmation.da_slot_height,
             self.l1_block_cache.clone(),
         )
         .await?;
