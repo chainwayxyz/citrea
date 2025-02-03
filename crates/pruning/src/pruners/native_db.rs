3eb5e95604416f9f11f405e70463325ab77d6ed4
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-02-03 14:16:03 +0300

    Batch delete keys

--- crates/pruning/src/pruners/native_db.rs
+++ crates/pruning/src/pruners/native_db.rs
@@ -14,18 +14,19 @@ pub(crate) fn prune_native_db(native_db: Arc<sov_schema_db::DB>, up_to_block: u6
     iter.seek_to_first();
 
     let mut counter = 1u32;
-    // TODO: Think about a better way to do this as the bigger the table, the slower this will be.
+    let mut keys_to_delete = vec![];
     while let Some(Ok(entry)) = iter.next() {
         let version = entry.key.1;
         // The version value is always ahead of block number by one.
         if version < up_to_block + 1 {
-            if let Err(e) = native_db.delete::<ModuleAccessoryState>(&entry.key) {
-                error!("Failed to delete native DB entry {:?}", e);
-                continue;
-            }
-
+            keys_to_delete.push(entry.key);
             counter += 1;
         }
+    }
+
+    if let Err(e) = native_db.delete_batch::<ModuleAccessoryState>(keys_to_delete) {
+        error!("Failed to delete native DB entry {:?}", e);
+        return;
     }
 
     debug!("Pruned {} native DB records", counter);
