3b0d5095899edafbae7a0cd9109d2b1baca45186
yaziciahmet <yaziciahmet991@gmail.com>
2025-02-04 11:24:27 +0300

    simplify first jmt update case

--- crates/sovereign-sdk/module-system/sov-state/src/prover_storage.rs
+++ crates/sovereign-sdk/module-system/sov-state/src/prover_storage.rs
@@ -133,9 +133,8 @@ where
         // TODO: Fix this before introducing snapshots!
         if jmt.get_root_hash_option(latest_version)?.is_none() {
             assert_eq!(latest_version, 0);
-            let empty_batch = Vec::default().into_iter();
             let (_, tree_update) = jmt
-                .put_value_set(empty_batch, latest_version)
+                .put_value_set(vec![], latest_version)
                 .expect("JMT update must succeed");
 
             self.db
