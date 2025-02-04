760ee304de64448470eb514da5e88d19055c06f2
eyusufatik <esadyusufatik@gmail.com>
2025-02-04 14:24:34 +0300

    attach wtxid to sequencer commitments

--- crates/bitcoin-da/src/service.rs
+++ crates/bitcoin-da/src/service.rs
@@ -999,7 +999,7 @@ impl DaService for BitcoinService {
                                         seq_comm.body,
                                         seq_comm.public_key,
                                         hash,
-                                        None,
+                                        Some(wtxid.to_byte_array()),
                                     );
 
                                     relevant_txs.push(relevant_tx);
