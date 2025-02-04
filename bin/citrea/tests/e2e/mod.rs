2860db97e8f7cdac323a17ee987174be546753d4
Esad Yusuf Atik <esadyusufatik+git@gmail.com>
2025-02-04 21:13:27 +0300

    Update bin/citrea/tests/e2e/mod.rs
    Co-authored-by: Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>

--- bin/citrea/tests/e2e/mod.rs
+++ bin/citrea/tests/e2e/mod.rs
@@ -683,7 +683,7 @@ async fn test_offchain_contract_storage() {
 
     let seq_fork = fork_from_block_number(seq_height);
 
-    // Assert we are at fork1
+    // Assert we are at fork2
     assert_eq!(seq_fork.spec_id, SpecId::Fork2);
 
     // This should access the `code` and copy code over to `offchain_code` in EVM
