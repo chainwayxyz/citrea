reproduce-env-tenki-cloud/citrea2

commit 36cbf7e7590683497416541f0d1352424fbbf843

Refactor rollup initialization (#1714)
Co-authored-by: jfldde <168934971+jfldde@users.noreply.github.com>

bin/citrea/src/main.rs
@@ -5,16 +5,22 @@ use std::time::Duration;
 use anyhow::{anyhow, Context as _};
 use bitcoin_da::service::BitcoinServiceConfig;
 use citrea::{
-    initialize_logging, BitcoinRollup, CitreaRollupBlueprint, MockDemoRollup, NetworkArg,
+    initialize_logging, BitcoinRollup, CitreaRollupBlueprint, Dependencies, MockDemoRollup, Storage,
 };
-use citrea_common::{
-    from_toml_path, BatchProverConfig, FromEnv, FullNodeConfig, LightClientProverConfig,
-    SequencerConfig,
-};
+use citrea_common::da::get_start_l1_height;
+use citrea_common::rpc::server::start_rpc_server;
+use citrea_common::{from_toml_path, FromEnv, FullNodeConfig};
+use citrea_light_client_prover::da_block_handler::StartVariant;
 use citrea_stf::genesis_config::GenesisPaths;
 use clap::Parser;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use metrics_util::MetricKindMask;
+use sov_db::ledger_db::SharedLedgerOps;
+use sov_db::rocks_db_config::RocksdbConfig;
+use sov_db::schema::tables::{
+    BATCH_PROVER_LEDGER_TABLES, FULL_NODE_LEDGER_TABLES, LIGHT_CLIENT_PROVER_LEDGER_TABLES,
+    SEQUENCER_LEDGER_TABLES,
+};
 use sov_mock_da::MockDaConfig;
 use sov_modules_api::Spec;
 use sov_modules_rollup_blueprint::RollupBlueprint;
@@ -22,69 +28,15 @@ use sov_rollup_interface::Network;
 use sov_state::storage::NativeStorage;
 use tracing::{debug, error, info, instrument};
 
+use crate::cli::{node_type_from_args, Args, NodeType, SupportedDaLayer};
+
+mod cli;
 #[cfg(test)]
 mod test_rpc;
 
 /// Main runner. Initializes a DA service, and starts a node using the provided arguments.
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// The mode in which the node runs.
-    /// This determines which guest code to use.
-    /// Default is Mainnet.
-    #[clap(short, long, default_value_t, value_enum)]
-    network: NetworkArg,
-
-    /// Run the development chain
-    #[arg(long, default_value_t)]
-    dev: bool,
-
-    /// Run the regtest chain
-    #[arg(long, default_value_t, conflicts_with = "dev")]
-    dev_all_forks: bool,
-
-    /// Path to the genesis configuration.
-    /// Defines the genesis of module states like evm.
-    #[arg(long)]
-    genesis_paths: String,
-
-    /// The data layer type.
-    #[arg(long, default_value = "mock")]
-    da_layer: SupportedDaLayer,
-
-    /// The path to the rollup config, if a string is provided, it will be used as the path to the rollup config, otherwise environment variables will be used.
-    #[arg(long)]
-    rollup_config_path: Option<String>,
-
-    /// The option to run the node in sequencer mode, if a string is provided, it will be used as the path to the sequencer config, otherwise environment variables will be used.
-    #[arg(long, conflicts_with_all = ["batch_prover", "light_client_prover"])]
-    sequencer: Option<Option<String>>,
-
-    /// The option to run the node in batch prover mode, if a string is provided, it will be used as the path to the batch prover config, otherwise the environment variables will be used.
-    #[arg(long, conflicts_with_all = ["sequencer", "light_client_prover"])]
-    batch_prover: Option<Option<String>>,
-
-    /// The option to run the node in light client prover mode, if a string is provided, it will be used as the path to the light client prover config, otherwise the environment variables will be used.
-    #[arg(long, conflicts_with_all = ["sequencer", "batch_prover"])]
-    light_client_prover: Option<Option<String>>,
-
-    /// Logging verbosity
-    #[arg(long, short = 'v', action = clap::ArgAction::Count, default_value = "2")]
-    verbose: u8,
-    /// Logging verbosity
-    #[arg(long, short = 'q', action)]
-    quiet: bool,
-}
-
-#[derive(clap::ValueEnum, Clone, Debug)]
-enum SupportedDaLayer {
-    Mock,
-    Bitcoin,
-}
-
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> anyhow::Result<()> {
     let mut args = Args::parse();
 
     if args.quiet {
@@ -100,57 +52,7 @@ async fn main() -> Result<(), anyhow::Error> {
     };
     initialize_logging(logging_level);
 
-    let sequencer_config = match args.sequencer {
-        Some(Some(path)) => Some(
-            from_toml_path(path)
-                .context("Failed to read sequencer configuration from the config file")?,
-        ),
-        Some(None) => Some(
-            SequencerConfig::from_env()
-                .context("Failed to read sequencer configuration from the environment")?,
-        ),
-        None => None,
-    };
-
-    let batch_prover_config = match args.batch_prover {
-        Some(Some(path)) => Some(
-            from_toml_path(path)
-                .context("Failed to read prover configuration from the config file")?,
-        ),
-        Some(None) => Some(
-            BatchProverConfig::from_env()
-                .context("Failed to read prover configuration from the environment")?,
-        ),
-        None => None,
-    };
-
-    let light_client_prover_config = match args.light_client_prover {
-        Some(Some(path)) => Some(
-            from_toml_path(path)
-                .context("Failed to read prover configuration from the config file")?,
-        ),
-        Some(None) => Some(
-            LightClientProverConfig::from_env()
-                .context("Failed to read prover configuration from the environment")?,
-        ),
-        None => None,
-    };
-
-    if batch_prover_config.is_some() && sequencer_config.is_some() {
-        return Err(anyhow::anyhow!(
-            "Cannot run in both batch prover and sequencer mode at the same time"
-        ));
-    }
-    if batch_prover_config.is_some() && light_client_prover_config.is_some() {
-        return Err(anyhow::anyhow!(
-            "Cannot run in both batch prover and light client prover mode at the same time"
-        ));
-    }
-    if light_client_prover_config.is_some() && sequencer_config.is_some() {
-        return Err(anyhow::anyhow!(
-            "Cannot run in both light client prover and sequencer mode at the same time"
-        ));
-    }
+    let node_type = node_type_from_args(&args)?;
 
     let mut network = args.network.into();
     if args.dev {
@@ -169,9 +71,7 @@ async fn main() -> Result<(), anyhow::Error> {
                 network,
                 &GenesisPaths::from_dir(&args.genesis_paths),
                 args.rollup_config_path,
-                batch_prover_config,
-                light_client_prover_config,
-                sequencer_config,
+                node_type,
             )
             .await?;
         }
@@ -180,9 +80,7 @@ async fn main() -> Result<(), anyhow::Error> {
                 network,
                 &GenesisPaths::from_dir(&args.genesis_paths),
                 args.rollup_config_path,
-                batch_prover_config,
-                light_client_prover_config,
-                sequencer_config,
+                node_type,
             )
             .await?;
         }
@@ -194,17 +92,15 @@ async fn main() -> Result<(), anyhow::Error> {
 #[instrument(level = "trace", skip_all, err)]
 async fn start_rollup<S, DaC>(
     network: Network,
-    rt_genesis_paths: &<<S as RollupBlueprint>::NativeRuntime as sov_modules_stf_blueprint::Runtime<
+    runtime_genesis_paths: &<<S as RollupBlueprint>::NativeRuntime as sov_modules_stf_blueprint::Runtime<
         <S as RollupBlueprint>::NativeContext,
         <S as RollupBlueprint>::DaSpec,
     >>::GenesisPaths,
     rollup_config_path: Option<String>,
-    batch_prover_config: Option<BatchProverConfig>,
-    light_client_prover_config: Option<LightClientProverConfig>,
-    sequencer_config: Option<SequencerConfig>,
+    node_type: NodeType,
 ) -> Result<(), anyhow::Error>
 where
-    DaC: serde::de::DeserializeOwned + DebugTrait + Clone + FromEnv,
+    DaC: serde::de::DeserializeOwned + DebugTrait + Clone + FromEnv + Send + Sync + 'static,
     S: CitreaRollupBlueprint<DaConfig = DaC>,
     <<S as RollupBlueprint>::NativeContext as Spec>::Storage: NativeStorage,
 {
@@ -236,64 +132,220 @@ where
 
     let rollup_blueprint = S::new(network);
 
-    if let Some(sequencer_config) = sequencer_config {
-        let (mut sequencer, rpc_methods) = rollup_blueprint
-            .create_new_sequencer(rt_genesis_paths, rollup_config.clone(), sequencer_config)
-            .await
-            .expect("Could not start sequencer");
-        sequencer.start_rpc_server(rpc_methods, None).await.unwrap();
+    // Based on the node's type, execute migrations before constructing an instance of LedgerDB
+    // so that avoid locking the DB.
+    let (tables, migrations) = match node_type {
+        NodeType::Sequencer(_) => (
+            SEQUENCER_LEDGER_TABLES
+                .iter()
+                .map(|table| table.to_string())
+                .collect::<Vec<_>>(),
+            citrea_sequencer::db_migrations::migrations(),
+        ),
+        NodeType::FullNode => (
+            FULL_NODE_LEDGER_TABLES
+                .iter()
+                .map(|table| table.to_string())
+                .collect::<Vec<_>>(),
+            citrea_fullnode::db_migrations::migrations(),
+        ),
+        NodeType::BatchProver(_) => (
+            BATCH_PROVER_LEDGER_TABLES
+                .iter()
+                .map(|table| table.to_string())
+                .collect::<Vec<_>>(),
+            citrea_batch_prover::db_migrations::migrations(),
+        ),
+        NodeType::LightClientProver(_) => (
+            LIGHT_CLIENT_PROVER_LEDGER_TABLES
+                .iter()
+                .map(|table| table.to_string())
+                .collect::<Vec<_>>(),
+            citrea_light_client_prover::db_migrations::migrations(),
+        ),
+    };
+    rollup_blueprint.run_ledger_migrations(&rollup_config, tables.clone(), migrations)?;
 
-        if let Err(e) = sequencer.run().await {
-            error!("Error: {}", e);
+    let genesis_config =
+        rollup_blueprint.create_genesis_config(runtime_genesis_paths, &rollup_config)?;
+
+    let rocksdb_path = rollup_config.storage.path.clone();
+    let rocksdb_config = RocksdbConfig::new(
+        rocksdb_path.as_path(),
+        rollup_config.storage.db_max_open_files,
+        Some(tables),
+    );
+
+    let Storage {
+        ledger_db,
+        storage_manager,
+        prover_storage,
+    } = rollup_blueprint.setup_storage(&rollup_config, &rocksdb_config)?;
+
+    let Dependencies {
+        da_service,
+        mut task_manager,
+        soft_confirmation_channel,
+    } = rollup_blueprint.setup_dependencies(&rollup_config).await?;
+
+    let sequencer_client_url = rollup_config
+        .runner
+        .clone()
+        .map(|runner| runner.sequencer_client_url);
+    let soft_confirmation_rx = match node_type {
+        NodeType::Sequencer(_) | NodeType::BatchProver(_) | NodeType::FullNode => {
+            soft_confirmation_channel.1
         }
-    } else if let Some(batch_prover_config) = batch_prover_config {
-        let (mut prover, rpc_methods) = CitreaRollupBlueprint::create_new_batch_prover(
-            &rollup_blueprint,
-            rt_genesis_paths,
-            rollup_config,
-            batch_prover_config,
-        )
-        .await
-        .expect("Could not start batch prover");
+        _ => None,
+    };
 
-        prover
-            .start_rpc_server(rpc_methods, None)
-            .await
-            .expect("Failed to start rpc server");
+    let rpc_module = rollup_blueprint.setup_rpc(
+        &prover_storage,
+        ledger_db.clone(),
+        da_service.clone(),
+        sequencer_client_url,
+        soft_confirmation_rx,
+    )?;
 
-        if let Err(e) = prover.run().await {
-            error!("Error: {}", e);
+    match node_type {
+        NodeType::Sequencer(sequencer_config) => {
+            let (mut sequencer, rpc_module) = rollup_blueprint
+                .create_sequencer(
+                    genesis_config,
+                    rollup_config.clone(),
+                    sequencer_config,
+                    da_service,
+                    ledger_db,
+                    storage_manager,
+                    prover_storage,
+                    soft_confirmation_channel.0,
+                    rpc_module,
+                )
+                .expect("Could not start sequencer");
+
+            start_rpc_server(
+                rollup_config.rpc.clone(),
+                &mut task_manager,
+                rpc_module,
+                None,
+            );
+
+            if let Err(e) = sequencer.run(task_manager).await {
+                error!("Error: {}", e);
+            }
         }
-    } else if let Some(light_client_prover_config) = light_client_prover_config {
-        let (mut prover, rpc_methods) = CitreaRollupBlueprint::create_new_light_client_prover(
-            &rollup_blueprint,
-            rollup_config,
-            light_client_prover_config,
-        )
-        .await
-        .expect("Could not start light client prover");
+        NodeType::BatchProver(batch_prover_config) => {
+            let (mut prover, l1_block_handler, rpc_module) =
+                CitreaRollupBlueprint::create_batch_prover(
+                    &rollup_blueprint,
+                    batch_prover_config,
+                    genesis_config,
+                    rollup_config.clone(),
+                    da_service,
+                    ledger_db.clone(),
+                    storage_manager,
+                    prover_storage,
+                    soft_confirmation_channel.0,
+                    rpc_module,
+                )
+                .await
+                .expect("Could not start batch prover");
 
-        prover
-            .start_rpc_server(rpc_methods, None)
-            .await
-            .expect("Failed to start rpc server");
+            start_rpc_server(
+                rollup_config.rpc.clone(),
+                &mut task_manager,
+                rpc_module,
+                None,
+            );
 
-        if let Err(e) = prover.run().await {
-            error!("Error: {}", e);
+            task_manager.spawn(|cancellation_token| async move {
+                let Ok(start_l1_height) = get_start_l1_height(&rollup_config, &ledger_db).await
+                else {
+                    error!("Failed to start prover L1 block handler due to start l1 height not present");
+                    return;
+                };
+                l1_block_handler
+                    .run(start_l1_height, cancellation_token)
+                    .await
+            });
+
+            if let Err(e) = prover.run(task_manager).await {
+                error!("Error: {}", e);
+            }
         }
-    } else {
-        let (mut rollup, rpc_methods) = CitreaRollupBlueprint::create_new_rollup(
-            &rollup_blueprint,
-            rt_genesis_paths,
-            rollup_config,
-        )
-        .await
-        .expect("Could not start full-node");
+        NodeType::LightClientProver(light_client_prover_config) => {
+            let starting_block = match ledger_db.get_last_scanned_l1_height()? {
+                Some(l1_height) => StartVariant::LastScanned(l1_height.0),
+                // first time starting the prover
+                // start from the block given in the config
+                None => StartVariant::FromBlock(light_client_prover_config.initial_da_height),
+            };
 
-        rollup.start_rpc_server(rpc_methods, None).await;
+            let (mut prover, l1_block_handler, rpc_module) =
+                CitreaRollupBlueprint::create_light_client_prover(
+                    &rollup_blueprint,
+                    light_client_prover_config,
+                    rollup_config.clone(),
+                    &rocksdb_config,
+                    da_service,
+                    ledger_db,
+                    rpc_module,
+                )
+                .await
+                .expect("Could not start light client prover");
 
-        if let Err(e) = rollup.run().await {
-            error!("Error: {}", e);
+            start_rpc_server(
+                rollup_config.rpc.clone(),
+                &mut task_manager,
+                rpc_module,
+                None,
+            );
+
+            task_manager.spawn(|cancellation_token| async move {
+                l1_block_handler
+                    .run(starting_block, cancellation_token)
+                    .await
+            });
+
+            if let Err(e) = prover.run(task_manager).await {
+                error!("Error: {}", e);
+            }
+        }
+        _ => {
+            let (mut full_node, l1_block_handler) = CitreaRollupBlueprint::create_full_node(
+                &rollup_blueprint,
+                genesis_config,
+                rollup_config.clone(),
+                da_service,
+                ledger_db.clone(),
+                storage_manager,
+                prover_storage,
+                soft_confirmation_channel.0,
+            )
+            .await
+            .expect("Could not start full-node");
+
+            start_rpc_server(
+                rollup_config.rpc.clone(),
+                &mut task_manager,
+                rpc_module,
+                None,
+            );
+
+            task_manager.spawn(|cancellation_token| async move {
+                let Ok(start_l1_height) = get_start_l1_height(&rollup_config, &ledger_db).await
+                else {
+                    error!("Failed to start fullnode L1 block handler due to start l1 height not present");
+                    return;
+                };
+                l1_block_handler
+                    .run(start_l1_height, cancellation_token)
+                    .await
+            });
+
+            if let Err(e) = full_node.run(task_manager).await {
+                error!("Error: {}", e);
+            }
         }
     }
 

chainwayxyz/citrea

commit 4ba3e0fa5906a964b7d1770a4abff2d87fa9b4d0

fix some rollup init bugs after introduced by #1714

bin/citrea/src/main.rs
@@ -187,7 +187,13 @@ where
         da_service,
         mut task_manager,
         soft_confirmation_channel,
-    } = rollup_blueprint.setup_dependencies(&rollup_config).await?;
+    } = rollup_blueprint
+        .setup_dependencies(
+            &rollup_config,
+            matches!(node_type, NodeType::Sequencer(_))
+                || matches!(node_type, NodeType::BatchProver(_)),
+        )
+        .await?;
 
     let sequencer_client_url = rollup_config
         .runner

bin/citrea/src/rollup/mod.rs
@@ -73,10 +73,11 @@ pub trait CitreaRollupBlueprint: RollupBlueprint {
     async fn setup_dependencies(
         &self,
         rollup_config: &FullNodeConfig<Self::DaConfig>,
+        require_da_wallet: bool,
     ) -> Result<Dependencies<Self>> {
         let mut task_manager = TaskManager::default();
         let da_service = self
-            .create_da_service(rollup_config, true, &mut task_manager)
+            .create_da_service(rollup_config, require_da_wallet, &mut task_manager)
             .await?;
         let (soft_confirmation_tx, soft_confirmation_rx) = broadcast::channel(10);
         // If subscriptions disabled, pass None
@@ -413,10 +414,9 @@ pub trait CitreaRollupBlueprint: RollupBlueprint {
             Self::DaSpec,
         >,
     > {
-        let genesis_root = prover_storage.get_root_hash(1);
         if let Some((number, soft_confirmation)) = ledger_db.get_head_soft_confirmation()? {
             // At least one soft confirmation was processed
-            info!("Initialize sequencer at batch number {:?}. State root: {:?}. Last soft confirmation hash: {:?}.", number, prover_storage.get_root_hash(number.0 + 1)?.as_ref(), soft_confirmation.hash);
+            info!("Initialize node at batch number {:?}. State root: {:?}. Last soft confirmation hash: {:?}.", number, prover_storage.get_root_hash(number.0 + 1)?.as_ref(), soft_confirmation.hash);
 
             return Ok(InitParams {
                 state_root: prover_storage.get_root_hash(number.0 + 1)?,
@@ -424,6 +424,7 @@ pub trait CitreaRollupBlueprint: RollupBlueprint {
             });
         }
 
+        let genesis_root = prover_storage.get_root_hash(1);
         if let Ok(state_root) = genesis_root {
             // Chain was initialized but no soft confirmations was processed
             debug!("Chain is already initialized. Skipping initialization.");

bin/citrea/tests/test_helpers/mod.rs
@@ -133,7 +133,10 @@ pub async fn start_rollup(
         mut task_manager,
         soft_confirmation_channel,
     } = mock_demo_rollup
-        .setup_dependencies(&rollup_config)
+        .setup_dependencies(
+            &rollup_config,
+            sequencer_config.is_some() || rollup_prover_config.is_some(),
+        )
         .await
         .expect("Dependencies setup should work");
 