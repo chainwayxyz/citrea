f8c4da47724440cf84488b3ecb1d905c206b810e
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-02-04 17:47:10 +0300

    Pass state db

--- bin/citrea/tests/test_helpers/mod.rs
+++ bin/citrea/tests/test_helpers/mod.rs
@@ -15,7 +15,7 @@ use citrea_common::{
 use citrea_light_client_prover::da_block_handler::StartVariant;
 use citrea_primitives::TEST_PRIVATE_KEY;
 use citrea_stf::genesis_config::GenesisPaths;
-use citrea_storage_ops::PruningConfig;
+use citrea_storage_ops::pruning::PruningConfig;
 use sov_db::ledger_db::SharedLedgerOps;
 use sov_db::rocks_db_config::RocksdbConfig;
 use sov_db::schema::tables::{
