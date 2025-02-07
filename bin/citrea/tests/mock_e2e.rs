8209dfb179fb5664800de2a468228f47c19e14ce
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-02-07 12:25:58 +0300

    Rename e2e to mock

--- bin/citrea/tests/mock_e2e.rs
+++ bin/citrea/tests/mock_e2e.rs
@@ -1,7 +1,4 @@
 // disable bank module tests due to needing a big rewrite to make it work
 // mod bank;
 mod common;
-mod e2e;
-mod evm;
-mod mempool;
-mod soft_confirmation_rule_enforcer;
+mod mock;
