3722c20e04ffa1d51a1344113b283715accf4784
jfldde <168934971+jfldde@users.noreply.github.com>
2025-02-12 13:50:56 +0000

    Lint

--- crates/sovereign-sdk/full-node/db/sov-schema-db/src/lib.rs
+++ crates/sovereign-sdk/full-node/db/sov-schema-db/src/lib.rs
@@ -27,8 +27,7 @@ use std::time::Instant;
 
 use ::metrics::{gauge, histogram};
 use anyhow::format_err;
-pub use iterator::ScanDirection;
-pub use iterator::{RawDbReverseIterator, SchemaIterator, SeekKeyEncoder};
+pub use iterator::{RawDbReverseIterator, ScanDirection, SchemaIterator, SeekKeyEncoder};
 pub use rocksdb;
 pub use rocksdb::DEFAULT_COLUMN_FAMILY_NAME;
 use rocksdb::{DBIterator, ReadOptions};
