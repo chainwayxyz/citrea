f9e5ab0152b812832c17cc14f96f3057b908d1a9
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-02-13 17:33:02 +0300

    Prune soft confirmations by number & hash

--- crates/storage-ops/src/pruning/components/ledger_db/mod.rs
+++ crates/storage-ops/src/pruning/components/ledger_db/mod.rs
@@ -8,18 +8,33 @@ use crate::pruning::types::PruningNodeType;
 
 mod tables;
 
+macro_rules! log_result_or_error {
+    ($table:literal, $call:expr) => {{
+        match $call {
+            Ok(result) => {
+                debug!("Deleted {} records from {}", $table, result);
+            }
+            Err(e) => {
+                error!("Failed to prune {} ledger tables: {:?}", $table, e);
+                return;
+            }
+        }
+    }};
+}
+
 /// Prune ledger
 pub(crate) fn prune_ledger(node_type: PruningNodeType, ledger_db: Arc<DB>, up_to_block: u64) {
     debug!("Pruning Ledger, up to L2 block {}", up_to_block);
 
-    if let Err(e) = prune_shared_tables(&ledger_db) {
-        error!("Failed to prune shared ledger tables: {:?}", e);
-        return;
+    match node_type {
+        PruningNodeType::Sequencer => {
+            log_result_or_error!(
+                "soft_confirmations_by_number",
+                prune_soft_confirmations_by_number(node_type, &ledger_db, up_to_block)
+            );
+        }
+        PruningNodeType::FullNode => todo!(),
+        PruningNodeType::BatchProver => todo!(),
+        PruningNodeType::LightClient => todo!(),
     }
-}
-
-fn prune_shared_tables(ledger_db: &DB, up_to_block: u64) -> anyhow::Result<()> {
-    prune_soft_confirmations_by_number(ledger_db)?;
-
-    Ok(())
 }
---
+++ crates/storage-ops/src/pruning/components/ledger_db/tables/soft_confirmations_by_number.rs
@@ -0,0 +1,37 @@
+use sov_db::schema::tables::{SoftConfirmationByHash, SoftConfirmationByNumber};
+use sov_db::schema::types::SoftConfirmationNumber;
+use sov_schema_db::{ScanDirection, DB};
+
+use crate::pruning::types::PruningNodeType;
+
+pub(crate) fn prune_soft_confirmations_by_number(
+    node_type: PruningNodeType,
+    ledger_db: &DB,
+    up_to_block: u64,
+) -> anyhow::Result<u64> {
+    let soft_confirmations = ledger_db.iter_with_direction::<SoftConfirmationByNumber>(
+        Default::default(),
+        ScanDirection::Forward,
+    )?;
+
+    let mut deleted = 0;
+    for record in soft_confirmations {
+        let Ok(record) = record else {
+            continue;
+        };
+
+        let soft_confirmation_number = record.key;
+
+        if soft_confirmation_number > SoftConfirmationNumber(up_to_block) {
+            break;
+        }
+        ledger_db.delete::<SoftConfirmationByNumber>(&record.key)?;
+        if !matches!(node_type, PruningNodeType::LightClient) {
+            let soft_confirmation = record.value;
+            ledger_db.delete::<SoftConfirmationByHash>(&soft_confirmation.hash)?;
+        }
+        deleted += 1;
+    }
+
+    Ok(deleted)
+}
