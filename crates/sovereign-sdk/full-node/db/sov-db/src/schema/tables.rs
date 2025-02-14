3c21427a065f0f4a77418aae6b25ebf9c509674c
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-02-14 12:30:08 +0300

    sequencer should not have verified batch proofs

--- crates/sovereign-sdk/full-node/db/sov-db/src/schema/tables.rs
+++ crates/sovereign-sdk/full-node/db/sov-db/src/schema/tables.rs
@@ -57,7 +57,6 @@ pub const SEQUENCER_LEDGER_TABLES: &[&str] = &[
     LastSequencerCommitmentSent::table_name(),
     SoftConfirmationStatus::table_name(),
     CommitmentsByNumber::table_name(),
-    VerifiedBatchProofsBySlotNumber::table_name(),
     MempoolTxs::table_name(),
     LastPrunedBlock::table_name(),
     #[cfg(test)]
