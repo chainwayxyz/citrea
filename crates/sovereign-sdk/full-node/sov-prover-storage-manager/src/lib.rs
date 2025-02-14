bc77ac9a2786b67317661e6dc33f2e8e982d0e83
yaziciahmet <yaziciahmet991@gmail.com>
2025-02-14 19:17:28 +0300

    panic on snapshot finalize call

--- crates/sovereign-sdk/full-node/sov-prover-storage-manager/src/lib.rs
+++ crates/sovereign-sdk/full-node/sov-prover-storage-manager/src/lib.rs
@@ -67,13 +67,7 @@ impl ProverStorageManager {
     /// Commits all the changes to `ProverStorage` to underlying database.
     /// If storage is a snapshot, nothing is committed, an false is returned.
     pub fn finalize_storage(&self, storage: ProverStorage) -> bool {
-        if storage.is_snapshot() {
-            tracing::debug!(
-                "Storage on version {} is a snapshot, skipping finalization",
-                storage.version()
-            );
-            return false;
-        }
+        assert!(!storage.is_snapshot(), "Snapshot storage should never be finalized");
 
         tracing::debug!("Finalizing storage on version {}", storage.version());
 
