6fa77f14aa4374fcb13daf20d5fd834daaf43a57
Erce Can Bektüre <canerce99@hotmail.com>
2025-02-18 20:21:14 +0300

    Lints

--- crates/batch-prover/tests/prover_tests.rs
+++ crates/batch-prover/tests/prover_tests.rs
@@ -363,13 +363,9 @@ fn make_transition_data(
         sequencer_public_key: vec![],
         sequencer_da_public_key: vec![],
         preproven_commitments: vec![],
-<<<<<<< HEAD
-        final_state_root: [0u8; 32],
         short_header_proofs: VecDeque::new(),
-=======
         final_state_root: [0; 32],
         sequencer_commitments: vec![],
->>>>>>> nightly
     }
 }
 
