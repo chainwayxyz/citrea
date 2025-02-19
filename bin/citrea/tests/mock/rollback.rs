ef20e2fecb995111581aea297219cd3f20856a8b
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-02-19 15:51:28 +0300

    Complete test

--- bin/citrea/tests/mock/rollback.rs
+++ bin/citrea/tests/mock/rollback.rs
@@ -1,24 +1,35 @@
-use std::collections::BTreeMap;
-use std::panic::AssertUnwindSafe;
+use std::fs;
 use std::str::FromStr;
+use std::sync::Arc;
 
 use alloy_primitives::Address;
-use futures::FutureExt;
+use citrea_common::SequencerConfig;
+use citrea_stf::genesis_config::GenesisPaths;
+use citrea_storage_ops::pruning::types::StorageNodeType;
+use citrea_storage_ops::rollback::Rollback;
 use reth_primitives::BlockNumberOrTag;
+use sov_db::ledger_db::{LedgerDB, SharedLedgerOps};
+use sov_db::native_db::NativeDB;
+use sov_db::rocks_db_config::RocksdbConfig;
+use sov_db::schema::tables::SEQUENCER_LEDGER_TABLES;
+use sov_db::state_db::StateDB;
 use sov_mock_da::{MockAddress, MockDaService};
 
-use crate::common::helpers::{tempdir_with_children, wait_for_l1_block, wait_for_l2_block};
-use crate::mock::{initialize_test, TestConfig};
+use crate::common::helpers::{
+    create_default_rollup_config, start_rollup, tempdir_with_children, wait_for_l1_block,
+    wait_for_l2_block, NodeMode,
+};
+use crate::common::TEST_DATA_GENESIS_PATH;
+use crate::mock::evm::init_test_rollup;
 
-/// Trigger pruning native DB data.
+/// Trigger rollback native DB data.
 #[tokio::test(flavor = "multi_thread")]
-async fn test_native_db_rollback() -> Result<(), anyhow::Error> {
+async fn test_rollback() -> Result<(), anyhow::Error> {
     citrea::initialize_logging(tracing::Level::DEBUG);
 
     let storage_dir = tempdir_with_children(&["DA", "sequencer", "full-node"]);
     let da_db_dir = storage_dir.path().join("DA").to_path_buf();
     let sequencer_db_dir = storage_dir.path().join("sequencer").to_path_buf();
-    let fullnode_db_dir = storage_dir.path().join("full-node").to_path_buf();
 
     let da_service = MockDaService::new(MockAddress::default(), &da_db_dir.clone());
 
@@ -28,42 +39,51 @@ async fn test_native_db_rollback() -> Result<(), anyhow::Error> {
     }
     wait_for_l1_block(&da_service, 3, None).await;
 
-    let (seq_test_client, full_node_test_client, seq_task, full_node_task, _) =
-        initialize_test(TestConfig {
-            da_path: da_db_dir,
-            sequencer_path: sequencer_db_dir,
-            fullnode_path: fullnode_db_dir,
-            pruning_config: None,
-            ..Default::default()
-        })
-        .await;
+    let sequencer_config = SequencerConfig::default();
+    let (seq_port_tx, seq_port_rx) = tokio::sync::oneshot::channel();
+    let rollup_config = create_default_rollup_config(
+        true,
+        &sequencer_db_dir,
+        &da_db_dir,
+        NodeMode::SequencerNode,
+        None,
+    );
+
+    let sequencer_config1 = sequencer_config.clone();
+    let task_manager = start_rollup(
+        seq_port_tx,
+        GenesisPaths::from_dir(TEST_DATA_GENESIS_PATH),
+        None,
+        None,
+        rollup_config,
+        Some(sequencer_config1),
+        None,
+    )
+    .await;
+
+    let seq_port = seq_port_rx.await.unwrap();
+    let seq_test_client = init_test_rollup(seq_port).await;
 
     let addr = Address::from_str("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92265").unwrap();
-    let mut transactions = BTreeMap::new();
-    let mut block_hashes = BTreeMap::new();
 
     for i in 1..=50 {
         // send one ether to some address
-        let pending = seq_test_client
+        let _ = seq_test_client
             .send_eth(addr, None, None, None, 1e18 as u128)
             .await
             .unwrap();
 
         seq_test_client.spam_publish_batch_request().await.unwrap();
 
-        let tx_hash = pending.tx_hash();
-        transactions.insert(i, *tx_hash);
-
         if i % 5 == 0 {
             wait_for_l2_block(&seq_test_client, i, None).await;
 
             // Get the hash of the latest block
-            let block_hash = seq_test_client
+            seq_test_client
                 .eth_get_block_by_number(Some(BlockNumberOrTag::Number(i)))
                 .await
                 .header
                 .hash;
-            block_hashes.insert(i, block_hash);
 
             da_service.publish_test_block().await.unwrap();
 
@@ -71,44 +91,57 @@ async fn test_native_db_rollback() -> Result<(), anyhow::Error> {
         }
     }
 
-    seq_test_client.send_publish_batch_request().await;
-    wait_for_l2_block(&full_node_test_client, 51, None).await;
+    task_manager.abort().await;
 
-    // ####################################
-    // ROUND 1: FAIL
-    // ###################################
-    // This request is requesting data which has been pruned.
-    let check_block_by_number_result = AssertUnwindSafe(
-        full_node_test_client
-            .eth_get_block_by_number_with_detail(Some(BlockNumberOrTag::Number(1))),
-    )
-    .catch_unwind()
-    .await;
-    assert!(check_block_by_number_result.is_err());
+    fs::remove_file(format!("{}/ledger/LOCK", sequencer_db_dir.display())).unwrap();
 
-    let get_block_receipts_result = AssertUnwindSafe(full_node_test_client.eth_get_block_receipts(
-        reth_primitives::BlockId::Number(BlockNumberOrTag::Number(1)),
-    ))
-    .catch_unwind()
-    .await;
-    assert!(get_block_receipts_result.is_err());
+    let sequencer_tables = SEQUENCER_LEDGER_TABLES
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>();
+    let rocksdb_config =
+        RocksdbConfig::new(&sequencer_db_dir, None, Some(sequencer_tables.to_vec()));
+    let ledger_db = LedgerDB::with_config(&rocksdb_config)?;
+    let native_db = Arc::new(NativeDB::setup_schema_db(&rocksdb_config)?);
+    let state_db = Arc::new(StateDB::setup_schema_db(&rocksdb_config)?);
+    let rollback = Rollback::new(ledger_db.inner(), state_db.clone(), native_db.clone());
 
-    let get_block_by_hash_result = AssertUnwindSafe(
-        full_node_test_client.eth_get_block_by_hash(*block_hashes.get(&5).unwrap()),
-    )
-    .catch_unwind()
-    .await;
-    assert!(get_block_by_hash_result.is_err());
+    rollback
+        .execute(StorageNodeType::Sequencer, 50, 10)
+        .await
+        .unwrap();
 
-    let check_transaction_by_hash_result = AssertUnwindSafe(
-        full_node_test_client.eth_get_transaction_by_hash(*transactions.get(&1).unwrap(), None),
-    )
-    .catch_unwind()
-    .await;
-    assert!(check_transaction_by_hash_result.unwrap().is_none());
+    drop(rollback);
+    drop(state_db);
+    drop(native_db);
+    drop(ledger_db);
+
+    let (seq_port_tx, seq_port_rx) = tokio::sync::oneshot::channel();
+    let rollup_config = create_default_rollup_config(
+        true,
+        &sequencer_db_dir,
+        &da_db_dir,
+        NodeMode::SequencerNode,
+        None,
+    );
+    let seq_task = tokio::spawn(async move {
+        start_rollup(
+            seq_port_tx,
+            GenesisPaths::from_dir(TEST_DATA_GENESIS_PATH),
+            None,
+            None,
+            rollup_config,
+            Some(sequencer_config),
+            None,
+        )
+        .await;
+    });
+    let seq_port = seq_port_rx.await.unwrap();
+    let seq_test_client = init_test_rollup(seq_port).await;
+
+    wait_for_l2_block(&seq_test_client, 40, None).await;
 
     seq_task.abort();
-    full_node_task.abort();
 
     Ok(())
 }
