b54976690516992680b6c861b84c1ec1bf7b8ba3
yaziciahmet <yaziciahmet991@gmail.com>
2025-03-04 16:51:14 +0100

    remove warn log

--- crates/ethereum-rpc/src/lib.rs
+++ crates/ethereum-rpc/src/lib.rs
@@ -579,7 +579,6 @@ where
         tx_hash: B256,
         opts: Option<GethDebugTracingOptions>,
     ) -> RpcResult<GethTrace> {
-        tracing::warn!("debug_traceTransaction");
         let evm = Evm::<C>::default();
         let mut working_set = WorkingSet::new(self.ethereum.storage.clone());
 
