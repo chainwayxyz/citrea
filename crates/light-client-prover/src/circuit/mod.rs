723618fd745dd675e4b72b38d9303ea20a97209f
eyusufatik <esadyusufatik@gmail.com>
2025-03-04 23:44:30 +0300

    allow native to access constant lcp values

--- crates/light-client-prover/src/circuit/mod.rs
+++ crates/light-client-prover/src/circuit/mod.rs
@@ -2,6 +2,7 @@
 mod accessors;
 pub mod new;
 pub mod old;
+pub mod primitives;
 
 // L2 activation height of the fork, and the batch proof method ID
 type InitialBatchProofMethodIds = Vec<(u64, [u32; 8])>;
--- guests/risc0/light-client-proof/mock/src/bin/light_client_proof_mock.rs
+++ guests/risc0/light-client-proof/mock/src/bin/light_client_proof_mock.rs
@@ -1,5 +1,9 @@
 #![no_main]
 use citrea_light_client_prover::circuit::old::run_circuit;
+use citrea_light_client_prover::circuit::primitives::mockda::{
+    BATCH_PROVER_DA_PUBLIC_KEY, GENESIS_ROOT, INITIAL_BATCH_PROOF_METHOD_IDS,
+    METHOD_ID_UPGRADE_AUTHORITY_DA_PUBLIC_KEY,
+};
 use citrea_risc0_adapter::guest::Risc0Guest;
 use sov_mock_da::MockDaVerifier;
 use sov_rollup_interface::zk::ZkvmGuest;
@@ -8,30 +12,6 @@ use sov_rollup_interface::Network;
 risc0_zkvm::guest::entry!(main);
 
 const NETWORK: Network = Network::Nightly;
-
-const L2_GENESIS_ROOT: [u8; 32] = match const_hex::const_decode_to_array(
-    b"dacb59b0ff5d16985a8418235133eee37758a3ac1b76ab6d1f87c6df20e4d4da",
-) {
-    Ok(root) => root,
-    Err(_) => panic!("Can't happen"),
-};
-
-const INITIAL_BATCH_PROOF_METHOD_IDS: &[(u64, [u32; 8])] =
-    &[(0, citrea_risc0_batch_proof::BATCH_PROOF_MOCK_ID)];
-
-const BATCH_PROVER_DA_PUBLIC_KEY: [u8; 33] = match const_hex::const_decode_to_array(
-    b"03eedab888e45f3bdc3ec9918c491c11e5cf7af0a91f38b97fbc1e135ae4056601",
-) {
-    Ok(pub_key) => pub_key,
-    Err(_) => panic!("Can't happen"),
-};
-
-const METHOD_ID_UPGRADE_AUTHORITY_DA_PUBLIC_KEY: [u8; 33] = match const_hex::const_decode_to_array(
-    b"0313c4ff65eb94999e0ac41cfe21592baa52910f5a5ada9074b816de4f560189db",
-) {
-    Ok(pub_key) => pub_key,
-    Err(_) => panic!("Can't happen"),
-};
 
 pub fn main() {
     let guest = Risc0Guest::new();
@@ -43,7 +23,7 @@ pub fn main() {
     let output = run_circuit::<MockDaVerifier, Risc0Guest>(
         da_verifier,
         input,
-        L2_GENESIS_ROOT,
+        GENESIS_ROOT,
         INITIAL_BATCH_PROOF_METHOD_IDS.to_vec(),
         &BATCH_PROVER_DA_PUBLIC_KEY,
         &METHOD_ID_UPGRADE_AUTHORITY_DA_PUBLIC_KEY,
