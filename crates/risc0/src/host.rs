f32dec88084ce10c52768656db33e95673ff9867
eyusufatik <esadyusufatik@gmail.com>
2025-03-08 01:04:07 +0300

    risc0 host better env var set in testing feature

--- crates/risc0/src/host.rs
+++ crates/risc0/src/host.rs
@@ -136,15 +136,18 @@ impl ZkvmHost for Risc0BonsaiHost {
 
         #[cfg(feature = "testing")]
         {
-            if self.network == Network::TestNetworkWithForks {
-                env.env_var("ALL_FORKS", "1");
-                env.env_var("RISC0_DEV_MODE", "1");
-            }
-
             // if we are testing, set guest env var to enable dev mode
             // so that it verifies fake receipts
-            if self.network == Network::Nightly {
-                env.env_var("RISC0_DEV_MODE", "1");
+            env.env_var("RISC0_DEV_MODE", "1");
+
+            match self.network {
+                Network::Nightly => {}
+                Network::TestNetworkWithForks => {
+                    env.env_var("ALL_FORKS", "1");
+                }
+                _ => {
+                    panic!("Invalid network in testing feature!")
+                }
             }
         }
 
