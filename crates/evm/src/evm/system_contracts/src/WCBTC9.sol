4398706539ce1da3d69eef75a4bc37960e2a232a
okkothejawa <beratrobot2@gmail.com>
2025-03-17 17:03:11 +0300

    Bump WCBTC version

--- crates/evm/src/evm/system_contracts/src/WCBTC9.sol
+++ crates/evm/src/evm/system_contracts/src/WCBTC9.sol
@@ -1,41 +1,39 @@
-pragma solidity ^0.4.18;
+pragma solidity >=0.4.22 <0.6;
 
 contract WCBTC9 {
-    string public name = "Wrapped Citrea Bitcoin";
-    string public symbol = "WCBTC";
-    uint8 public decimals = 18;
+    string public name     = "Wrapped Citrea Bitcoin";
+    string public symbol   = "WCBTC";
+    uint8  public decimals = 18;
 
-    event Approval(address indexed src, address indexed guy, uint wad);
-    event Transfer(address indexed src, address indexed dst, uint wad);
-    event Deposit(address indexed dst, uint wad);
-    event Withdrawal(address indexed src, uint wad);
+    event  Approval(address indexed src, address indexed guy, uint wad);
+    event  Transfer(address indexed src, address indexed dst, uint wad);
+    event  Deposit(address indexed dst, uint wad);
+    event  Withdrawal(address indexed src, uint wad);
 
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+    mapping (address => uint)                       public  balanceOf;
+    mapping (address => mapping (address => uint))  public  allowance;
 
-    function() public payable {
+    function() external payable {
         deposit();
     }
-
     function deposit() public payable {
         balanceOf[msg.sender] += msg.value;
-        Deposit(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
-
     function withdraw(uint wad) public {
         require(balanceOf[msg.sender] >= wad);
         balanceOf[msg.sender] -= wad;
         msg.sender.transfer(wad);
-        Withdrawal(msg.sender, wad);
+        emit Withdrawal(msg.sender, wad);
     }
 
     function totalSupply() public view returns (uint) {
-        return this.balance;
+        return address(this).balance;
     }
 
     function approve(address guy, uint wad) public returns (bool) {
         allowance[msg.sender][guy] = wad;
-        Approval(msg.sender, guy, wad);
+        emit Approval(msg.sender, guy, wad);
         return true;
     }
 
@@ -43,11 +41,10 @@ contract WCBTC9 {
         return transferFrom(msg.sender, dst, wad);
     }
 
-    function transferFrom(
-        address src,
-        address dst,
-        uint wad
-    ) public returns (bool) {
+    function transferFrom(address src, address dst, uint wad)
+        public
+        returns (bool)
+    {
         require(balanceOf[src] >= wad);
 
         if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
@@ -58,7 +55,7 @@ contract WCBTC9 {
         balanceOf[src] -= wad;
         balanceOf[dst] += wad;
 
-        Transfer(src, dst, wad);
+        emit Transfer(src, dst, wad);
 
         return true;
     }
