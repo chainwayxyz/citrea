d42f553e21ad8608e1c766c2326e622c628640e3
yaziciahmet <yaziciahmet991@gmail.com>
2025-03-21 13:49:12 +0100

    remove old batch prover

--- crates/batch-prover/src/runner.rs
+++ crates/batch-prover/src/runner.rs
@@ -1,24 +1,19 @@
 use core::panic;
 
-use citrea_common::l2::{L2BlockSignal, L2Syncer};
 use sov_db::ledger_db::BatchProverLedgerOps;
-use sov_db::schema::types::L2BlockNumber;
 use sov_rollup_interface::services::da::DaService;
 use tokio::select;
-use tokio::sync::mpsc;
 use tokio_util::sync::CancellationToken;
 use tracing::{info, instrument};
 
-use crate::metrics::BATCH_PROVER_METRICS;
+use crate::l2_syncer::L2Syncer;
 
 pub struct CitreaBatchProver<DA, DB>
 where
     DA: DaService<Error = anyhow::Error>,
     DB: BatchProverLedgerOps + Clone,
 {
-    ledger_db: DB,
     l2_syncer: L2Syncer<DA, DB>,
-    l2_signal_rx: mpsc::Receiver<L2BlockSignal>,
 }
 
 impl<DA, DB> CitreaBatchProver<DA, DB>
@@ -26,16 +21,8 @@ where
     DA: DaService<Error = anyhow::Error>,
     DB: BatchProverLedgerOps + Clone + 'static,
 {
-    pub fn new(
-        ledger_db: DB,
-        l2_syncer: L2Syncer<DA, DB>,
-        l2_signal_rx: mpsc::Receiver<L2BlockSignal>,
-    ) -> Result<Self, anyhow::Error> {
-        Ok(Self {
-            ledger_db,
-            l2_syncer,
-            l2_signal_rx,
-        })
+    pub fn new(l2_syncer: L2Syncer<DA, DB>) -> Result<Self, anyhow::Error> {
+        Ok(Self { l2_syncer })
     }
 
     #[instrument(level = "trace", skip_all, err)]
@@ -46,21 +33,8 @@ where
         loop {
             select! {
                 _ = &mut l2_syncer => {},
-                Some(l2_block) = self.l2_signal_rx.recv() => {
-                    if let Some(state_diff) = l2_block.state_diff {
-                        // Save state diff to ledger DB
-                        self.ledger_db.set_l2_state_diff(
-                            L2BlockNumber(l2_block.height),
-                            state_diff,
-                        )?;
-                    }
-
-                    BATCH_PROVER_METRICS.current_l2_block.set(l2_block.height as f64);
-                    BATCH_PROVER_METRICS.process_l2_block.record(l2_block.process_duration);
-                }
                 _ = cancellation_token.cancelled() => {
-                    info!("Shutting down fullnode");
-                    self.l2_signal_rx.close();
+                    info!("Shutting down batch prover");
                     break;
                 },
             }
