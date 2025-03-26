6bcd20691f9866a678705d0af9d52fa047f9c425
leruaa <aurelien.catinon@gmail.com>
2025-03-26 02:17:06 -0700

    cleanup

--- crates/primitives/src/types.rs
+++ crates/primitives/src/types.rs
@@ -1,6 +1,5 @@
 pub type L2BlockHash = [u8; 32];
 pub use sov_rollup_interface::zk::Digest;
 
-pub type SoftConfirmationHash = [u8; 32];
 pub type BlockNumber = u64;
 pub type L2Range = (BlockNumber, BlockNumber);
