7b50cf3b7d53994b339a556bd023178d0fb84815
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-03-28 02:59:29 +0300

    Fix imports

--- crates/light-client-prover/src/rpc.rs
+++ crates/light-client-prover/src/rpc.rs
@@ -115,7 +115,7 @@ where
             .unwrap_or_default()
             .into_iter()
             .map(|id| BatchProofMethodIdRpcResponse {
-                method_id: id.1,
+                method_id: id.1.into(),
                 height: alloy_primitives::U64::from(id.0),
             })
             .collect::<Vec<_>>();
