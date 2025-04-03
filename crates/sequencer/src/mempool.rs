adf0b1d94b74407ab063cfd006ab8adca575bef8
eyusufatik <esadyusufatik@gmail.com>
2025-04-03 19:13:18 +0300

    use new mempool feature minimal_protocol_basefee

--- crates/sequencer/src/mempool.rs
+++ crates/sequencer/src/mempool.rs
@@ -4,6 +4,7 @@ use alloy_eips::Typed2718;
 use alloy_primitives::TxHash;
 use citrea_common::SequencerMempoolConfig;
 use citrea_evm::SYSTEM_SIGNER;
+use citrea_primitives::MIN_BASE_FEE_PER_GAS;
 use reth_execution_types::ChangedAccount;
 use reth_tasks::TokioTaskExecutor;
 use reth_transaction_pool::blobstore::NoopBlobStore;
@@ -54,6 +55,7 @@ impl CitreaMempool {
                 max_size: 0,
             },
             max_account_slots: mempool_conf.max_account_slots as usize,
+            minimal_protocol_basefee: MIN_BASE_FEE_PER_GAS.try_into().unwrap(),
             ..Default::default()
         };
 
