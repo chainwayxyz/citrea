e99ecfd8911a688333519b377440f1f24b455aeb
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-04-04 15:21:40 +0300

    Fix test_chain_proof_by_commitment_index flakiness

--- bin/citrea/tests/bitcoin/light_client_test.rs
+++ bin/citrea/tests/bitcoin/light_client_test.rs
@@ -1831,7 +1831,7 @@ impl TestCase for ChainProofByCommitmentIndex {
             .unwrap();
 
         let bp = create_serialized_fake_receipt_batch_proof(
-            genesis_state_root,
+            [1u8; 32], // The state root of the previous commitment
             300,
             method_ids[0].method_id.into(),
             None,
