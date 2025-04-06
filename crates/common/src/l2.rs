0dc9283f021a0f414ba76365d339e1fbc4e9f1dc
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-04-06 16:46:16 +0300

    revert changes in write order of dbs

--- crates/common/src/l2.rs
+++ crates/common/src/l2.rs
@@ -120,13 +120,12 @@ pub async fn process_l2_block<Da: DaService, DB: SharedLedgerOps>(
         bail!("Post state root mismatch at height: {}", l2_height)
     }
 
+    storage_manager.finalize_storage(l2_block_result.change_set);
+
     let tx_hashes = compute_tx_hashes::<DefaultContext>(&l2_block.txs, current_spec);
     let tx_bodies = if include_tx_body { tx_bodies } else { None };
 
     ledger_db.commit_l2_block(l2_block, tx_hashes, tx_bodies)?;
-    // Write to state/native db after the ledger db commit, otherwise an interrupt 
-    // after the finalize_storage might cause ledger db to miss the block
-    storage_manager.finalize_storage(l2_block_result.change_set);
 
     // TODO: https://github.com/chainwayxyz/citrea/issues/1992
     // self.ledger_db.extend_l2_range_of_l1_slot(
