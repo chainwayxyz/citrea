01768f0e6d9b74d1f1720c2d1d7d1ead0e9847e3
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2025-04-06 20:11:39 -0300

    Rewrite 7702 authorization_list with filter_map

--- crates/evm/src/evm/handler.rs
+++ crates/evm/src/evm/handler.rs
@@ -612,8 +612,7 @@ fn calc_diff_size<CTX>(context: &mut CTX) -> usize
 where
     CTX: CitreaContextTr,
 {
-    let tx_caller = context.tx().caller();
-    let journaled_state = context.journal_ref();
+    let (journaled_state, tx) = (context.journal_ref(), context.tx());
 
     // For each call there is a journal entry.
     // We need to iterate over all journal entries to get the size of the diff.
@@ -632,6 +631,7 @@ where
     let mut account_changes: BTreeMap<&Address, AccountChange<'_>> = BTreeMap::new();
 
     // tx.from always has `account_info_changed` because its nonce is incremented
+    let tx_caller = tx.caller();
     let from = account_changes.entry(&tx_caller).or_default();
     from.account_info_changed = true;
 
@@ -641,38 +641,38 @@ where
     // collecting then consuming the iterator
     // to avoid borrowing issues
     // also not doing tx type check as authorization_list will return empty list
-    let auths = context
-        .tx()
+    let auths = tx
         .authorization_list()
-        .map(|auth| (auth.authority(), auth.address()))
+        .filter_map(|auth| {
+            let delegated_to = auth.address();
+            let authority = auth.authority();
+            authority.map(|authority| (authority, delegated_to))
+        })
         .collect::<Vec<_>>();
 
-    for signer in auths.iter() {
-        if let (Some(ref authority), delegated_to) = signer {
-            // if returns None, the authorization failed at one of the following checks:
-            // - if the chain id check failed
-            // - if nonce was u64::MAX
-            // - if the signer couldn't be recovered <-- this case is not possible as we checked this on the above
-            //   if let
-            if let Some(authority_in_state) = journaled_state.state.get(authority) {
-                // if the final code of the authority is equal to delegated address
-                // or the delegated address is zero and the account code hash is KECCAK_EMPTY
-                // we know the authorization went through
-                if (*delegated_to == Address::ZERO
-                    && authority_in_state.info.code_hash == KECCAK_EMPTY)
-                    || authority_in_state
-                        .info
-                        .code
-                        .as_ref()
-                        .is_some_and(|code| *code == Bytecode::new_eip7702(*delegated_to))
-                {
-                    // we set account changed for the authority
-                    let account = account_changes.entry(authority).or_default();
-                    account.account_info_changed = true;
-                    // doing this does not matter anymore as account info is already set to changed
-                    // but doing it anyway to be consistent
-                    account.code_changed = true;
-                }
+    for (authority, delegated_to) in &auths {
+        // if returns None, the authorization failed at one of the following checks:
+        // - if the chain id check failed
+        // - if nonce was u64::MAX
+        // - if the signer couldn't be recovered <-- this case is not possible as we checked this on the above
+        //   if let
+        if let Some(authority_in_state) = journaled_state.state.get(authority) {
+            // if the final code of the authority is equal to delegated address
+            // or the delegated address is zero and the account code hash is KECCAK_EMPTY
+            // we know the authorization went through
+            if (delegated_to == &Address::ZERO && authority_in_state.info.code_hash == KECCAK_EMPTY)
+                || authority_in_state
+                    .info
+                    .code
+                    .as_ref()
+                    .is_some_and(|code| *code == Bytecode::new_eip7702(*delegated_to))
+            {
+                // we set account changed for the authority
+                let account = account_changes.entry(authority).or_default();
+                account.account_info_changed = true;
+                // doing this does not matter anymore as account info is already set to changed
+                // but doing it anyway to be consistent
+                account.code_changed = true;
             }
         }
     }
