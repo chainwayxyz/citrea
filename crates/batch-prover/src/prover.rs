d139e2251493f29350536834930743a55a022c24
yaziciahmet <yaziciahmet991@gmail.com>
2025-04-07 20:56:16 +0200

    lower signal msgs to debug

--- crates/batch-prover/src/prover.rs
+++ crates/batch-prover/src/prover.rs
@@ -109,7 +109,7 @@ where
                 l1_signal = self.l1_signal_rx.recv() => {
                     l1_signal.expect("L1 signal sender channel closed abruptly");
 
-                    info!("Got L1 signal to try proving");
+                    debug!("Got L1 signal to try proving");
                     if let Err(e) = self.try_proving(true).await {
                         error!("Failed to start proving: {}", e);
                     }
@@ -126,7 +126,7 @@ where
                         continue;
                     }
 
-                    info!("Got L2 signal to try proving");
+                    debug!("Got L2 signal to try proving");
                     if let Err(e) = self.try_proving(true).await {
                         error!("Failed to start proving: {}", e);
                     }
@@ -144,7 +144,7 @@ where
                             warn!("Paused proving");
                         }
                         ProverRequest::Prove(result_tx) => {
-                            info!("Got rpc request to try proving");
+                            debug!("Got rpc request to try proving");
                             match self.try_proving(false).await {
                                 Ok(job_ids) => {
                                     let _ = result_tx.send(job_ids);
@@ -165,7 +165,7 @@ where
         }
 
         if with_sampling && !self.should_prove() {
-            info!("Skipping proving due to sampling");
+            debug!("Skipping proving due to sampling");
             return Ok(Vec::new());
         }
 
