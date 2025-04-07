1c20ab98ea988884eec325e0e673e8e43e3387cf
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-04-07 08:53:19 +0300

    Remove unneeded return

--- bin/citrea/src/main.rs
+++ bin/citrea/src/main.rs
@@ -413,15 +413,12 @@ pub async fn wait_shutdown(task_manager: TaskManager) {
     tokio::select! {
         _ = signal::ctrl_c() => {
             task_manager.graceful_shutdown_with_timeout(wait_duration);
-            return;
         }
         _ = term_signal.recv() => {
             task_manager.graceful_shutdown_with_timeout(wait_duration);
-            return;
         },
         _ = interrupt_signal.recv() => {
             task_manager.graceful_shutdown_with_timeout(wait_duration);
-            return;
         }
     }
 }
