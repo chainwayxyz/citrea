0a7f25ba5487e619cf99718d789f095d0d563887
eyusufatik <esadyusufatik@gmail.com>
2025-04-08 23:38:56 +0300

    add final part of the diff_sizes test

--- bin/citrea/tests/mock/evm/diff_sizes.rs
+++ bin/citrea/tests/mock/evm/diff_sizes.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use alloy_primitives::{Address, U64};
 use citrea_common::SequencerConfig;
-use citrea_evm::smart_contracts::SimpleStorageContract;
+use citrea_evm::smart_contracts::{SimpleStorageContract, SimpleStorageDuplicatorContract};
 use citrea_stf::genesis_config::GenesisPaths;
 
 use super::init_test_rollup;
@@ -47,7 +47,8 @@ async fn diff_sizes() -> Result<(), anyhow::Error> {
     let seq_test_client = init_test_rollup(seq_port).await;
 
     // compare sending cBTC to the same address
-    // resulting in different sizes because the second address was just created
+    // resulting in different sizes because in the first tx address was just created
+    // so on the second tx we don't pay for setting address index
     {
         let receiver_address = Address::random();
 
@@ -103,20 +104,21 @@ async fn diff_sizes() -> Result<(), anyhow::Error> {
         );
     }
 
+    let simple_storage_address = seq_test_client.from_addr.create(2);
+
     // call a smart contract and see storages were charged
     {
         let deploy_tx = seq_test_client
             .deploy_contract(SimpleStorageContract::default().byte_code(), None)
             .await
             .unwrap();
-        let contract_address = seq_test_client.from_addr.create(2);
 
         seq_test_client.send_publish_batch_request().await;
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
         let set_storage_tx = seq_test_client
             .contract_transaction(
-                contract_address,
+                simple_storage_address,
                 SimpleStorageContract::default().set_call_data(100),
                 None,
             )
@@ -129,7 +131,7 @@ async fn diff_sizes() -> Result<(), anyhow::Error> {
         assert_eq!(
             seq_test_client
                 .contract_call::<U64>(
-                    contract_address,
+                    simple_storage_address,
                     SimpleStorageContract::default().get_call_data(),
                     None,
                 )
@@ -180,7 +182,94 @@ async fn diff_sizes() -> Result<(), anyhow::Error> {
     // make a transaction to a contract that calls another contract
     // resulting in storage changes in both
     // all storage changes should be charged
-    {}
+    // as there is a seperate Journal for each call in the tx
+    // we must make sure we include all Journals in the tx
+    {
+        let deploy_tx = seq_test_client
+            .deploy_contract(SimpleStorageDuplicatorContract::default().byte_code(), None)
+            .await
+            .unwrap();
+
+        let duplicator_address = seq_test_client.from_addr.create(4);
+
+        seq_test_client.send_publish_batch_request().await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+        let set_storage_tx = seq_test_client
+            .contract_transaction(
+                duplicator_address,
+                SimpleStorageDuplicatorContract::default()
+                    .set_call_data(500, simple_storage_address),
+                None,
+            )
+            .await;
+
+        seq_test_client.send_publish_batch_request().await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+        // sanity check
+        assert_eq!(
+            seq_test_client
+                .contract_call::<U64>(
+                    simple_storage_address,
+                    SimpleStorageContract::default().get_call_data(),
+                    None,
+                )
+                .await
+                .unwrap(),
+            U64::from(500),
+        );
+
+        assert_eq!(
+            seq_test_client
+                .contract_call::<U64>(
+                    duplicator_address,
+                    SimpleStorageDuplicatorContract::default().get_call_data(),
+                    None,
+                )
+                .await
+                .unwrap(),
+            U64::from(500),
+        );
+
+        let deploy_receipt = seq_test_client
+            .eth_get_transaction_receipt(*deploy_tx.tx_hash())
+            .await
+            .unwrap();
+
+        let set_storage_receipt = seq_test_client
+            .eth_get_transaction_receipt(*set_storage_tx.tx_hash())
+            .await
+            .unwrap();
+
+        // ((53 * 32 + 85 * 32) // 100 + 32) * 48 // 100 + 2
+        assert_eq!(
+            U64::from_str(
+                deploy_receipt
+                    .other
+                    .get("l1DiffSize")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+            )
+            .unwrap(),
+            U64::from(38)
+        );
+
+        // ((53 * 32 // 100) + (2 * 68 * 66 // 100)) * 48 // 100 + 2
+        assert_eq!(
+            U64::from_str(
+                set_storage_receipt
+                    .other
+                    .get("l1DiffSize")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+            )
+            .unwrap(),
+            U64::from(52)
+        );
+    }
 
     seq_task.graceful_shutdown();
     Ok(())
