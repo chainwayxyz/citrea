3eb41f6b686a0c2fb097c085e1ed5a7c2e691ef9
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-04-19 18:57:27 +0200

    Update mod.rs

--- crates/sovereign-sdk/rollup-interface/src/node/rpc/mod.rs
+++ crates/sovereign-sdk/rollup-interface/src/node/rpc/mod.rs
@@ -425,7 +425,7 @@ pub trait LedgerRpcProvider {
     /// Returns the slot number of a given hash
     fn get_slot_number_by_hash(&self, hash: [u8; 32]) -> Result<Option<u64>, anyhow::Error>;
 
-    /// Takes an L1 height and and returns all the sequencer commitments on the slot
+    /// Takes an L1 height and returns all the sequencer commitments on the slot
     fn get_sequencer_commitments_on_slot_by_number(
         &self,
         height: u64,
