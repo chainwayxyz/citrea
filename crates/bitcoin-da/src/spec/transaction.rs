29f5001b0d7c3e91cfe864eb270488b43a1df444
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-05-01 15:53:19 +0300

    Revert TransactionWrapper field visibility

--- crates/bitcoin-da/src/spec/transaction.rs
+++ crates/bitcoin-da/src/spec/transaction.rs
@@ -11,7 +11,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, PartialEq, Eq, Debug, Hash, Deserialize, Serialize)]
 #[repr(transparent)]
 #[serde(transparent)]
-pub struct TransactionWrapper(pub(crate) Transaction);
+pub struct TransactionWrapper(Transaction);
 
 impl TransactionWrapper {
     pub fn empty() -> Self {
