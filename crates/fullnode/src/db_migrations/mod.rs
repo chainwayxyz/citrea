e9087603e5ec8945914fa755130f8f84c051d7b7
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-05-05 16:48:02 +0300

    Get rid of pending da data ledger db migrations in favor of l1 re-indexing

--- crates/fullnode/src/db_migrations/mod.rs
+++ crates/fullnode/src/db_migrations/mod.rs
@@ -2,19 +2,8 @@ use std::sync::OnceLock;
 
 use sov_db::ledger_db::migrations::LedgerMigration;
 
-mod pending_commitments;
-mod pending_proofs;
-
-use pending_commitments::PendingCommitmentsL1HeightFieldAdd;
-use pending_proofs::PendingProofsL1HeightFieldAdd;
-
 pub fn migrations() -> &'static Vec<Box<dyn LedgerMigration + Send + Sync + 'static>> {
     static MIGRATIONS: OnceLock<Vec<Box<dyn LedgerMigration + Send + Sync + 'static>>> =
         OnceLock::new();
-    MIGRATIONS.get_or_init(|| {
-        vec![
-            Box::new(PendingCommitmentsL1HeightFieldAdd {}),
-            Box::new(PendingProofsL1HeightFieldAdd {}),
-        ]
-    })
+    MIGRATIONS.get_or_init(std::vec::Vec::new)
 }
