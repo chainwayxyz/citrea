f35e6adecdf75970abbaa007e1ecc32af30bc990
jfldde <168934971+jfldde@users.noreply.github.com>
2025-05-07 20:28:47 +0100

    Comment

--- bin/citrea/tests/bitcoin/fork.rs
+++ bin/citrea/tests/bitcoin/fork.rs
@@ -314,7 +314,7 @@ impl ForkActivationTest {
         let height = sequencer.client.ledger_get_head_l2_block_height().await?;
         println!("Running test_tangerine_features at height {height}");
 
-        client.sync_nonce().await;
+        client.sync_nonce().await; // sync nonce because of failed pre-tangerine txs
 
         // Test that SCHNORR_VERIFY is available post Tangerine
         {
