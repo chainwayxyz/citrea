59bc3e7d127deea17689f9bdec4d66c346e8f8cb
eyusufatik <esadyusufatik@gmail.com>
2025-05-09 16:26:48 +0300

    update genesis root for lcp nightly

--- crates/light-client-prover/src/circuit/initial_values.rs
+++ crates/light-client-prover/src/circuit/initial_values.rs
@@ -83,7 +83,7 @@ pub mod bitcoinda {
     pub const NIGHTLY_GENESIS_ROOT: [u8; 32] = {
         let hex_root = match option_env!("L2_GENESIS_ROOT") {
             Some(hex_root) => hex_root,
-            None => "1857a148ae2f45e7d268b5abefc885379f63d2b2726597347de96353a9b40b11",
+            None => "8b98ac0e52b57b585a42d9e015c3f0bfdd52e98b0bf41b15837bb6824db9fcec",
         };
 
         match const_hex::const_decode_to_array(hex_root.as_bytes()) {
@@ -95,7 +95,7 @@ pub mod bitcoinda {
     pub const TEST_NETWORK_WITH_FORKS_GENESIS_ROOT: [u8; 32] = {
         let hex_root = match option_env!("L2_GENESIS_ROOT") {
             Some(hex_root) => hex_root,
-            None => "1857a148ae2f45e7d268b5abefc885379f63d2b2726597347de96353a9b40b11",
+            None => "8b98ac0e52b57b585a42d9e015c3f0bfdd52e98b0bf41b15837bb6824db9fcec",
         };
 
         match const_hex::const_decode_to_array(hex_root.as_bytes()) {
