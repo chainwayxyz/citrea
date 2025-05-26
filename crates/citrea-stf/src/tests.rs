e219bc23873bb24d86f4f167896959c3c451f6ee
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-05-26 12:54:11 +0300

    Test wrong sequencer public key

---
+++ crates/citrea-stf/src/tests.rs
@@ -0,0 +1,87 @@
+use sov_keys::default_signature::k256_private_key::K256PrivateKey;
+use sov_keys::PrivateKey;
+use sov_mock_da::MockDaSpec;
+use sov_modules_api::default_context::DefaultContext;
+use sov_modules_api::hooks::{HookL2BlockInfo, L2BlockError};
+use sov_modules_api::WorkingSet;
+use sov_modules_stf_blueprint::StfBlueprint;
+use sov_prover_storage_manager::ProverStorageManager;
+use sov_rollup_interface::stf::StateTransitionError;
+use sov_state::Config;
+
+use crate::runtime::CitreaRuntime;
+
+fn init_storage_manager() -> ProverStorageManager {
+    let dir = tempfile::tempdir().unwrap();
+    let storage_config = Config {
+        path: dir.path().to_path_buf(),
+        db_max_open_files: None,
+    };
+    ProverStorageManager::new(storage_config).unwrap()
+}
+
+#[test]
+fn test_wrong_sequencer_public_key() {
+    let mut stf_blueprint: StfBlueprint<
+        DefaultContext,
+        MockDaSpec,
+        CitreaRuntime<DefaultContext, MockDaSpec>,
+    > = StfBlueprint::<DefaultContext, MockDaSpec, CitreaRuntime<DefaultContext, MockDaSpec>>::new(
+    );
+
+    let storage_manager = init_storage_manager();
+    let prover_storage = storage_manager.create_storage_for_next_l2_height();
+    let mut working_set = WorkingSet::new(prover_storage);
+
+    let sequencer_private_key = K256PrivateKey::generate();
+    let sequencer_public_key = sequencer_private_key.pub_key();
+
+    let random_private_key = K256PrivateKey::generate();
+    let random_public_key = random_private_key.pub_key();
+
+    let l2_block_info = HookL2BlockInfo {
+        l2_height: 1,
+        pre_state_root: [0; 32],
+        current_spec: sov_modules_api::SpecId::Kumquat,
+        sequencer_pub_key: sequencer_public_key,
+        l1_fee_rate: 123,
+        timestamp: 1,
+    };
+    let result = stf_blueprint.begin_l2_block(&random_public_key, &mut working_set, &l2_block_info);
+    assert!(matches!(
+        result,
+        Err(StateTransitionError::L2BlockError(
+            L2BlockError::SequencerPublicKeyMismatch
+        ))
+    ))
+}
+
+#[test]
+fn test_wrong_l2_block_hash() {
+    let mut stf_blueprint: StfBlueprint<
+        DefaultContext,
+        MockDaSpec,
+        CitreaRuntime<DefaultContext, MockDaSpec>,
+    > = StfBlueprint::<DefaultContext, MockDaSpec, CitreaRuntime<DefaultContext, MockDaSpec>>::new(
+    );
+
+    let storage_manager = init_storage_manager();
+    let prover_storage = storage_manager.create_storage_for_next_l2_height();
+    let mut working_set = WorkingSet::new(prover_storage);
+
+    let sequencer_private_key = K256PrivateKey::generate();
+    let sequencer_public_key = sequencer_private_key.pub_key();
+
+    let random_private_key = K256PrivateKey::generate();
+    let random_public_key = random_private_key.pub_key();
+
+    let l2_block_info = HookL2BlockInfo {
+        l2_height: 1,
+        pre_state_root: [0; 32],
+        current_spec: sov_modules_api::SpecId::Kumquat,
+        sequencer_pub_key: sequencer_public_key,
+        l1_fee_rate: 123,
+        timestamp: 1,
+    };
+    let result = stf_blueprint.begin_l2_block(&random_public_key, &mut working_set, &l2_block_info);
+}
