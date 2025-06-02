03d3b6d0db02f049bdcab621af4bf2a826050dae
yaziciahmet <yaziciahmet991@gmail.com>
2025-06-02 16:44:14 +0200

    rm unused contract

--- crates/evm/src/evm/test_data/OneWeiReceiver.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.28;
-
-contract OneWeiReceiver {
-    receive() external payable {
-        require(msg.value == 1, "Amount must be exactly 1 wei");
-    }
-}
