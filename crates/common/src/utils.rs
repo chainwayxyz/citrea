3e86f5a43f8fe9b4ee795b2fd125c7c9862eecc8
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-06-11 11:27:38 +0300

    Do not return error on unknown function selector for system txs

--- crates/common/src/utils.rs
+++ crates/common/src/utils.rs
@@ -104,8 +104,10 @@ async fn update_short_header_proof_from_sys_tx<Da: DaService, DB: SharedLedgerOp
         BridgeContract::depositCall::SELECTOR => {
             tracing::info!("Deposit system tx found inside block");
         }
-        _ => {
-            return Err(anyhow!("Invalid system tx"));
+        unexpected_selector => {
+            tracing::warn!(
+                "Invalid system tx, unexpected function selector: {unexpected_selector:?} , tx input: {:?}, tx hash: {:?}, tx nonce: {:?}", tx.inner().transaction().input(), tx.inner().hash(), tx.inner().transaction().nonce()
+            );
         }
     }
 
