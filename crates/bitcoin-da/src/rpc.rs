188dd14e30cc72bd1a16368aebb7fb1ee01e980e
jfldde <168934971+jfldde@users.noreply.github.com>
2025-06-20 13:19:09 +0200

    Optional hex verbosity

--- crates/bitcoin-da/src/rpc.rs
+++ crates/bitcoin-da/src/rpc.rs
@@ -20,11 +20,11 @@ pub struct MonitoredTxResponse {
     pub prev_txid: Option<Txid>,
     pub next_txid: Option<Txid>,
     pub status: TxStatus,
-    pub hex: String,
+    pub hex: Option<String>,
 }
 
-impl From<(Txid, MonitoredTx)> for MonitoredTxResponse {
-    fn from((txid, tx): (Txid, MonitoredTx)) -> Self {
+impl From<(Txid, MonitoredTx, bool)> for MonitoredTxResponse {
+    fn from((txid, tx, with_hex): (Txid, MonitoredTx, bool)) -> Self {
         let base_fee = if let TxStatus::Pending { base_fee, .. } = tx.status {
             Some(base_fee)
         } else {
@@ -32,7 +32,7 @@ impl From<(Txid, MonitoredTx)> for MonitoredTxResponse {
         };
 
         let raw_tx_bytes = bitcoin::consensus::encode::serialize(&tx.tx);
-        let hex = hex::encode(&raw_tx_bytes);
+        let hex = with_hex.then(|| hex::encode(&raw_tx_bytes));
 
         MonitoredTxResponse {
             txid,
@@ -45,6 +45,12 @@ impl From<(Txid, MonitoredTx)> for MonitoredTxResponse {
             status: tx.status,
             hex,
         }
+    }
+}
+
+impl From<(Txid, MonitoredTx)> for MonitoredTxResponse {
+    fn from((txid, tx): (Txid, MonitoredTx)) -> Self {
+        Self::from((txid, tx, false)) // Defaults to hex verbosity false
     }
 }
 
@@ -121,7 +127,7 @@ impl DaRpcServer for DaRpcServerImpl {
             .monitoring
             .get_monitored_tx(&txid)
             .await
-            .map(|tx| (txid, tx).into()))
+            .map(|tx| (txid, tx, true).into()))
     }
 
     async fn da_get_tx_status(&self, txid: Txid) -> RpcResult<Option<TxStatus>> {
