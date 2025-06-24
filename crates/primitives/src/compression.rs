566b134a740d95e2ac9df33cbfce52523f9719ba
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-06-24 12:29:53 +0300

    Preallocate dynamically

--- crates/primitives/src/compression.rs
+++ crates/primitives/src/compression.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::io::{self, Read, Write};
 
 use crate::MAX_DECOMPRESSED_BLOB_SIZE;
@@ -17,7 +18,7 @@ pub fn decompress_blob(blob: &[u8]) -> Result<Vec<u8>, io::Error> {
 
     let mut buf = [0u8; 4096];
 
-    let mut decompressed_data = Vec::with_capacity(200 * 1024); // Preallocate for 200 KB
+    let mut decompressed_data = Vec::with_capacity(max(blob.len() * 10 / 3, 1024 * 400)); // Knowing that on average brotli compresses our proofs by about 70%, we can preallocate dynamically with the compressed size in mind.
 
     loop {
         match reader.read(&mut buf) {
