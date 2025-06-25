358c0e0f116be346892ff223e96f7a60d37eb7c8
jfldde <168934971+jfldde@users.noreply.github.com>
2025-06-25 13:39:18 +0200

    Remove native flag

--- bin/citrea/tests/bitcoin/mod.rs
+++ bin/citrea/tests/bitcoin/mod.rs
@@ -6,10 +6,10 @@ pub mod rollback;
 mod utils;
 // pub mod mempool_accept;
 pub mod backup;
-#[cfg(feature = "native")]
+// #[cfg(feature = "native")]
 pub mod bitcoin_service;
 pub mod bitcoin_test;
-#[cfg(feature = "native")]
+// #[cfg(feature = "native")]
 pub mod bitcoin_verifier;
 pub mod fork;
 #[cfg(feature = "testing")]
