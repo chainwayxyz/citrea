bb919182791ac6825bf49a8e4868af74aeba1f3a
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-06-30 16:51:40 +0300

    runner docs

--- crates/light-client-prover/src/runner.rs
+++ crates/light-client-prover/src/runner.rs
@@ -1,7 +1,9 @@
+//! Module for the Citrea light client prover runner.
 use citrea_common::RunnerConfig;
 use reth_tasks::shutdown::GracefulShutdown;
 use tracing::instrument;
 
+/// Citrea light client prover runner.
 pub struct CitreaLightClientProver {
     /// Runner configuration.
     _runner_config: RunnerConfig,
@@ -9,6 +11,7 @@ pub struct CitreaLightClientProver {
 
 impl CitreaLightClientProver {
     #[allow(clippy::too_many_arguments)]
+    /// Creates a new instance of the Citrea light client prover.
     pub fn new(runner_config: RunnerConfig) -> Result<Self, anyhow::Error> {
         Ok(Self {
             _runner_config: runner_config,
