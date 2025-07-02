35e625a0a1723e82559b6c7e451621d4d5eaf5ee
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-07-02 14:08:34 +0300

    Implement display for NodeType

--- crates/primitives/src/types.rs
+++ crates/primitives/src/types.rs
@@ -1,13 +1,22 @@
-use derive_more::Display;
-
 pub type L2BlockHash = [u8; 32];
 pub type BlockNumber = u64;
 pub type L2Range = (BlockNumber, BlockNumber);
 
-#[derive(Copy, Clone, Debug, Display)]
+#[derive(Copy, Clone, Debug)]
 pub enum NodeType {
     Sequencer,
     FullNode,
     BatchProver,
     LightClientProver,
+}
+
+impl std::fmt::Display for NodeType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NodeType::BatchProver => write!(f, "batch-prover"),
+            NodeType::Sequencer => write!(f, "sequencer"),
+            NodeType::FullNode => write!(f, "full-node"),
+            NodeType::LightClientProver => write!(f, "light-client-prover"),
+        }
+    }
 }
