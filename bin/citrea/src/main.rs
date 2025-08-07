b07f693eda4d393d0fcefea3c6fd20475f4606ae
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-08-07 12:02:30 +0300

    Lint

--- bin/citrea/src/main.rs
+++ bin/citrea/src/main.rs
@@ -18,7 +18,6 @@ use citrea_stf::genesis_config::GenesisPaths;
 use citrea_stf::runtime::{CitreaRuntime, DefaultContext};
 use clap::Parser;
 use metrics_exporter_prometheus::PrometheusBuilder;
-use metrics_util::MetricKindMask;
 use reth_tasks::TaskManager;
 use short_header_proof_provider::{
     NativeShortHeaderProofProviderService, SHORT_HEADER_PROOF_PROVIDER,
