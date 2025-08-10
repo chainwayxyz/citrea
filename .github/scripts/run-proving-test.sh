5a93a07f5570a9c238d38b5399cd1ed12a28d194
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-08-10 16:15:45 +0300

    fix path in checks and the script

--- .github/scripts/run-proving-test.sh
+++ .github/scripts/run-proving-test.sh
@@ -1,4 +1,4 @@
-TEST_DIR=bin/citrea/tests/proving-stats
+TEST_DIR=proving-stats
 OUT_FILE_NAME="$1"
 
 docker compose -f $TEST_DIR/docker-compose.regtest.yml up -d
@@ -11,7 +11,7 @@ RUST_LOG=off target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $
 PARALLEL_PROOF_LIMIT=2 target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/batch_prover_rollup_config.toml --batch-prover $TEST_DIR/configs/batch_prover_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis >> batch-prover.log &
 
 mkdir -p $TEST_DIR/results
-python3 bin/citrea/tests/proving-stats/get-proving-stats.py batch-prover.log $TEST_DIR/results/$OUT_FILE_NAME
+python3 $TEST_DIR/get-proving-stats.py batch-prover.log $TEST_DIR/results/$OUT_FILE_NAME
 
 pkill citrea
 docker compose -f $TEST_DIR/docker-compose.regtest.yml down
