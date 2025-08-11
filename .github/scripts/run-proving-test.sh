691df83286c6a632a23a3ef88c5bde46edf985d7
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-08-11 17:05:20 +0300

    kill based on pids

--- .github/scripts/run-proving-test.sh
+++ .github/scripts/run-proving-test.sh
@@ -11,12 +11,14 @@ docker exec bitcoin-regtest bitcoin-cli -regtest -rpcuser=citrea -rpcpassword=ci
 docker exec bitcoin-regtest bitcoin-cli -regtest -rpcuser=citrea -rpcpassword=citrea loadwallet batch-prover
 
 RUST_LOG=off target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/sequencer_rollup_config.toml --sequencer $TEST_DIR/configs/sequencer_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis &
+PID_SEQUENCER=$!
 PARALLEL_PROOF_LIMIT=2 target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/batch_prover_rollup_config.toml --batch-prover $TEST_DIR/configs/batch_prover_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis >> batch-prover.log &
+PID_BATCH_PROVER=$!
 
 mkdir -p $TEST_DIR/results
 python3 $TEST_DIR/get-proving-stats.py batch-prover.log $TEST_DIR/results/$OUT_FILE_NAME
 
-pkill -9 citrea
+kill -9 $PID_SEQUENCER $PID_BATCH_PROVER
 docker compose -f $TEST_DIR/docker-compose.regtest.yml down
 sleep 2 
 # After running the container, change ownership back to the user
