cded4d138e915b96effb967fb465717700089ff2
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-08-19 12:01:53 +0300

    Give the var a better name

--- crates/sequencer/src/rpc.rs
+++ crates/sequencer/src/rpc.rs
@@ -311,13 +311,13 @@ impl SequencerRpcServer for SequencerRpcServerImpl {
         match tx_res {
             Ok(hex_res) => {
                 tracing::debug!("Deposit tx processed successfully {}", hex_res);
-                let already_added = self
+                let successfully_added = self
                     .context
                     .deposit_mempool
                     .lock()
                     .add_deposit_tx(deposit.to_vec());
 
-                if !already_added {
+                if !successfully_added {
                     return Err(jsonrpsee::types::error::ErrorObject::owned(
                         jsonrpsee::types::error::INVALID_PARAMS_CODE,
                         "Deposit already pending in mempool",
