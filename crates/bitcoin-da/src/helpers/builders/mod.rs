6e08f321b6521bf8c62bf42eb905b3428dda799e
jfldde <168934971+jfldde@users.noreply.github.com>
2025-08-23 15:34:25 +0200

    Remove early return in commit transaction building
    direct_return wasn't checking against latest tx size. That means that on first iteration, two inputs could be chosen, bumping the tx vsize and the required fee accordingly but the fee was still using default vsize of single input

--- crates/bitcoin-da/src/helpers/builders/mod.rs
+++ crates/bitcoin-da/src/helpers/builders/mod.rs
@@ -113,7 +113,6 @@ fn build_commit_transaction(
         let (chosen_utxos, sum, leftover_utxos) =
             choose_utxos(prev_utxo.clone(), &utxos, input_total)?;
         let has_change = (sum - input_total) >= REVEAL_OUTPUT_AMOUNT;
-        let direct_return = !has_change;
 
         let outputs = if !has_change {
             vec![
@@ -152,20 +151,9 @@ fn build_commit_transaction(
             })
             .collect();
 
-        if direct_return {
-            break (
-                leftover_utxos,
-                Transaction {
-                    lock_time: LockTime::ZERO,
-                    version: bitcoin::transaction::Version(2),
-                    input: inputs,
-                    output: outputs,
-                },
-            );
-        }
-
         let size = get_size_commit(&inputs, &outputs);
 
+        // Size didn't change on this iteration. Fee calculation can be trusted
         if size == last_size {
             break (
                 leftover_utxos,
@@ -237,6 +225,9 @@ fn build_reveal_transaction(
         input: inputs,
         output: outputs,
     };
+
+    println!("[reveal] fee : {:?}", fee);
+    println!("[reveal] tx.vsize() : {:?}", tx.vsize());
 
     Ok(tx)
 }
