f31c66d4f936fa36c52e2e67b9517ffa7e9264a3
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-09-11 16:51:03 +0300

    change comment

--- crates/bitcoin-da/src/verifier.rs
+++ crates/bitcoin-da/src/verifier.rs
@@ -605,7 +605,8 @@ fn bits_to_target(bits: u32) -> [u8; 32] {
     let size = (bits >> 24) as usize;
     let mantissa = bits & 0x00ffffff;
 
-    // Mantissa should not be negative
+    // Mantissa is signed in Bitcoin core, if the sign bit is set, the target is set to 0.
+    // https://github.com/bitcoin/bitcoin/blob/ee42d59d4de970769ebabf77b89ff4269498f61e/src/arith_uint256.cpp#L175
     if mantissa > 0x7F_FFFF {
         return [0; 32];
     }
