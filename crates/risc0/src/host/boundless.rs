627362074209df9e7bf9189513f4ab69a5de774d
eyusufatik <esadyusufatik@gmail.com>
2025-09-15 19:20:13 +0300

    erce pricing updates

--- crates/risc0/src/host/boundless.rs
+++ crates/risc0/src/host/boundless.rs
@@ -281,7 +281,7 @@ impl BoundlessProver {
                     .with_timeout(timeout as u32)
                     .with_ramp_up_period(ramp_up_period as u32)
                     .with_bidding_start(bidding_start)
-                    .with_lock_stake(U256::from(lock_stake) * Unit::MWEI.wei_const()),
+                    .with_lock_stake(U256::from(lock_stake)),
             );
 
         // If we can provide these then in the preflight layer of request sending there won't be a double execution of the program
@@ -489,7 +489,7 @@ impl BoundlessProver {
         // Retrieve the maximum possible price again from the pricing service as the price of ether may have changed.
         let exponential_backoff = ExponentialBackoff::default();
 
-        let max_possible_price = retry_backoff(exponential_backoff, || async move {
+        let price_response = retry_backoff(exponential_backoff, || async move {
             match self
                 .pricing_service
                 .get_price(mcycles_count.saturating_mul(1_000_000))
@@ -514,8 +514,9 @@ impl BoundlessProver {
                 request_id,
                 e
             )
-        })?
-        .max_possible_price;
+        })?;
+        let max_possible_price = price_response.max_possible_price;
+        let lock_stake = price_response.lock_stake;
 
         // TODO: https://github.com/chainwayxyz/citrea/issues/2417
         // Define new request with updated parameters
@@ -567,12 +568,6 @@ impl BoundlessProver {
             }
         };
 
-        // This is usdc amount * 10^6 because usdc denomination is 6
-        // 1 usdc = 10^6 micro usdc (mwei)
-        let lock_stake = failed_request.offer.lockStake;
-        // Convert to usdc amount
-        let lock_stake = lock_stake.div_ceil(Unit::MWEI.wei_const());
-
         let new_request = self.build_proof_request(
             image_id,
             failed_request
@@ -594,7 +589,7 @@ impl BoundlessProver {
             (new_lock_timeout * 2) as u64,
             failed_request.offer.rampUpPeriod as u64,
             current_timestamp_as_secs(), // bidding start
-            lock_stake.to::<u64>(),
+            lock_stake,
             // TODO: https://github.com/chainwayxyz/citrea/issues/2820
             None,
             None,
