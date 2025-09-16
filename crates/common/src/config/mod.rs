03a983a3e5930a0c1d0b50b79f0d1f2f5e4267ab
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-09-16 17:22:37 +0300

    add to rpc config test

--- crates/common/src/config/mod.rs
+++ crates/common/src/config/mod.rs
@@ -634,6 +634,7 @@ mod tests {
         std::env::set_var("RPC_MAX_CONNECTIONS", "500");
         std::env::set_var("RPC_ENABLE_SUBSCRIPTIONS", "true");
         std::env::set_var("RPC_MAX_SUBSCRIPTIONS_PER_CONNECTION", "200");
+        std::env::set_var("RPC_DISABLE_JS_TRACER", "true");
         std::env::set_var("RPC_TIMEOUT", "30");
 
         std::env::set_var(
@@ -667,7 +668,7 @@ mod tests {
                 max_subscriptions_per_connection: 200,
                 trace_chain_block_limit: None,
                 timeout: 30,
-                disable_js_tracer: false,
+                disable_js_tracer: true,
                 api_key: None,
             },
             storage: StorageConfig {
