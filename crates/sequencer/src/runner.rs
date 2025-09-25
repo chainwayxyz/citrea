5f3e24c30262cead027eb853371ca16812d0f5ec
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-09-25 17:53:44 +0300

    mul by literal

--- crates/sequencer/src/runner.rs
+++ crates/sequencer/src/runner.rs
@@ -1070,7 +1070,8 @@ where
 
         let multiplied_l1_fee_rate = |rate: u128| -> u128 {
             let multiplied = ((rate as f64) * l1_fee_rate_multiplier).ceil() as u128;
-            let max_fee_wei_per_byte = max_l1_fee_rate as u128 * 10_u128.pow(10) / 4;
+            // multiply with 10^10/4 = 2_500_000_000 to convert sat/vbyte to wei/byte
+            let max_fee_wei_per_byte = max_l1_fee_rate as u128 * 2_500_000_000;
 
             multiplied.min(max_fee_wei_per_byte)
         };
