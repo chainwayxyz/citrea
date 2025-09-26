66b2215b1025c2d1bbff9a96480469be1b705906
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-09-26 16:58:07 +0300

    test with citrea e2e branched from older commit

--- bin/citrea/tests/bitcoin/bitcoin_test.rs
+++ bin/citrea/tests/bitcoin/bitcoin_test.rs
@@ -11,7 +11,6 @@ use citrea_batch_prover::rpc::BatchProverRpcClient;
 use citrea_e2e::bitcoin::{BitcoinNode, DEFAULT_FINALITY_DEPTH};
 use citrea_e2e::config::{BitcoinConfig, TestCaseConfig};
 use citrea_e2e::framework::TestFramework;
-use citrea_e2e::node::NodeKind;
 use citrea_e2e::test_case::{TestCase, TestCaseRunner};
 use citrea_e2e::traits::Restart;
 use citrea_e2e::Result;
@@ -27,7 +26,7 @@ impl TestCase for BitcoinReorgTest {
         TestCaseConfig {
             with_sequencer: true,
             with_batch_prover: true,
-            n_nodes: std::collections::HashMap::from([(NodeKind::Bitcoin, 2)]),
+            n_nodes: 2,
             ..Default::default()
         }
     }
