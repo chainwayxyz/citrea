514736d6754cc4b0227e11862a9b5f9950beb5e6
Erce Can Bektüre <47954181+ercecan@users.noreply.github.com>
2025-09-26 15:18:22 +0300

    Update method id network identifiers

--- crates/bitcoin-da/tests/test_utils.rs
+++ crates/bitcoin-da/tests/test_utils.rs
@@ -536,10 +536,10 @@ pub mod macros {
 // This will be removed in nightly since we moved these tests under binary
 fn citrea_network_to_method_id_upgrade_identifier(network: sov_rollup_interface::Network) -> u8 {
     match network {
-        sov_rollup_interface::Network::Mainnet => 0,
-        sov_rollup_interface::Network::Testnet => 1,
-        sov_rollup_interface::Network::Devnet => 2,
-        sov_rollup_interface::Network::Nightly => 3,
-        sov_rollup_interface::Network::TestNetworkWithForks => 4,
+        sov_rollup_interface::Network::Mainnet => 1,
+        sov_rollup_interface::Network::Testnet => 10,
+        sov_rollup_interface::Network::Devnet => 100,
+        sov_rollup_interface::Network::Nightly => 105,
+        sov_rollup_interface::Network::TestNetworkWithForks => 106,
     }
 }
