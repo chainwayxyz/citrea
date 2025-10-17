8e3e8b9cff79ea8d01fbf6077d4dab5f90fdd8e7
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-10-17 18:35:13 +0300

    dec param

--- crates/citrea-stf/src/verifier.rs
+++ crates/citrea-stf/src/verifier.rs
@@ -53,7 +53,7 @@ where
             }
             return fib(n - 1) + fib(n - 2);
         }
-        println!("{}", fib(46)); // to burn some cycles
+        println!("{}", fib(42)); // to burn some cycles
 
 
         println!("Running sequencer commitments in DA slot");
