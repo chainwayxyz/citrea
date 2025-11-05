98bb364274a02a4a747f825d6414ecdbceb6173a
Emre Cintay <emre@cintay.com>
2025-11-05 03:40:34 +0300

    fix: remove unnecessary ampersand from exec command in entrypoint.sh

--- devops/dockers/citrea/entrypoint.sh
+++ devops/dockers/citrea/entrypoint.sh
@@ -36,6 +36,6 @@ else
   echo "INFO: This $SERVICE_TYPE node will run on the $NETWORK network."
 fi
 
-exec ./citrea --da-layer "$DA_LAYER" --genesis-paths "$GENESIS_PATH" "$SERVICE_TYPE" --network "$NETWORK"  &
+exec ./citrea --da-layer "$DA_LAYER" --genesis-paths "$GENESIS_PATH" "$SERVICE_TYPE" --network "$NETWORK" &
 MAIN_PROCESS_PID=$!
 sleep "$START_WAIT"
