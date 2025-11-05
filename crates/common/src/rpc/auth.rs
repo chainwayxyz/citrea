ce8a04a7f93943e77e24041818fa603beb8508b1
jfldde <168934971+jfldde@users.noreply.github.com>
2025-11-05 17:24:53 +0000

    Remove da job RPCs from PROTECTED_METHODS

--- crates/common/src/rpc/auth.rs
+++ crates/common/src/rpc/auth.rs
@@ -8,13 +8,7 @@ use jsonrpsee::MethodResponse;
 use serde_json::value::RawValue;
 use serde_json::Value;
 
-const PROTECTED_METHODS: [&str; 5] = [
-    "backup_create",
-    "backup_validate",
-    "backup_info",
-    "da_job_cancel",
-    "da_job_retry",
-];
+const PROTECTED_METHODS: [&str; 5] = ["backup_create", "backup_validate", "backup_info"];
 
 #[derive(Debug, Clone)]
 pub struct Auth<S> {
