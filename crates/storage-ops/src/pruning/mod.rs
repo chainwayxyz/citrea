3f09bf0791f91729f94b2ca12ba7121a1983773e
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-11-06 20:50:52 +0300

    Reactivate state pruning

--- crates/storage-ops/src/pruning/mod.rs
+++ crates/storage-ops/src/pruning/mod.rs
@@ -6,7 +6,6 @@ use futures::future;
 use ledger::prune_ledger;
 use native::prune_native_db;
 use sov_db::schema::tables::{LastPrunedBlock, LastPrunedL2Height};
-use tracing::info;
 
 use self::criteria::{Criteria, DistanceCriteria};
 pub use self::service::*;
@@ -23,7 +22,6 @@ pub struct Pruner {
     /// Access to native DB.
     native_db: Arc<sov_schema_db::DB>,
     /// Access to state DB.
-    #[allow(dead_code)]
     state_db: Arc<sov_schema_db::DB>,
     /// Criteria to decide pruning
     criteria: Box<dyn Criteria + Send + Sync>,
@@ -63,26 +61,22 @@ impl Pruner {
 
     /// Prune everything
     pub async fn prune(&self, node_type: NodeType, up_to_block: u64) {
-        info!("Pruning up to L2 block: {}", up_to_block);
         let ledger_db = self.ledger_db.clone();
-
         let native_db = self.native_db.clone();
-
-        // let state_db = self.state_db.clone();
+        let state_db = self.state_db.clone();
 
         let ledger_pruning_handle =
             tokio::task::spawn_blocking(move || prune_ledger(node_type, ledger_db, up_to_block));
 
-        // TODO: Fix me
-        // let state_db_pruning_handle =
-        //     tokio::task::spawn_blocking(move || prune_state_db(state_db, up_to_block));
+        let state_db_pruning_handle =
+            tokio::task::spawn_blocking(move || state::prune_state_db(state_db, up_to_block));
 
         let native_db_pruning_handle =
             tokio::task::spawn_blocking(move || prune_native_db(native_db, up_to_block));
 
-        future::join_all([
+        let _results = future::join_all([
             ledger_pruning_handle,
-            // state_db_pruning_handle,
+            state_db_pruning_handle,
             native_db_pruning_handle,
         ])
         .await;
