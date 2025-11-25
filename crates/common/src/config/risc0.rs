c106d1e575a9c54108002a04261bf2ac0bf530d5
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2025-11-25 10:52:51 +0300

    Fix presigned config logic

--- crates/common/src/config/risc0.rs
+++ crates/common/src/config/risc0.rs
@@ -30,8 +30,8 @@ impl FromEnv for BoundlessS3StorageConfig {
         let s3_url = read_env("BOUNDLESS_S3_URL")?;
         let aws_region = read_env("BOUNDLESS_AWS_REGION")?;
         let s3_use_presigned = read_env("BOUNDLESS_S3_NO_PRESIGNED")
-            .map(|s| s.eq_ignore_ascii_case("true") || s == "1")
-            .unwrap_or(true);
+            .map(|s| !(s.eq_ignore_ascii_case("true") || s == "1"))
+            .unwrap_or_default();
 
         Ok(Self {
             s3_access_key,
