1e8d26ced9467ef841d9ee65a0ec347cfc3d184d
eyusufatik <esadyusufatik@gmail.com>
2025-12-18 16:16:08 +0300

    remove outdated comment

--- crates/bitcoin-da/src/fee.rs
+++ crates/bitcoin-da/src/fee.rs
@@ -110,7 +110,6 @@ impl FeeService {
     /// Get the fee rate in sat/vB from the mempool space or via the Bitcoin Core client.
     #[instrument(level = "trace", skip_all, ret)]
     pub async fn get_fee_rate(&self) -> Result<f64> {
-        // If network is regtest or signet, mempool space is not available
         let sat_vkb = match get_fee_rate_from_mempool_space(&self.mempool_space_url).await {
             Ok(fee_rate) => fee_rate,
             Err(e) => {
