10d833201bf9e4b3004267329a48fea703604572
eyusufatik <esadyusufatik@gmail.com>
2026-01-05 11:18:27 +0300

    rustier code

--- crates/bitcoin-da/src/service.rs
+++ crates/bitcoin-da/src/service.rs
@@ -1233,8 +1233,14 @@ impl DaService for BitcoinService {
 
         // multiply with 10^10/4 = 25*10^8 = 2_500_000_000 for BTC to CBTC conversion (decimals)
         // if somehow the value is out of bounds, return a default fee rate of 1 BTC/vB
-        let multiplied_fee =
-            f64_to_u128_or_default((sat_vb * 2_500_000_000f64).ceil(), 1 * 2_500_000_000);
+        let sat_vb = (sat_vb * 2_500_000_000f64).ceil();
+        let multiplied_fee = f64_to_u128(sat_vb).unwrap_or_else(|| {
+            warn!(
+                "Fee rate {} out of bounds, returning default fee rate of 1 CBTC/vB",
+                sat_vb
+            );
+            1 * 2_500_000_000
+        });
         Ok(multiplied_fee)
     }
 
@@ -1422,11 +1428,10 @@ fn calculate_witness_root(txdata: &[TransactionWrapper], tx_count: usize) -> [u8
     BitcoinMerkleTree::new(hashes).root()
 }
 
-fn f64_to_u128_or_default(x: f64, default: u128) -> u128 {
+fn f64_to_u128(x: f64) -> Option<u128> {
     if x.is_finite() && x >= 0.0 && x <= (u128::MAX as f64) {
-        x as u128
+        Some(x as u128)
     } else {
-        warn!("Fee rate conversion out of bounds: {x}, using default {default}");
-        default
+        None
     }
 }
