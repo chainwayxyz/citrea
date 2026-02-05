acae22033797cd6b9d833de37bc96a50059b6172
eyusufatik <esadyusufatik@gmail.com>
2026-02-05 11:56:08 +0300

    set mainnet activation height

--- crates/primitives/src/forks.rs
+++ crates/primitives/src/forks.rs
@@ -53,7 +53,10 @@ pub fn fork_from_block_number(block_number: u64) -> Fork {
     forks[pos]
 }
 
-pub const MAINNET_FORKS: [Fork; 1] = [Fork::new(SpecId::Tangelo, 0)];
+pub const MAINNET_FORKS: [Fork; 2] = [
+    Fork::new(SpecId::Tangelo, 0),
+    Fork::new(SpecId::TangeloSelfdestructFix, 3098000), // will be reached Feb 5 2026 ~1 PM UTC
+];
 
 pub const TESTNET_FORKS: [Fork; 5] = [
     Fork::new(SpecId::Genesis, 0),
